6d3914cd34b984fcf0682fb4d786680655771199
James Henstridge <james@daa.com.au>
1998-12-08 04:12:24 +0000

    forgot the copyright message at the top of the file.
    1998-12-08  James Henstridge  <james@daa.com.au>
    	* url-properties.c: forgot the copyright message at the top of the
    	file.

--- capplets/url-properties/url-properties.c
+++ capplets/url-properties/url-properties.c
@@ -1,3 +1,21 @@
+/* url-properties -- a capplet to configure the behaviour of gnome_url_show
+ * Copyright (C) 1998  James Henstridge <james@daa.com.au>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif
