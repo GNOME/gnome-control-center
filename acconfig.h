7b75a1062f522f1dddf9d4cda8b01e473df0bde6
Michael Fulbright <drmike@src.gnome.org>
1998-12-08 20:47:55 +0000

    New and better than ever in a new home
    Dr Mike <drmike@redhat.com>

--- acconfig.h
+++ acconfig.h
@@ -13,25 +13,9 @@
 #undef HAVE_ESD
 #undef HAVE_LIBESD
 
-#undef HAVE_LIBGTOP
-
-/* Define if LibGTop has support for multiple processors. */
-#undef HAVE_LIBGTOP_SMP
-
 /* Define if there is no `u_int64_t' and `int64_t'. */
 #undef u_int64_t
 #undef int64_t
 
 #undef HAVE_DEVGTK
 
-/* LibGTop major, minor and micro version. */
-#undef LIBGTOP_MAJOR_VERSION
-#undef LIBGTOP_MINOR_VERSION
-#undef LIBGTOP_MICRO_VERSION
-
-/* LibGTop version and numerical version code ("1.234.567" -> 1234567). */
-#undef LIBGTOP_VERSION
-#undef LIBGTOP_VERSION_CODE
-
-/* LibGTop server version, increased each time the protocol changes. */
-#undef LIBGTOP_SERVER_VERSION
