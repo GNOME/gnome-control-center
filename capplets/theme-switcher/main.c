99a2cb46f4c0fca36aeaa2e1b8e6aa45fa96e6a1
Jonathan Blandford <jrb@redhat.com>
1999-02-22 16:22:28 +0000

    changed session_args[2] -> session_args[3] to allow for THREE items in it.
    1999-02-22  Jonathan Blandford  <jrb@redhat.com>
    
    	* capplets/background-properties/app-background.c (main): changed
    	session_args[2] -> session_args[3] to allow for THREE items in it.
    	* capplets/screensaver-properties/screensaver-properties-capplet.c
    	(main):  ditto.
    	* capplets/bell-properties/bell-properties.c (main): ditto
    	* capplets/keyboard-properties/keyboard-properties.c (main):
    	ditto.
    	* capplets/sound-properties/sound-properties.c (main): ditto.
    
    1999-02-22  Jonathan Blandford  <jrb@redhat.com>
    
    	* capplet-manager.c (launch_capplet): fixed bug in save-changes
    	dialog
    
    	* callbacks.c (exit_row_callback): fixed the about box.

--- capplets/theme-switcher/main.c
+++ capplets/theme-switcher/main.c
@@ -13,9 +13,13 @@ main(int argc, char **argv)
 
   set_tmp_rc();
   do_demo(argc, argv);
-  gnome_capplet_init ("theme-switcher-capplet",
-		      THEME_SWITCHER_VERSION, argc, argv, NULL, 0, NULL);
-  
+  switch (gnome_capplet_init ("theme-switcher-capplet",
+			      THEME_SWITCHER_VERSION, argc, argv, NULL, 0, NULL)) {
+  case -1:
+	  exit (1);
+  case 1:
+	  return 0;
+  }  
   w = make_main();
   gtk_widget_show_all(w);
   send_socket();
