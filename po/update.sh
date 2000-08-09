c5f862ea35d05a2e4d6e6a0cac65b0b139c438d7
Robin Slomkowski <rslomkow@src.gnome.org>
2000-08-09 17:13:37 +0000

    fixed lost header

--- po/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-xgettext --default-domain=control-center --directory=.. \
-  --add-comments --keyword=_ --keyword=N_ \
-  --files-from=./POTFILES.in \
-&& test ! -f control-center.po \
-   || ( rm -f ./control-center.pot \
-    && mv control-center.po ./control-center.pot )
