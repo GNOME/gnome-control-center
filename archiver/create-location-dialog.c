7beca6f9f93ae9adaa8b5e29b8a7018108d236d8
Hans Petter <hansp@src.gnome.org>
2001-01-12 15:03:02 +0000

    Changed e-mail addresses: helixcode.com -> ximian.com.

--- archiver/create-location-dialog.c
+++ archiver/create-location-dialog.c
@@ -3,7 +3,7 @@
 /* create-location-dialog.c
  * Copyright (C) 2000-2001 Ximian, Inc.
  *
- * Written by Bradford Hovinen <hovinen@helixcode.com>
+ * Written by Bradford Hovinen <hovinen@ximian.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
