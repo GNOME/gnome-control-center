bc6834b61ff738feeda92b44a41fa714504d625b
Chema Celorio <chema@celorio.com>
2001-07-14 03:04:57 +0000

    check for capplets/common/capplet-util.h
    2001-07-13  Chema Celorio  <chema@celorio.com>
    
    	* autogen.sh (PKG_NAME): check for capplets/common/capplet-util.h
    
    	* configure.in (capplet_modules): pkgconfig 0.8.0 now handles
    	gnome-vfs. No need to call gnome-vfs-config.

--- autogen.sh
+++ autogen.sh
@@ -6,8 +6,7 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="GNOME Control Center"
 
-(test -f $srcdir/configure.in \
-  && test -d $srcdir/capplets \
+(test -f $srcdir/capplets/common/capplet-util.h \
   && test -d $srcdir/control-center) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level "\`$PKG_NAME\'" directory"
