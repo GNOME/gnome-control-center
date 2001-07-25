801b831ff49158c3d18665bc436d0fef3eb43787
Jose Maria Celorio <chema@src.gnome.org>
2001-07-25 06:08:55 +0000

    Die mofo !

---
+++ control-center/kill-gnomecc.sh
@@ -0,0 +1,12 @@
+killall background-properties-capplet
+killall bonobo-moniker-archiver
+killall sound-properties
+killall keyboard-properties
+killall oafd
+oaf-slay
+
+killall -9 background-properties-capplet
+killall -9 bonobo-moniker-archiver
+killall -9 sound-properties
+killall -9 keyboard-properties
+killall -9 oafd
