d69facaef6594c9f52a298f5f3a1ec7aa8009cdc
Bradford Hovinen <hovinen@ximian.com>
2001-12-17 20:47:24 +0000

    Set the pixmap_id correctly if pixmap is NULL or -1.
    2001-12-17  Bradford Hovinen  <hovinen@ximian.com>
    
    	* applier.c (set_root_pixmap): Set the pixmap_id correctly if
    	pixmap is NULL or -1.

--- libbackground/applier.c
+++ libbackground/applier.c
@@ -1313,7 +1313,10 @@ set_root_pixmap (GdkPixmap *pixmap)
 	guchar *data_esetroot;
 	Pixmap pixmap_id;
 
-	pixmap_id = GDK_WINDOW_XWINDOW (pixmap);
+	if (pixmap != NULL && pixmap != (GdkPixmap *) -1)
+		pixmap_id = GDK_WINDOW_XWINDOW (pixmap);
+	else
+		pixmap_id = 0;
 
 	XGrabServer (GDK_DISPLAY ());
 
