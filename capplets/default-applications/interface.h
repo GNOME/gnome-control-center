7113193d0c9b48f40c737a1b8253492aef745511
Jonathan Blandford <jrb@src.gnome.org>
2002-01-16 05:43:18 +0000

    clean up directory

--- capplets/default-applications/interface.h
+++
@@ -1,7 +0,0 @@
-
-void edit_create (void);
-
-extern void fill_default_browser (void);
-extern void fill_default_terminal (void);
-extern void fill_default_help (void);
-extern void fill_default_editor (void);
