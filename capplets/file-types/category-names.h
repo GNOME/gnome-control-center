09ba137cf2c40b6eb1d8d162650184ebe8b36855
Bradford Hovinen <hovinen@ximian.com>
2002-01-27 20:07:30 +0000

    Invoke gettext macro on the category name
    2002-01-27  Bradford Hovinen  <hovinen@ximian.com>
    
    	* mime-types-model.c (mime_types_model_get_value): Invoke gettext
    	macro on the category name
    
    	* category-names.h: Add this file

---
+++ capplets/file-types/category-names.h
@@ -0,0 +1,16 @@
+/*
+ * Translatable strings file
+ * Add this file to your project's POTFILES.in.
+ * DO NOT compile it as part of your application.
+ */
+
+gchar *s = N_("Audio");
+gchar *s = N_("Video");
+gchar *s = N_("Images");
+gchar *s = N_("Documents");
+gchar *s = N_("Word Processor");
+gchar *s = N_("World Wide Web Page (HTML)");
+gchar *s = N_("Spreadsheet");
+gchar *s = N_("Plain Text");
+gchar *s = N_("Source Code");
+gchar *s = N_("Extended Markup Language (XML)");
