9d5d9145f4e5ae60a9a6e50ab350a3d653bfe97e
Kjartan Maraas <kmaraas@gnome.org>
2002-02-27 22:14:18 +0000

    s/PACKAGE/GETTEXT_PACKAGE/g
    2002-02-27  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* gnome-default-applications-properties.c: s/PACKAGE/GETTEXT_PACKAGE/g

--- capplets/default-applications/gnome-default-applications-properties.c
+++ capplets/default-applications/gnome-default-applications-properties.c
@@ -576,9 +576,9 @@ main (int argc, char **argv)
 		{ NULL, '\0', 0, NULL, 0, NULL, NULL }
 	};
 
-	bindtextdomain (PACKAGE, GNOMELOCALEDIR);
-	bind_textdomain_codeset (PACKAGE, "UTF-8");
-	textdomain (PACKAGE);
+	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+	textdomain (GETTEXT_PACKAGE);
 
 	gnome_program_init (argv[0], VERSION, LIBGNOMEUI_MODULE, argc, argv,
 			    GNOME_PARAM_POPT_TABLE, cap_options,
