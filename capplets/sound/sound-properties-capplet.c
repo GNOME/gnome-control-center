1c6664a71ba328e73eca55a46b0828777f891aea
Richard Hestilow <hestilow@ximian.com>
2002-03-07 01:55:39 +0000

    Remove textdomain stuff. I'm not 100% certain this is correct, but none of
    2002-03-06  Richard Hestilow  <hestilow@ximian.com>
    
    	* sound-properties-capplet.c (main): Remove textdomain stuff.
    	I'm not 100% certain this is correct, but none of the other capplets
    	have it.

--- capplets/sound/sound-properties-capplet.c
+++ capplets/sound/sound-properties-capplet.c
@@ -139,10 +139,6 @@ main (int argc, char **argv)
 		{ NULL, '\0', 0, NULL, 0, NULL, NULL }
 	};
 
-	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
-	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-	textdomain (GETTEXT_PACKAGE);
-
 	gnome_program_init (argv[0], VERSION, LIBGNOMEUI_MODULE, argc, argv,
 			    GNOME_PARAM_POPT_TABLE, cap_options,
 			    NULL);
