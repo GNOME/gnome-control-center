ff128913a45eb3ab20283d4e8ef038d65c55ee7b
Lauris Kaplinski <lauris@src.gnome.org>
2002-03-26 22:56:20 +0000

    Moved font capplet backt o original GUI and made it to work otherwise

--- capplets/font/main.c
+++ capplets/font/main.c
@@ -1,56 +1,150 @@
+#define __GPM_PRINTER_C__
+
+/*
+ *  GNOME Print Manager
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * Authors:
+ *  <Write your name here if you are>
+ *   Lauris Kaplinski <lauris@ximian.com>
+ *
+ *  Copyright (C) 2002 Ximian, Inc. and authors
+ */
+
 #include <config.h>
-#include <gnome.h>
+
+#include <string.h>
+
+#include <gconf/gconf-client.h>
+#include <libgnome/gnome-i18n.h>
+#include <libgnome/gnome-program.h>
+#include <gtk/gtkmain.h>
+#include <gtk/gtkdialog.h>
+#include <gtk/gtktogglebutton.h>
+#include <gtk/gtktogglebutton.h>
+#include <libgnomeui/gnome-ui-init.h>
+#include <libgnomeui/gnome-font-picker.h>
 #include <glade/glade.h>
 
-#include "gconf-property-editor.h"
+#include "activate-settings-daemon.h"
 
 static GladeXML *xml;
-static GConfChangeSet *changeset;
 	
+#define FONT_CAPPLET_DIR "/desktop/gnome/interface"
+#define FONT_CAPPLET_KEY_FONT_NAME "/desktop/gnome/interface/font_name"
 
 #define WID(w) (glade_xml_get_widget (xml, (w)))
-
-enum
-{
-	RESPONSE_APPLY = 1,
-	RESPONSE_CLOSE
-};
 
 void
 response_cb (GtkDialog *dialog, gint r, gpointer data)
 {
-	switch (r)
-	{
-	case RESPONSE_APPLY:
-		gconf_client_commit_change_set (gconf_client_get_default (), changeset, TRUE, NULL);
-		
-		/* We may want to revert to the default schema-ed value */
-		if (!gconf_client_get_bool (gconf_client_get_default (), "/desktop/gnome/interface/use_custom_font", NULL))
-		{
-			g_print ("Unsetting\n");
-			gconf_client_unset (gconf_client_get_default (), "/desktop/gnome/interface/font_name", NULL);
-		}		
-		
-		break;
-	case RESPONSE_CLOSE:
-		gtk_main_quit ();
-		break;
+	gtk_main_quit ();
+}
+
+static void
+font_capplet_custom_toggled (GtkToggleButton *tb, GConfClient *client)
+{
+	gchar *sval;
+
+	sval = gconf_client_get_string (client, FONT_CAPPLET_KEY_FONT_NAME, NULL);
+
+	if (gtk_toggle_button_get_active (tb)) {
+		const gchar *fontname;
+		/* Use custom font */
+		fontname = gnome_font_picker_get_font_name (GNOME_FONT_PICKER (WID ("font_picker")));
+		if (fontname) {
+			/* We are really nice here */
+			if (!sval || strcmp (fontname, sval)) {
+				gconf_client_set_string (client, FONT_CAPPLET_KEY_FONT_NAME, fontname, NULL);
+			}
+		}
+	} else {
+		/* Do not use custom font */
+		if (sval) {
+			gconf_client_unset (client, FONT_CAPPLET_KEY_FONT_NAME, NULL);
+		}
+	}
+
+	gtk_widget_set_sensitive (WID ("font_picker"), gtk_toggle_button_get_active (tb));
+
+	if (sval) g_free (sval);
+}
+
+static void
+font_capplet_font_set (GnomeFontPicker *fp, const gchar *fontname, GConfClient *client)
+{
+	gchar *sval;
+
+	sval = gconf_client_get_string (client, FONT_CAPPLET_KEY_FONT_NAME, NULL);
+
+	if (fontname) {
+		if (!sval || strcmp (fontname, sval)) {
+			gconf_client_set_string (client, FONT_CAPPLET_KEY_FONT_NAME, fontname, NULL);
+		}
+	}
+
+	if (sval) g_free (sval);
+}
+
+static void
+font_capplet_value_notify (GConfClient *client, guint id, GConfEntry *entry, gpointer data)
+{
+	if (entry->value) {
+		const gchar *sval;
+		/* We have value set */
+		sval = gconf_value_get_string (entry->value);
+		if (sval) {
+			gnome_font_picker_set_font_name (GNOME_FONT_PICKER (WID ("font_picker")), sval);
+			gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (WID ("custom_check")), TRUE);
+		}
+	} else {
+		/* No value set */
+		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (WID ("custom_check")), FALSE);
 	}
 }
 
 static void
-setup_dialog (void)
+setup_dialog (GConfClient *client)
 {
+	gchar *sval;
+#if 0
 	GObject *peditor;
 
 	peditor = gconf_peditor_new_boolean (NULL, /*changeset,*/ "/desktop/gnome/interface/use_custom_font", WID ("custom_check"), NULL);
 	gconf_peditor_widget_set_guard (GCONF_PROPERTY_EDITOR (peditor), WID ("font_picker"));
 	peditor = gconf_peditor_new_font (NULL, /*changeset,*/ "/desktop/gnome/interface/font_name", WID ("font_picker"), NULL);
+#endif
+
+	/* Set initial values */
+	sval = gconf_client_get_string (client, FONT_CAPPLET_KEY_FONT_NAME, NULL);
+	if (sval) gnome_font_picker_set_font_name (GNOME_FONT_PICKER (WID ("font_picker")), sval);
+	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (WID ("custom_check")), sval != NULL);
+	gtk_widget_set_sensitive (WID ("font_picker"), sval != NULL);
+	if (sval) g_free (sval);
+
+	g_signal_connect (G_OBJECT (WID ("custom_check")), "toggled", G_CALLBACK (font_capplet_custom_toggled), client);
+	g_signal_connect (G_OBJECT (WID ("font_picker")), "font_set", G_CALLBACK (font_capplet_font_set), client);
+	gconf_client_notify_add (client, FONT_CAPPLET_KEY_FONT_NAME, font_capplet_value_notify, NULL, NULL, NULL);
 }
 	
 int
 main (int argc, char **argv)
 {
+	GConfClient *client;
+
 	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 	textdomain (GETTEXT_PACKAGE);
@@ -60,13 +154,15 @@ main (int argc, char **argv)
 
 	xml = glade_xml_new (GNOMECC_DATA_DIR "/interfaces/font-properties.glade", NULL, NULL);
 
-	changeset = gconf_change_set_new ();
-	setup_dialog ();
+	activate_settings_daemon ();
+
+	client = gconf_client_get_default ();
+	gconf_client_add_dir (client, FONT_CAPPLET_DIR, GCONF_CLIENT_PRELOAD_ONELEVEL, NULL);
+
+	setup_dialog (client);
 	
 	glade_xml_signal_autoconnect (xml);
 	gtk_main ();
-
-	gconf_change_set_unref (changeset);
  
 	return 0;
 }
