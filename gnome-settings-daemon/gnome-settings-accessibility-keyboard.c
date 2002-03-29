84332cb647fab2d3bcb5e4a08d7039e4e855cb5a
Jody Goldberg <jody@gnome.org>
2002-03-29 22:30:40 +0000

    fix spelling error in schema.
    2002-03-29  Jody Goldberg <jody@gnome.org>
    
    	* gnome-settings-accessibility-keyboard.c (set_server_from_gconf) :
    	  fix spelling error in schema.

--- gnome-settings-daemon/gnome-settings-accessibility-keyboard.c
+++ gnome-settings-daemon/gnome-settings-accessibility-keyboard.c
@@ -33,7 +33,7 @@
 #  include <X11/XKBlib.h>
 #  include <X11/extensions/XKBstr.h>
 
-#define CONFIG_ROOT "/desktop/gnome/accesibility/keyboard"
+#define CONFIG_ROOT "/desktop/gnome/accessibility/keyboard"
 
 #ifdef DEBUG_ACCESSIBILITY
 #define d(str)		fprintf (stderr, str)
