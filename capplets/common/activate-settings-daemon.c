d9f1844c305fae7430769af8ced2ffc4373a1216
Jody Goldberg <jody@gnome.org>
2002-04-18 14:41:21 +0000

    message dialogs must have separators.
    2002-04-18  Jody Goldberg <jody@gnome.org>
    
    	* activate-settings-daemon.c (static) : message dialogs must have
    	  separators.

--- capplets/common/activate-settings-daemon.c
+++ capplets/common/activate-settings-daemon.c
@@ -14,8 +14,8 @@ static void popup_error_message ()
 {
   GtkWidget *dialog;
 
-  dialog = gtk_message_dialog_new (NULL, GTK_DIALOG_NO_SEPARATOR, GTK_MESSAGE_WARNING,
-				   GTK_BUTTONS_OK, _("Unable to start the settings manager 'gnome2-settings-daemon'.\n"
+  dialog = gtk_message_dialog_new (NULL, GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_WARNING,
+				   GTK_BUTTONS_OK, _("Unable to start the settings manager 'gnome-settings-daemon'.\n"
 				   "Without the GNOME settings manager running, some preferences may not take effect. This could "
 				   "indicate a problem with Bonobo, or a non-GNOME (e.g. KDE) settings manager may already "
 				   "be active and conflicting with the GNOME settings manager."));
