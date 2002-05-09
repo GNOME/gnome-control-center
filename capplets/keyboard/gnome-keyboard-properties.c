263bc20dd57159b0b5aaec74ebb6baff799e015c
Jody Goldberg <jody@gnome.org>
2002-05-09 17:57:50 +0000

    support help. add help.
    2002-05-09  Jody Goldberg <jody@gnome.org>
    
    	* gnome-keyboard-properties.c (dialog_response) : support help.
    	* gnome-keyboard-properties.glade : add help.

--- capplets/keyboard/gnome-keyboard-properties.c
+++ capplets/keyboard/gnome-keyboard-properties.c
@@ -223,14 +223,20 @@ dialog_response (GtkWidget *widget,
 		 gint       response_id,
 		 GConfChangeSet *changeset)
 {
-	switch (response_id)
-	{
-	case RESPONSE_CLOSE:
-	case GTK_RESPONSE_DELETE_EVENT:
-	default:
+	if (response_id == GTK_RESPONSE_HELP) {
+		GError *error = NULL;
+
+		/* TODO : get this written */
+		gnome_help_display_desktop (NULL,
+			"control-center-manual",
+			"config-keyboard.xml",
+			"CONFIGURATION", &error);
+		if (error) {
+			g_warning ("help error: %s\n", error->message);
+			g_error_free (error);
+		}
+	} else
 		gtk_main_quit ();
-		break;
-	}
 }
 
 static void
