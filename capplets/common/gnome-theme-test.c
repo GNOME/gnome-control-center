f5bd46f3a6fbdec9a8d6f00d91b0156f3c12c50b
Jonathan Blandford <jrb@redhat.com>
2003-01-16 20:51:24 +0000

    new little test program for helping people debug their installation.
    Thu Jan 16 15:51:33 2003  Jonathan Blandford  <jrb@redhat.com>
    
    	* gnome-theme-test.c: new little test program for helping people
    	debug their installation.

---
+++ capplets/common/gnome-theme-test.c
@@ -0,0 +1,114 @@
+#include <gtk/gtk.h>
+#include <libgnomevfs/gnome-vfs-init.h>
+#include <libgnomevfs/gnome-vfs-ops.h>
+#include <libgnomevfs/gnome-vfs-utils.h>
+#include <libgnomevfs/gnome-vfs-directory.h>
+#include <string.h>
+#include <libgnome/gnome-desktop-item.h>
+#include "gnome-theme-info.h"
+
+int
+main (int argc, char *argv[])
+{
+  gtk_init (&argc, &argv);
+  gnome_vfs_init ();
+  gboolean monitor_not_added = FALSE;
+  GList *themes, *list;
+
+  gnome_theme_init (&monitor_not_added);
+  
+  themes = gnome_theme_meta_info_find_all ();
+  if (themes == NULL)
+    {
+      g_print ("No meta themes were found.\n");
+    }
+  else
+    {
+      g_print ("%d meta themes were found:\n", g_list_length (themes));
+      for (list = themes; list; list = list->next)
+	{
+	  GnomeThemeMetaInfo *meta_theme_info;
+	  
+	  meta_theme_info = list->data;
+	  g_print ("\t%s\n", meta_theme_info->readable_name);
+	}
+    }
+  g_list_free (themes);
+
+  themes = gnome_theme_icon_info_find_all ();
+  if (themes == NULL)
+    {
+      g_print ("No icon themes were found.\n");
+    }
+  else
+    {
+      g_print ("%d icon themes were found:\n", g_list_length (themes));
+      for (list = themes; list; list = list->next)
+	{
+	  GnomeThemeIconInfo *icon_theme_info;
+	  
+	  icon_theme_info = list->data;
+	  g_print ("\t%s\n", icon_theme_info->name);
+	}
+    }
+  g_list_free (themes);
+
+  themes = gnome_theme_info_find_by_type (GNOME_THEME_METACITY);
+  if (themes == NULL)
+    {
+      g_print ("No metacity themes were found.\n");
+    }
+  else
+    {
+      g_print ("%d metacity themes were found:\n", g_list_length (themes));
+      for (list = themes; list; list = list->next)
+	{
+	  GnomeThemeInfo *theme_info;
+	  
+	  theme_info = list->data;
+	  g_print ("\t%s\n", theme_info->name);
+	}
+    }
+  g_list_free (themes);
+
+  themes = gnome_theme_info_find_by_type (GNOME_THEME_GTK_2);
+  if (themes == NULL)
+    {
+      g_print ("No gtk-2 themes were found.\n");
+    }
+  else
+    {
+      g_print ("%d gtk-2 themes were found:\n", g_list_length (themes));
+      for (list = themes; list; list = list->next)
+	{
+	  GnomeThemeInfo *theme_info;
+	  
+	  theme_info = list->data;
+	  g_print ("\t%s\n", theme_info->name);
+	}
+    }
+  g_list_free (themes);
+
+  themes = gnome_theme_info_find_by_type (GNOME_THEME_GTK_2_KEYBINDING);
+  if (themes == NULL)
+    {
+      g_print ("No keybinding themes were found.\n");
+    }
+  else
+    {
+      g_print ("%d keybinding themes were found:\n", g_list_length (themes));
+      for (list = themes; list; list = list->next)
+	{
+	  GnomeThemeInfo *theme_info;
+	  
+	  theme_info = list->data;
+	  g_print ("\t%s\n", theme_info->name);
+	}
+    }
+  g_list_free (themes);
+
+  gtk_main ();
+
+  return 0;
+}
+
