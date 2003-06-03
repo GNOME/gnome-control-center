4cd7796a0aa8104db10776c3e66dee830a45eed3
Frédéric Crozat <fcrozat@src.gnome.org>
2003-06-03 16:42:01 +0000

    Quiet cvs.
            * .cvsignore:
            * capplets/common/.cvsignore:
            * schemas/.cvsignore:
            * vfs-methods/.cvsignore: Quiet cvs.
    
            * capplets/common/Makefile.am: fix parallel build.
    
            * capplets/common/gnome-theme-apply.c: add missing header.

--- capplets/common/gnome-theme-apply.c
+++ capplets/common/gnome-theme-apply.c
@@ -17,6 +17,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+#include <string.h>
 #include <gconf/gconf-client.h>
 #include <gnome-wm-manager.h>
 #include "gnome-theme-apply.h"
