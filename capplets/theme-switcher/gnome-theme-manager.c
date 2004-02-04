4b2fc55b018b2462827afe1a1b00ba8d797dabeb
Jonathan Blandford <jrb@gnome.org>
2004-02-04 05:21:28 +0000

    exit if it is a non-compliant wm (eg, anything but metacity)
    Wed Feb  4 00:05:11 2004  Jonathan Blandford  <jrb@gnome.org>
    
            * gnome-theme-manager.c (load_meta_themes): exit if it is a
            non-compliant wm (eg, anything but metacity)

--- capplets/theme-switcher/gnome-theme-manager.c
+++ capplets/theme-switcher/gnome-theme-manager.c
@@ -289,15 +289,23 @@ load_meta_themes (GtkTreeView *tree_view,
   current_icon_theme = gconf_client_get_string (client, ICON_THEME_KEY, NULL);
   window_manager = gnome_wm_manager_get_current (gdk_display_get_default_screen (gdk_display_get_default ()));
   wm_settings.flags = GNOME_WM_SETTING_THEME;
-  if (window_manager)
+
+  if (! window_manager)
     {
-      gnome_window_manager_get_settings (window_manager, &wm_settings);
-      current_window_theme = g_strdup (wm_settings.theme);
+      GtkWidget *dialog;
+
+      dialog = gtk_message_dialog_new (NULL,
+				       GTK_DIALOG_MODAL,
+				       GTK_MESSAGE_ERROR,
+				       GTK_BUTTONS_OK,
+				       _("The gnome-theme-manager does not know how to change the themes on the current window manager."));
+      gtk_dialog_run (GTK_DIALOG (dialog));
+      gtk_widget_destroy (dialog);
+      exit (0);
     }
-  else
-    {
-      current_window_theme = g_strdup (window_theme_default_name);
-    }
+
+  gnome_window_manager_get_settings (window_manager, &wm_settings);
+  current_window_theme = g_strdup (wm_settings.theme);
 
   /* FIXME: What do we really do when there is no theme? */
   if (current_icon_theme == NULL)
