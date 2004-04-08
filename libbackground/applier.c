12350223af3db3b09d4079df7ce3d32bf49bc6ba
Jody Goldberg <jody@gnome.org>
2004-04-08 17:13:42 +0000

    disable translation until after gnome-2-6 branch
    2004-04-08  Jody Goldberg <jody@gnome.org>
    
    	* libbackground/applier.c (egg_pixbuf_new_from_file_at_size) :
    	  disable translation until after gnome-2-6 branch

--- libbackground/applier.c
+++ libbackground/applier.c
@@ -574,7 +574,8 @@ egg_pixbuf_new_from_file_at_size (const char *filename,
                 g_set_error (error,
                              G_FILE_ERROR,
                              g_file_error_from_errno (errno),
-                             _("Failed to open file '%s': %s"),
+#warning store translation here after gnome-2-6 branches
+                             "Failed to open file '%s': %s",
                              filename, g_strerror (errno));
 		return NULL;
         }
@@ -612,7 +613,8 @@ egg_pixbuf_new_from_file_at_size (const char *filename,
 		g_set_error (error,
                              GDK_PIXBUF_ERROR,
                              GDK_PIXBUF_ERROR_FAILED,
-                             _("Failed to load image '%s': reason not known, probably a corrupt image file"),
+#warning store translation here after gnome-2-6 branches
+                             "Failed to load image '%s': reason not known, probably a corrupt image file",
                              filename);
 		return NULL;
 	}
