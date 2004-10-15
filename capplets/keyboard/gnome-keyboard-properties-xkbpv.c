cafb0ad7485adb92c7ecd61c81e540b257d8995f
Sergey V. Udaltsov <svu@src.gnome.org>
2004-10-15 22:43:13 +0000

    Fixing #154358

--- capplets/keyboard/gnome-keyboard-properties-xkbpv.c
+++ capplets/keyboard/gnome-keyboard-properties-xkbpv.c
@@ -34,6 +34,8 @@
 #include "libkbdraw/keyboard-drawing.h"
 
 static GtkWidget * previewWindow = NULL;
+static gint        preview_origin_x = -1;
+static gint        preview_origin_y = -1;
 
 static gboolean click_on_X (GtkWidget *widget,
                             GdkEvent *event,
@@ -48,19 +50,26 @@ static void
 init_preview (GladeXML * dialog)
 {
   GtkWidget *kbdraw = keyboard_drawing_new ();
+  GtkWidget *capplet_dialog = glade_xml_get_widget(dialog, "keyboard_dialog");
 
   previewWindow = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_window_set_destroy_with_parent (GTK_WINDOW (previewWindow), TRUE);
-  gtk_window_set_keep_above(GTK_WINDOW (previewWindow), TRUE);
-  gtk_window_set_resizable(GTK_WINDOW (previewWindow), TRUE);
+  gtk_window_set_type_hint (GTK_WINDOW (previewWindow), GDK_WINDOW_TYPE_HINT_UTILITY);
+  gtk_window_set_transient_for (GTK_WINDOW(previewWindow), GTK_WINDOW(capplet_dialog));
+
   gtk_window_set_skip_pager_hint(GTK_WINDOW (previewWindow), TRUE);
   gtk_window_set_skip_taskbar_hint(GTK_WINDOW (previewWindow), TRUE);
+
+  gtk_window_set_resizable(GTK_WINDOW (previewWindow), TRUE);
   gtk_window_set_default_size(GTK_WINDOW (previewWindow), 500, 300);
+
   gtk_window_set_title(GTK_WINDOW (previewWindow), _("Keyboard layout preview"));
 
   keyboard_drawing_set_track_group (KEYBOARD_DRAWING (kbdraw), TRUE);
   keyboard_drawing_set_track_config (KEYBOARD_DRAWING (kbdraw), TRUE);
-// show levels 1 and 2 - hoping that no more levels are used
+
+  /* show levels 1 and 2 - hoping that no more levels are used */
+  
   keyboard_drawing_set_levels (KEYBOARD_DRAWING (kbdraw), 0, 1);
 
   gtk_container_add (GTK_CONTAINER (previewWindow), kbdraw);
@@ -78,8 +87,17 @@ preview_toggled (GladeXML * dialog, GtkWidget * button)
     init_preview (dialog);
 
   if (doShow)
-    gtk_widget_show_all (previewWindow);
+    {
+      gtk_widget_show_all (previewWindow);
+      if (preview_origin_x >= 0)
+         gtk_window_move (GTK_WINDOW (previewWindow), 
+			  preview_origin_x, preview_origin_y);
+    }
   else
     if (previewWindow != NULL)
-      gtk_widget_hide_all (previewWindow);
+      {
+        gtk_window_get_position (GTK_WINDOW (previewWindow), 
+				 &preview_origin_x, &preview_origin_y);
+        gtk_widget_hide_all (previewWindow);
+      }
 }
