aa28ce34fc73875c3da7a7b9ad143aa4037ca276
Sebastien Bacher <seb128@debian.org>
2005-05-22 16:50:20 +0000

    fix the build with gcc-2.95, patch from Jens Granseuer <jensgr@gmx.net>
    2005-05-22  Sebastien Bacher  <seb128@debian.org>
    
            * gnome-keyboard-properties-xkbot.c: (xkb_options_add_group):
            fix the build with gcc-2.95, patch from Jens Granseuer <jensgr@gmx.net>
            (Closes: #170159).

--- capplets/keyboard/gnome-keyboard-properties-xkbot.c
+++ capplets/keyboard/gnome-keyboard-properties-xkbot.c
@@ -242,21 +242,21 @@ xkb_options_add_group (const XklConfigItemPtr
                                          Bool allowMultipleSelection,
                                          GladeXML * dialog)
 {
-  GSList * expanders_list = g_object_get_data (G_OBJECT (dialog), EXPANDERS_PROP);
+  GtkWidget *expander, *align, *vbox;
 
-  current1stLevelId = configItem->name;
+  GSList * expanders_list = g_object_get_data (G_OBJECT (dialog), EXPANDERS_PROP);
 
   gchar *utfGroupName = xci_desc_to_utf8 (configItem);
   gchar *titlemarkup = g_strconcat ("<span>", utfGroupName, "</span>", NULL);
 
-  GtkWidget *expander = gtk_expander_new (titlemarkup);
+  expander = gtk_expander_new (titlemarkup);
   g_object_set_data_full (G_OBJECT (expander), "utfGroupName", utfGroupName, g_free);
 
   g_free (titlemarkup);
   gtk_expander_set_use_markup (GTK_EXPANDER (expander), TRUE);
-  GtkWidget *align = gtk_alignment_new (0, 0, 1, 1);
+  align = gtk_alignment_new (0, 0, 1, 1);
   gtk_alignment_set_padding (GTK_ALIGNMENT (align), 6, 12, 12, 0);
-  GtkWidget *vbox = gtk_vbox_new (TRUE, 6);
+  vbox = gtk_vbox_new (TRUE, 6);
   gtk_container_add (GTK_CONTAINER (align), vbox);
   gtk_container_add (GTK_CONTAINER (expander), align);
   currentVbox = vbox;
@@ -264,6 +264,7 @@ xkb_options_add_group (const XklConfigItemPtr
   currentMultiSelect = (gboolean) allowMultipleSelection;
   currentRadioGroup = NULL;
 
+  current1stLevelId = configItem->name;
   XklConfigEnumOptions (configItem->name, (ConfigItemProcessFunc)
                         xkb_options_add_option, dialog);
 
