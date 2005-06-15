aa9ffeb6eb1bae091d95b0aae8b92c06f2a5c40a
Diego González <dggonz@src.gnome.org>
2005-06-15 00:29:46 +0000

    Forgot to commit this file

--- capplets/about-me/gnome-about-me-password.h
+++ capplets/about-me/gnome-about-me-password.h
@@ -1,7 +1,9 @@
 #ifndef __GNOME_ABOUT_ME_PASSWORD_H__
 #define __GNOME_ABOUT_ME_PASSWORD_H__
 
+#include <gtk/gtkwindow.h>
+
 void
-gnome_about_me_password (void);
+gnome_about_me_password (GtkWindow *parent);
 
 #endif /* __GNOME_ABOUT_ME_PASSWORD_H__ */
