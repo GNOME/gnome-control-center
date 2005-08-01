c5d1a9b0b7705eebb5bb40e5bfd9e9db7262574e
Sebastien Bacher <seb128@debian.org>
2005-08-01 13:11:03 +0000

    don't crash if there is no cursor theme installed (Closes: #311599).
    2005-08-01  Sebastien Bacher  <seb128@debian.org>
    
            * gnome-mouse-properties.c: (cursor_theme_changed):
            don't crash if there is no cursor theme installed (Closes: #311599).

--- capplets/mouse/gnome-mouse-properties.c
+++ capplets/mouse/gnome-mouse-properties.c
@@ -472,9 +472,9 @@ cursor_theme_changed (GConfClient *client,
 	GtkTreeModel    * smodel    = gtk_tree_view_get_model(view);
 	GtkTreeModel    * model     = gtk_tree_model_sort_get_model(GTK_TREE_MODEL_SORT(smodel));
 	gchar           * theme     = gconf_client_get_string (client, CURSOR_THEME_KEY, NULL);
-	gtk_tree_model_get_iter_root(model, &iter);
 	gint              size      = gconf_client_get_int (client, CURSOR_SIZE_KEY, NULL);
-	gtk_tree_model_get_iter_root(model, &iter);
+
+	g_return_if_fail (gtk_tree_model_get_iter_first (model, &iter));
 
 	do {
 		gchar* theme_name;
