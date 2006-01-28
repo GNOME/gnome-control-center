8ca32d80d93ccfedd5f66098180a6bbf6911f436
Rodrigo Moya <rodrigo@novell.com>
2006-01-28 18:46:47 +0000

    Fixes part of #327749
    2006-01-28  Rodrigo Moya <rodrigo@novell.com>
    
    	Fixes part of #327749
    
    	* gnome-default-applications-properties.glade:
    	* gnome-da-capplet.c (web_combo_changed_cb, mail_combo_changed_cb,
    	terminal_combo_changed_cb, web_browser_update_combo_box,
    	mail_reader_update_combo_box, terminal_update_combo_box): don't set
    	entries' sensitivity, just disable input on them.

--- capplets/default-applications/gnome-da-capplet.c
+++ capplets/default-applications/gnome-da-capplet.c
@@ -248,7 +248,7 @@ web_combo_changed_cb (GtkComboBox *combo, GnomeDACapplet *capplet)
     gtk_widget_set_sensitive (capplet->new_win_radiobutton, has_net_remote);
     gtk_widget_set_sensitive (capplet->new_tab_radiobutton, has_net_remote);
 
-    gtk_widget_set_sensitive (capplet->web_browser_command_entry, is_custom_active);
+    gtk_entry_set_editable (GTK_ENTRY (capplet->web_browser_command_entry), is_custom_active);
     gtk_widget_set_sensitive (capplet->web_browser_command_label, is_custom_active);
     gtk_widget_set_sensitive (capplet->web_browser_terminal_checkbutton, is_custom_active);
 }
@@ -297,7 +297,7 @@ mail_combo_changed_cb (GtkComboBox *combo, GnomeDACapplet *capplet)
     gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (capplet->mail_reader_terminal_checkbutton),
 				  gconf_client_get_bool (capplet->gconf, DEFAULT_APPS_KEY_MAILER_NEEDS_TERM, NULL));
 
-    gtk_widget_set_sensitive (capplet->mail_reader_command_entry, is_custom_active);
+    gtk_entry_set_editable (GTK_ENTRY (capplet->mail_reader_command_entry), is_custom_active);
     gtk_widget_set_sensitive (capplet->mail_reader_command_label, is_custom_active);
     gtk_widget_set_sensitive (capplet->mail_reader_terminal_checkbutton, is_custom_active);
 }
@@ -346,9 +346,9 @@ terminal_combo_changed_cb (GtkComboBox *combo, GnomeDACapplet *capplet)
     gtk_entry_set_text (GTK_ENTRY (capplet->terminal_exec_flag_entry),
 			gconf_client_get_string (capplet->gconf, DEFAULT_APPS_KEY_TERMINAL_EXEC_ARG, NULL));
 
-    gtk_widget_set_sensitive (capplet->terminal_command_entry, is_custom_active);
+    gtk_entry_set_editable (GTK_ENTRY (capplet->terminal_command_entry), is_custom_active);
     gtk_widget_set_sensitive (capplet->terminal_command_label, is_custom_active);
-    gtk_widget_set_sensitive (capplet->terminal_exec_flag_entry, is_custom_active);
+    gtk_entry_set_editable (GTK_ENTRY (capplet->terminal_exec_flag_entry), is_custom_active);
     gtk_widget_set_sensitive (capplet->terminal_exec_flag_label, is_custom_active);
 }
 
@@ -430,7 +430,7 @@ web_browser_update_combo_box (GnomeDACapplet *capplet, const gchar *command)
     gtk_widget_set_sensitive (capplet->default_radiobutton, has_net_remote);
     gtk_widget_set_sensitive (capplet->new_win_radiobutton, has_net_remote);
     gtk_widget_set_sensitive (capplet->new_tab_radiobutton, has_net_remote);
-    gtk_widget_set_sensitive (capplet->web_browser_command_entry, is_custom_active);
+    gtk_entry_set_editable (GTK_ENTRY (capplet->web_browser_command_entry), is_custom_active);
     gtk_widget_set_sensitive (capplet->web_browser_command_label, is_custom_active);
     gtk_widget_set_sensitive (capplet->web_browser_terminal_checkbutton, is_custom_active);
 
@@ -459,7 +459,7 @@ mail_reader_update_combo_box (GnomeDACapplet *capplet, const gchar *command)
 
     gtk_entry_set_text (GTK_ENTRY (capplet->mail_reader_command_entry), command);
 
-    gtk_widget_set_sensitive (capplet->mail_reader_command_entry, is_custom_active);
+    gtk_entry_set_editable (GTK_ENTRY (capplet->mail_reader_command_entry), is_custom_active);
     gtk_widget_set_sensitive (capplet->mail_reader_command_label, is_custom_active);
     gtk_widget_set_sensitive (capplet->mail_reader_terminal_checkbutton, is_custom_active);
 
@@ -488,9 +488,9 @@ terminal_update_combo_box (GnomeDACapplet *capplet, const gchar *command)
 
     gtk_entry_set_text (GTK_ENTRY (capplet->terminal_command_entry), command);
 
-    gtk_widget_set_sensitive (capplet->terminal_command_entry, is_custom_active);
+    gtk_entry_set_editable (GTK_ENTRY (capplet->terminal_command_entry), is_custom_active);
     gtk_widget_set_sensitive (capplet->terminal_command_label, is_custom_active);
-    gtk_widget_set_sensitive (capplet->terminal_exec_flag_entry, is_custom_active);
+    gtk_entry_set_editable (GTK_ENTRY (capplet->terminal_exec_flag_entry), is_custom_active);
     gtk_widget_set_sensitive (capplet->terminal_exec_flag_label, is_custom_active);
 
     if (gtk_combo_box_get_active (GTK_COMBO_BOX (capplet->term_combo_box)) != index)
