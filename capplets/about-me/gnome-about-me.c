4b54253c4af591e6a9c60155d9a54a3bcb04f55c
Sebastien Bacher <sbacher@src.gnome.org>
2006-08-06 22:45:09 +0000

    fix build with the new libebook API (Closes: #348821)

--- capplets/about-me/gnome-about-me.c
+++ capplets/about-me/gnome-about-me.c
@@ -417,9 +417,9 @@ about_me_load_photo (GnomeAboutMe *me, EContact *contact)
 
 	photo = e_contact_get (contact, E_CONTACT_PHOTO);
 
-	if (photo) {
+	if (photo && photo->type == E_CONTACT_PHOTO_TYPE_INLINED) {
 		me->have_image = TRUE;
-		e_image_chooser_set_image_data (E_IMAGE_CHOOSER (widget), photo->data, photo->length);
+		e_image_chooser_set_image_data (E_IMAGE_CHOOSER (widget), photo->data.inlined.data, photo->data.inlined.length);
 		e_contact_photo_free (photo);
 	} else {
 		me->have_image = FALSE;
@@ -490,15 +490,16 @@ about_me_update_photo (GnomeAboutMe *me)
 		}
 
 		photo = g_new0 (EContactPhoto, 1);
-		photo->data = data;
-		photo->length = length;
+		photo->type = E_CONTACT_PHOTO_TYPE_INLINED;
+		photo->data.inlined.data = data;
+		photo->data.inlined.length = length;
 		e_contact_set (me->contact, E_CONTACT_PHOTO, photo);
 
 		/* Save the image for GDM */
 		/* FIXME: I would have to read the default used by the gdmgreeter program */
 		file = g_strdup_printf ("%s/.face", g_get_home_dir ());
 		fp = fopen (file, "wb");
-		fwrite (photo->data, 1, photo->length, fp);
+		fwrite (photo->data.inlined.data, 1, photo->data.inlined.length, fp);
 		fclose (fp);
 
 		g_free (file);	
