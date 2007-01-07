809b57b71b16b9a58e98f485e1b59ecff17d1967
Thomas Wood <thos@src.gnome.org>
2007-01-07 01:20:05 +0000

    Reverting earlier changes as it prevents the directory from building
    svn path=/trunk/; revision=7074

--- vfs-methods/themus/themus-theme-applier.c
+++ vfs-methods/themus/themus-theme-applier.c
@@ -31,13 +31,6 @@
 
 #define FONT_KEY           "/desktop/gnome/interface/font_name"
 
-static gchar **arguments = NULL;
-
-static const GOptionEntry options[] = {
-	{ G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_STRING_ARRAY, &arguments, NULL, N_("[FILE...]") },
-	{NULL}
-};
-
 int main (int argc, char **argv)
 {
 	GnomeVFSURI *uri;
@@ -47,18 +40,12 @@ int main (int argc, char **argv)
 	GtkWidget *font_dialog;
 	GtkWidget *font_sample;
 	gboolean apply_font = FALSE;
-	GOptionContext *context;
-
-	context = g_option_context_new(_("- GNOME Theme Applier"));
-	g_option_context_add_main_entries(context, options, NULL);
+	poptContext ctx;
+	gchar **args;
 
 	gtk_init (&argc, &argv);
-	program = gnome_program_init ("themus-theme-applier", "0.3.0", LIBGNOME_MODULE,
-				      argc, argv,
-				      GNOME_PARAM_GOPTION_CONTEXT, context,
-				      GNOME_PARAM_NONE);
-
-	if (!arguments) return 1;
+	program = gnome_program_init ("ThemeApplier", "0.3.0", LIBGNOME_MODULE,
+				      argc, argv, GNOME_PARAM_NONE);
 	
 	g_object_get (program, GNOME_PARAM_POPT_CONTEXT, &ctx, NULL);
 	args = (char**) poptGetArgs(ctx);
