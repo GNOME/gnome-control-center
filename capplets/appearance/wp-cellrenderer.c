ef3bb7e92b14fb8cfc6ce4376a4287f833ed6acc
Jens Granseuer <jensgr@gmx.net>
2007-07-28 11:38:06 +0000

    fix cell renderer state logic to not be always on
    2007-07-28  Jens Granseuer  <jensgr@gmx.net>
    
    	* wp-cellrenderer.c: (cell_renderer_wallpaper_render):
    	* wp-cellrenderer.h: fix cell renderer state logic to not be always on
    
    svn path=/trunk/; revision=7909

--- capplets/appearance/wp-cellrenderer.c
+++ capplets/appearance/wp-cellrenderer.c
@@ -1,8 +1,24 @@
-
+/*
+ * Copyright (C) 2007 The GNOME Foundation
+ * Written by Denis Washington <denisw@svn.gnome.org>
+ *            Jens Granseuer <jensgr@gmx.net>
+ * All Rights Reserved
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 #include "wp-cellrenderer.h"
-#include <glib.h>
-#include <gdk/gdk.h>
-#include <gtk/gtk.h>
 
 static void cell_renderer_wallpaper_render (GtkCellRenderer *cell,
                                             GdkWindow *window,
@@ -18,7 +34,7 @@ static void
 cell_renderer_wallpaper_class_init (CellRendererWallpaperClass *klass)
 {
   GtkCellRendererClass *renderer_class;
-  
+
   renderer_class = (GtkCellRendererClass *) klass;
   renderer_class->render = cell_renderer_wallpaper_render;
 }
@@ -46,17 +62,27 @@ cell_renderer_wallpaper_render (GtkCellRenderer *cell,
 {
   (* GTK_CELL_RENDERER_CLASS (cell_renderer_wallpaper_parent_class)->render)
       (cell, window, widget, background_area, cell_area, expose_area, flags);
-  
-  if (flags && GTK_CELL_RENDERER_FOCUSED)
+
+  if ((flags & (GTK_CELL_RENDERER_SELECTED|GTK_CELL_RENDERER_PRELIT)) != 0)
   {
+    GtkStateType state;
     GdkGC *gc;
 
+    if ((flags & GTK_CELL_RENDERER_SELECTED) != 0)
+    {
+      if (GTK_WIDGET_HAS_FOCUS (widget))
+        state = GTK_STATE_SELECTED;
+      else
+        state = GTK_STATE_ACTIVE;
+    }
+    else
+      state = GTK_STATE_PRELIGHT;
+
     gc = gdk_gc_new (GDK_DRAWABLE (window));
-    
-    gdk_gc_set_foreground (gc, &widget->style->bg[GTK_STATE_SELECTED]);
+    gdk_gc_set_foreground (gc, &widget->style->bg[state]);
     gdk_gc_set_line_attributes (gc, 3,
                                 GDK_LINE_SOLID, GDK_CAP_BUTT, GDK_JOIN_MITER);
-    
+
     gdk_draw_rectangle (GDK_DRAWABLE (window), gc, FALSE,
                         background_area->x, background_area->y,
                         background_area->width, background_area->height);
