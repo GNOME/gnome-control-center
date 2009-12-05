42133f871f892e86f28ca8af5928042bbb1b59aa
Thomas Wood <thos@gnome.org>
2009-12-05 11:22:52 +0000

    Rename configure.in to configure.ac
    The ".ac" extension is the preferred use:
    
    http://www.gnu.org/software/autoconf/manual/html_node/Writing-Autoconf-Input.html

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="control-center"
 
-(test -f $srcdir/configure.in \
+(test -f $srcdir/configure.ac \
   && test -f $srcdir/autogen.sh \
   && test -d $srcdir/shell) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
