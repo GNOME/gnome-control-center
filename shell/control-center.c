2fb9e62d07c6420cc6cec9b003359cb81e2c0727
Thomas Wood <thomas.wood@intel.com>
2010-02-12 14:44:33 +0000

    Include "NoDisplay" items in the shell view
    This allows the items to be hidden by default in menus.

--- shell/control-center.c
+++ shell/control-center.c
@@ -281,7 +281,8 @@ fill_model (ShellData *data)
 
   vbox = W (data->builder, "main-vbox");
 
-  tree = gmenu_tree_lookup (MENUDIR "/gnomecc.menu", 0);
+  tree = gmenu_tree_lookup (MENUDIR "/gnomecc.menu",
+                            GMENU_TREE_FLAGS_INCLUDE_NODSIPLAY);
 
   if (!tree)
     {
