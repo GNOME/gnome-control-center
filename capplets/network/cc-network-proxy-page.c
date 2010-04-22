f25f9353c5a315cb1e58d1666358d764298b1e8f
Thomas Wood <thomas.wood@intel.com>
2010-04-22 10:51:12 +0100

    network: Move the "Delete Location" button to the top of the panel
    In single window mode, it makes more sense for the Delete Location button
    to be next to the list of locations.

--- capplets/network/cc-network-proxy-page.c
+++ capplets/network/cc-network-proxy-page.c
@@ -1303,9 +1303,6 @@ setup_page (CcNetworkProxyPage *page)
         mode_type = g_enum_register_static ("NetworkPreferencesProxyType",
                                             proxytype_values);
 
-        page->priv->delete_button = gtk_button_new_with_label (_("Delete Location"));
-        gtk_widget_show (page->priv->delete_button);
-
         /* Locations */
         page->priv->location_combobox = WID ("location_combobox");
         store = gtk_list_store_new (2,
@@ -1514,15 +1511,18 @@ setup_page (CcNetworkProxyPage *page)
 
 
         /* FIXME: move to .ui once it isn't shared */
-        hbox = gtk_hbox_new (FALSE, 6);
-        gtk_box_pack_end (GTK_BOX (hbox), page->priv->delete_button, FALSE, FALSE, 0);
+        hbox = WID ("hbox2");
+        page->priv->delete_button = gtk_button_new_with_label (_("Delete Location"));
+        gtk_widget_show (page->priv->delete_button);
+
+        gtk_box_pack_end (GTK_BOX (hbox), page->priv->delete_button, FALSE,
+                          FALSE, 0);
         g_signal_connect (page->priv->delete_button,
                           "clicked",
                           G_CALLBACK (on_delete_button_clicked),
                           page);
 
         widget = WID ("network_proxy_vbox");
-        gtk_box_pack_end (GTK_BOX (widget), hbox, FALSE, FALSE, 0);
 
         gtk_widget_reparent (widget, GTK_WIDGET (page));
         gtk_widget_show (widget);
