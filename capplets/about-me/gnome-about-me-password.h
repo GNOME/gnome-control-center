792e8cb20e1f6cabb335989a5b0f5b0475930454
Bastien Nocera <hadess@hadess.net>
2010-10-06 12:10:30 +0100

    capplets: Remove capplets, they're not built

--- capplets/about-me/gnome-about-me-password.h
+++
@@ -1,9 +0,0 @@
-#ifndef __GNOME_ABOUT_ME_PASSWORD_H__
-#define __GNOME_ABOUT_ME_PASSWORD_H__
-
-#include <gtk/gtk.h>
-
-void
-gnome_about_me_password (GtkWindow *parent);
-
-#endif /* __GNOME_ABOUT_ME_PASSWORD_H__ */
