ed9942b8c3b526e55559b25adc9b048fe9c0e2ab
Richard Hughes <richard@hughsie.com>
2010-12-10 11:32:57 +0000

    Actually load the proxy panel if NetworkManager is not available, rather than looping for infinity

--- panels/proxy/cc-proxy-panel.c
+++ panels/proxy/cc-proxy-panel.c
@@ -122,6 +122,6 @@ cc_proxy_panel_register (GIOModule *module)
   cc_proxy_panel_register_type (G_TYPE_MODULE (module));
   g_io_extension_point_implement (CC_SHELL_PANEL_EXTENSION_POINT,
                                   CC_TYPE_PROXY_PANEL,
-                                  "network", 0);
+                                  "proxy", 0);
 }
 
