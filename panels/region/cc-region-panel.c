cc172a475441b2332e11fa9553d8a25ec220ff7c
Bastien Nocera <hadess@hadess.net>
2011-01-24 17:25:12 +0000

    region: Remove unused finalize function

--- panels/region/cc-region-panel.c
+++ panels/region/cc-region-panel.c
@@ -73,12 +73,6 @@ cc_region_panel_dispose (GObject * object)
 }
 
 static void
-cc_region_panel_finalize (GObject * object)
-{
-	G_OBJECT_CLASS (cc_region_panel_parent_class)->finalize (object);
-}
-
-static void
 cc_region_panel_class_init (CcRegionPanelClass * klass)
 {
 	GObjectClass *object_class = G_OBJECT_CLASS (klass);
@@ -88,7 +82,6 @@ cc_region_panel_class_init (CcRegionPanelClass * klass)
 	object_class->get_property = cc_region_panel_get_property;
 	object_class->set_property = cc_region_panel_set_property;
 	object_class->dispose = cc_region_panel_dispose;
-	object_class->finalize = cc_region_panel_finalize;
 }
 
 static void
