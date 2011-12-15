d0db3c96e267377815302a79b9ce149395d1d216
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2011-12-15 12:16:37 +0100

    gvc-level-bar: fix shadow global declaration warnings
    Those variables are already defined in some headers (like bits/math-finite.h)
    making gcc unhappy.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=666240

--- panels/sound/gvc-level-bar.c
+++ panels/sound/gvc-level-bar.c
@@ -518,17 +518,17 @@ gvc_level_bar_size_allocate (GtkWidget     *widget,
 
 static void
 curved_rectangle (cairo_t *cr,
-                  double   x0,
-                  double   y0,
+                  double   _x0,
+                  double   _y0,
                   double   width,
                   double   height,
                   double   radius)
 {
         double x1;
-        double y1;
+        double _y1;
 
-        x1 = x0 + width;
-        y1 = y0 + height;
+        x1 = _x0 + width;
+        _y1 = _y0 + height;
 
         if (!width || !height) {
                 return;
@@ -536,37 +536,37 @@ curved_rectangle (cairo_t *cr,
 
         if (width / 2 < radius) {
                 if (height / 2 < radius) {
-                        cairo_move_to  (cr, x0, (y0 + y1) / 2);
-                        cairo_curve_to (cr, x0 ,y0, x0, y0, (x0 + x1) / 2, y0);
-                        cairo_curve_to (cr, x1, y0, x1, y0, x1, (y0 + y1) / 2);
-                        cairo_curve_to (cr, x1, y1, x1, y1, (x1 + x0) / 2, y1);
-                        cairo_curve_to (cr, x0, y1, x0, y1, x0, (y0 + y1) / 2);
+                        cairo_move_to  (cr, _x0, (_y0 + _y1) / 2);
+                        cairo_curve_to (cr, _x0 ,_y0, _x0, _y0, (_x0 + x1) / 2, _y0);
+                        cairo_curve_to (cr, x1, _y0, x1, _y0, x1, (_y0 + _y1) / 2);
+                        cairo_curve_to (cr, x1, _y1, x1, _y1, (x1 + _x0) / 2, _y1);
+                        cairo_curve_to (cr, _x0, _y1, _x0, _y1, _x0, (_y0 + _y1) / 2);
                 } else {
-                        cairo_move_to  (cr, x0, y0 + radius);
-                        cairo_curve_to (cr, x0, y0, x0, y0, (x0 + x1) / 2, y0);
-                        cairo_curve_to (cr, x1, y0, x1, y0, x1, y0 + radius);
-                        cairo_line_to (cr, x1, y1 - radius);
-                        cairo_curve_to (cr, x1, y1, x1, y1, (x1 + x0) / 2, y1);
-                        cairo_curve_to (cr, x0, y1, x0, y1, x0, y1 - radius);
+                        cairo_move_to  (cr, _x0, _y0 + radius);
+                        cairo_curve_to (cr, _x0, _y0, _x0, _y0, (_x0 + x1) / 2, _y0);
+                        cairo_curve_to (cr, x1, _y0, x1, _y0, x1, _y0 + radius);
+                        cairo_line_to (cr, x1, _y1 - radius);
+                        cairo_curve_to (cr, x1, _y1, x1, _y1, (x1 + _x0) / 2, _y1);
+                        cairo_curve_to (cr, _x0, _y1, _x0, _y1, _x0, _y1 - radius);
                 }
         } else {
                 if (height / 2 < radius) {
-                        cairo_move_to  (cr, x0, (y0 + y1) / 2);
-                        cairo_curve_to (cr, x0, y0, x0 , y0, x0 + radius, y0);
-                        cairo_line_to (cr, x1 - radius, y0);
-                        cairo_curve_to (cr, x1, y0, x1, y0, x1, (y0 + y1) / 2);
-                        cairo_curve_to (cr, x1, y1, x1, y1, x1 - radius, y1);
-                        cairo_line_to (cr, x0 + radius, y1);
-                        cairo_curve_to (cr, x0, y1, x0, y1, x0, (y0 + y1) / 2);
+                        cairo_move_to  (cr, _x0, (_y0 + _y1) / 2);
+                        cairo_curve_to (cr, _x0, _y0, _x0 , _y0, _x0 + radius, _y0);
+                        cairo_line_to (cr, x1 - radius, _y0);
+                        cairo_curve_to (cr, x1, _y0, x1, _y0, x1, (_y0 + _y1) / 2);
+                        cairo_curve_to (cr, x1, _y1, x1, _y1, x1 - radius, _y1);
+                        cairo_line_to (cr, _x0 + radius, _y1);
+                        cairo_curve_to (cr, _x0, _y1, _x0, _y1, _x0, (_y0 + _y1) / 2);
                 } else {
-                        cairo_move_to  (cr, x0, y0 + radius);
-                        cairo_curve_to (cr, x0 , y0, x0 , y0, x0 + radius, y0);
-                        cairo_line_to (cr, x1 - radius, y0);
-                        cairo_curve_to (cr, x1, y0, x1, y0, x1, y0 + radius);
-                        cairo_line_to (cr, x1, y1 - radius);
-                        cairo_curve_to (cr, x1, y1, x1, y1, x1 - radius, y1);
-                        cairo_line_to (cr, x0 + radius, y1);
-                        cairo_curve_to (cr, x0, y1, x0, y1, x0, y1 - radius);
+                        cairo_move_to  (cr, _x0, _y0 + radius);
+                        cairo_curve_to (cr, _x0 , _y0, _x0 , _y0, _x0 + radius, _y0);
+                        cairo_line_to (cr, x1 - radius, _y0);
+                        cairo_curve_to (cr, x1, _y0, x1, _y0, x1, _y0 + radius);
+                        cairo_line_to (cr, x1, _y1 - radius);
+                        cairo_curve_to (cr, x1, _y1, x1, _y1, x1 - radius, _y1);
+                        cairo_line_to (cr, _x0 + radius, _y1);
+                        cairo_curve_to (cr, _x0, _y1, _x0, _y1, _x0, _y1 - radius);
                 }
         }
 
