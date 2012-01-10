a54c382377dc80f44eddaf0b3bdda95ae156b35f
Bastien Nocera <hadess@hadess.net>
2012-01-10 14:32:51 +0000

    wacom: Make test app fixed width

--- panels/wacom/test-wacom.c
+++ panels/wacom/test-wacom.c
@@ -1,6 +1,8 @@
 #include "cc-wacom-page.h"
 #include "gsd-wacom-device.h"
 
+#define FIXED_WIDTH 675
+
 static void
 add_page (GList *devices,
 	  GtkWidget *notebook)
@@ -51,6 +53,8 @@ int main (int argc, char **argv)
 	gtk_init (&argc, &argv);
 
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_resizable (GTK_WINDOW (window), FALSE);
+	gtk_widget_set_size_request (window, FIXED_WIDTH, -1);
 	g_signal_connect (G_OBJECT (window), "delete-event",
 			  G_CALLBACK (delete_event_cb), NULL);
 	notebook = gtk_notebook_new ();
