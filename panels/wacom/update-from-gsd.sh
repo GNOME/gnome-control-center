abbc0210e608356080d900afed23cde6620ed985
Bastien Nocera <hadess@hadess.net>
2012-01-25 19:11:52 +0000

    wacom: Add "update-from-gsd" target
    To update common files from gnome-settings-daemon

---
+++ panels/wacom/update-from-gsd.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+function die() {
+  echo $*
+  exit 1
+}
+
+if test -z "$DIR"; then
+   echo "Must set DIR"
+   exit 1
+fi
+
+if test -z "$FILES"; then
+   echo "Must set FILES"
+   exit 1
+fi
+
+for FILE in $FILES; do
+  if cmp -s $DIR/$FILE $FILE; then
+     echo "File $FILE is unchanged"
+  else
+     cp $DIR/$FILE $FILE || die "Could not move $DIR/$FILE to $FILE"
+     echo "Updated $FILE"
+     git add $FILE
+  fi
+done
