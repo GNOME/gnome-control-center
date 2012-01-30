decc5cf668067c2898cb90e65919442e78a3fbe5
Bastien Nocera <hadess@hadess.net>
2012-01-30 16:27:49 +0000

    wacom: Make .ui files load the images properly
    So it's easier to see what it looks like in glade.

--- panels/wacom/cc-wacom-stylus-page.c
+++ panels/wacom/cc-wacom-stylus-page.c
@@ -342,7 +342,7 @@ set_icon_name (CcWacomStylusPage *page,
 	priv = page->priv;
 
 	filename = g_strdup_printf ("%s.svg", icon_name);
-	path = g_build_filename (PIXMAP_DIR, filename, NULL);
+	path = g_build_filename (GNOMECC_UI_DIR, filename, NULL);
 	g_free (filename);
 
 	gtk_image_set_from_file (GTK_IMAGE (WID (widget_name)), path);
