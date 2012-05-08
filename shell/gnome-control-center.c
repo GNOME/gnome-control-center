c3fa222461df600a863fa5411566a98a7db6cb3b
Bastien Nocera <hadess@hadess.net>
2012-05-08 18:53:04 +0100

    Revert "shell: Reset "small_screen_set" when we use a big screen"
    This reverts commit 3e5f488160ff2ba72d6871a8b9848fb1206da3c8.

--- shell/gnome-control-center.c
+++ shell/gnome-control-center.c
@@ -1167,10 +1167,8 @@ update_small_screen_settings (GnomeControlCenter *self)
       gtk_scrolled_window_set_min_content_height (GTK_SCROLLED_WINDOW (self->priv->scrolled_window), SMALL_SCREEN_FIXED_HEIGHT);
 
       if (!self->priv->small_screen_set
-          && self->priv->small_screen != small) {
+          && self->priv->small_screen != small)
         gtk_window_maximize (GTK_WINDOW (self->priv->window));
-        self->priv->small_screen_set = TRUE;
-      }
     }
   else
     {
@@ -1179,10 +1177,10 @@ update_small_screen_settings (GnomeControlCenter *self)
         gtk_window_unmaximize (GTK_WINDOW (self->priv->window));
 
       gtk_window_set_resizable (GTK_WINDOW (self->priv->window), FALSE);
-      self->priv->small_screen_set = FALSE;
     }
 
   self->priv->small_screen = small;
+  self->priv->small_screen_set = TRUE;
 }
 
 static gboolean
