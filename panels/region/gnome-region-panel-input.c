9d46f8393c0074cbc65c1c4b62e3996fd5b6b255
Rui Matos <tiagomatos@gmail.com>
2012-09-04 14:43:42 +0200

    region: Expand the supported IBus engines whitelist
    Thanks to Daiki Ueno for assembling the list.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=682313

--- panels/region/gnome-region-panel-input.c
+++ panels/region/gnome-region-panel-input.c
@@ -65,11 +65,139 @@ static GCancellable *ibus_cancellable = NULL;
 static guint shell_name_watch_id = 0;
 
 static const gchar *supported_ibus_engines[] = {
-  "bopomofo",
+  /* Simplified Chinese */
   "pinyin",
+  "bopomofo",
+  /* Default in Fedora, where ibus-libpinyin replaces ibus-pinyin */
+  "libpinyin",
+  "libbopomofo",
+
+  /* Traditional Chinese */
+  /* https://bugzilla.gnome.org/show_bug.cgi?id=680840 */
   "chewing",
+  "cangjie5",
+  "cangjie3",
+  "quick5",
+  "quick3",
+  "stroke5",
+
+  /* Japanese */
   "anthy",
+  "mozc-jp",
+  "skk",
+
+  /* Korean */
   "hangul",
+
+  /* Thai */
+  "m17n:th:kesmanee",
+  "m17n:th:pattachote",
+  "m17n:th:tis820",
+
+  /* Vietnamese (confirmation needed, maybe add more) */
+  "m17n:vi:tcvn",
+  "m17n:vi:telex",
+  "m17n:vi:viqr",
+  "m17n:vi:vni",
+  "Unikey",
+
+  /* Sinhala */
+  "m17n:si:wijesekera",
+  "m17n:si:phonetic-dynamic",
+  "m17n:si:trans",
+  "sayura",
+
+  /* Farsi (confirmation needed) */
+  "m17n:fa:isiri",
+
+  /* Amharic (confirmation needed) */
+  "m17n:am:sera",
+
+  /* Indic */
+  /* https://fedoraproject.org/wiki/I18N/Indic#Keyboard_Layouts */
+
+  /* Assamese */
+  "m17n:as:phonetic",
+  "m17n:as:inscript",
+  "m17n:as:itrans",
+
+  /* Bengali */
+  "m17n:bn:inscript",
+  "m17n:bn:itrans",
+  "m17n:bn:probhat",
+
+  /* Gujarati */
+  "m17n:gu:inscript",
+  "m17n:gu:itrans",
+  "m17n:gu:phonetic",
+
+  /* Hindi */
+  "m17n:hi:inscript",
+  "m17n:hi:itrans",
+  "m17n:hi:phonetic",
+  "m17n:hi:remington",
+  "m17n:hi:typewriter",
+  "m17n:hi:vedmata",
+
+  /* Kannada */
+  "m17n:kn:kgp",
+  "m17n:kn:inscript",
+  "m17n:kn:itrans",
+
+  /* Kashmiri */
+  "m17n:ks:inscript",
+
+  /* Maithili */
+  "m17n:mai:inscript",
+
+  /* Malayalam */
+  "m17n:ml:inscript",
+  "m17n:ml:itrans",
+  "m17n:ml:mozhi",
+  "m17n:ml:swanalekha",
+
+  /* Marathi */
+  "m17n:mr:inscript",
+  "m17n:mr:itrans",
+
+  /* Nepali */
+  "m17n:ne:rom",
+  "m17n:ne:trad",
+
+  /* Oriya */
+  "m17n:or:inscript",
+  "m17n:or:itrans",
+  "m17n:or:phonetic",
+
+  /* Punjabi */
+  "m17n:pa:inscript",
+  "m17n:pa:itrans",
+  "m17n:pa:phonetic",
+  "m17n:pa:jhelum",
+
+  /* Sanskrit */
+  "m17n:sa:harvard-kyoto",
+
+  /* Sindhi */
+  "m17n:sd:inscript",
+
+  /* Tamil */
+  "m17n:ta:tamil99",
+  "m17n:ta:inscript",
+  "m17n:ta:itrans",
+  "m17n:ta:phonetic",
+  "m17n:ta:lk-renganathan",
+  "m17n:ta:vutam",
+  "m17n:ta:typewriter",
+
+  /* Telugu */
+  "m17n:te:inscript",
+  "m17n:te:apple",
+  "m17n:te:pothana",
+  "m17n:te:rts",
+
+  /* Urdu */
+  "m17n:ur:phonetic",
   NULL
 };
 #endif  /* HAVE_IBUS */
