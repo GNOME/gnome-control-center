b8d5bb1a4a76a7c0d105d4408b2ef2e1ab2d2275
Matthias Clasen <mclasen@redhat.com>
2012-11-17 10:23:36 -0500

    Add a privacy panel
    This adds a panel where we present information and controls
    affecting the users privacy. This initial commit just puts
    the framework in place, the panel itself is empty.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=687774

---
+++ panels/privacy/cc-privacy-panel.h
@@ -0,0 +1,73 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*-
+ *
+ * Copyright (C) 2012 Red Hat, Inc
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * Author: Matthias Clasen <mclasen@redhat.com>
+ */
+
+#ifndef _CC_PRIVACY_PANEL_H
+#define _CC_PRIVACY_PANEL_H
+
+#include <shell/cc-panel.h>
+
+G_BEGIN_DECLS
+
+#define CC_TYPE_PRIVACY_PANEL cc_privacy_panel_get_type()
+
+#define CC_PRIVACY_PANEL(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
+  CC_TYPE_PRIVACY_PANEL, CcPrivacyPanel))
+
+#define CC_PRIVACY_PANEL_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST ((klass), \
+  CC_TYPE_PRIVACY_PANEL, CcPrivacyPanelClass))
+
+#define CC_IS_PRIVACY_PANEL(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
+  CC_TYPE_PRIVACY_PANEL))
+
+#define CC_IS_PRIVACY_PANEL_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass), \
+  CC_TYPE_PRIVACY_PANEL))
+
+#define CC_PRIVACY_PANEL_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), \
+  CC_TYPE_PRIVACY_PANEL, CcPrivacyPanelClass))
+
+typedef struct _CcPrivacyPanel CcPrivacyPanel;
+typedef struct _CcPrivacyPanelClass CcPrivacyPanelClass;
+typedef struct _CcPrivacyPanelPrivate CcPrivacyPanelPrivate;
+
+struct _CcPrivacyPanel
+{
+  CcPanel parent;
+
+  CcPrivacyPanelPrivate *priv;
+};
+
+struct _CcPrivacyPanelClass
+{
+  CcPanelClass parent_class;
+};
+
+GType cc_privacy_panel_get_type (void) G_GNUC_CONST;
+
+void  cc_privacy_panel_register (GIOModule *module);
+
+G_END_DECLS
+
+#endif /* _CC_PRIVACY_PANEL_H */
