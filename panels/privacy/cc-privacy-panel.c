ace5a5e8bc4e906ce20415d85b928576a5ca763d
Matthias Clasen <mclasen@redhat.com>
2012-12-04 23:56:26 -0500

    privacy: Fix list sizing issues
    Make the list not expand all the way to the bottom.
    
    Revert when https://bugzilla.gnome.org/show_bug.cgi?id=689714
    is committed.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=689607

--- panels/privacy/cc-privacy-panel.c
+++ panels/privacy/cc-privacy-panel.c
@@ -172,23 +172,25 @@ add_row (CcPrivacyPanel *self,
 {
   GtkWidget *box, *w;
 
-  box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+  box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 50);
   g_object_set_data (G_OBJECT (box), "dialog-id", (gpointer)dialog_id);
   gtk_widget_set_hexpand (box, TRUE);
   gtk_container_add (GTK_CONTAINER (self->priv->list_box), box);
 
   w = gtk_label_new (label);
+  gtk_misc_set_alignment (GTK_MISC (w), 0.0f, 0.5f);
   gtk_widget_set_margin_left (w, 12);
   gtk_widget_set_margin_right (w, 12);
   gtk_widget_set_margin_top (w, 6);
   gtk_widget_set_margin_bottom (w, 6);
   gtk_widget_set_halign (w, GTK_ALIGN_START);
   gtk_widget_set_valign (w, GTK_ALIGN_CENTER);
-  gtk_container_add (GTK_CONTAINER (box), w);
+  gtk_widget_set_hexpand (w, TRUE);
+  gtk_box_pack_start (GTK_BOX (box), w, TRUE, TRUE, 0);
   gtk_widget_set_margin_left (status, 12);
   gtk_widget_set_margin_right (status, 12);
-  gtk_widget_set_halign (w, GTK_ALIGN_END);
-  gtk_widget_set_valign (w, GTK_ALIGN_CENTER);
+  gtk_widget_set_halign (status, GTK_ALIGN_END);
+  gtk_widget_set_valign (status, GTK_ALIGN_CENTER);
   gtk_box_pack_end (GTK_BOX (box), status, FALSE, FALSE, 0);
 
   gtk_widget_show_all (box);
