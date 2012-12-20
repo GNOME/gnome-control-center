4ce6d0a7ab7124f4ad6dae6def38ae7f231fdfa2
Bastien Nocera <hadess@hadess.net>
2012-12-20 22:48:49 +0100

    shell: Add bash completion file
    Only completes the panel names for now.

---
+++ shell/list-panel.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+LIST=""
+for i in $1/panels/*/gnome-*panel.desktop.in.in ; do
+	basename=`basename $i`
+	LIST="$LIST `echo $basename | sed 's/gnome-//' | sed 's/-panel.desktop.in.in/ /'`"
+done
+echo -n $LIST
