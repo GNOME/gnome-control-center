aeb8f31abe18e9e730d465a67d3e2786f9aafd4c
Bastien Nocera <hadess@hadess.net>
2013-01-09 18:18:37 +0100

    network: Write IPv6 instead of IP if IPv6 is the only address

--- panels/network/net-device-ethernet.c
+++ panels/network/net-device-ethernet.c
@@ -190,7 +190,7 @@ add_details (GtkWidget *details, NMDevice *device, NMConnection *connection)
         } else if (ip4_address) {
                 add_details_row (details, i++, _("IP Address"), ip4_address);
         } else if (ip6_address) {
-                add_details_row (details, i++, _("IP Address"), ip6_address);
+                add_details_row (details, i++, _("IPv6 Address"), ip6_address);
         }
 
         add_details_row (details, i++, _("Hardware Address"),
