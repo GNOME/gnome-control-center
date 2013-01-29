bcc1cc2a5ae3c5d30b6aa26fa1dea37b7263781f
Jeremy Bicha <jbicha@ubuntu.com>
2013-01-29 01:21:06 -0500

    mouse: Fix conflicting mnemonic
    'Test Your Settings' and 'Disable while typing' both were set to 't'
    
    https://bugzilla.gnome.org/show_bug.cgi?id=692767

--- panels/mouse/cc-mouse-panel.c
+++ panels/mouse/cc-mouse-panel.c
@@ -120,7 +120,7 @@ cc_mouse_panel_constructed (GObject *object)
   /* Add test area toggle to shell header. */
   box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 2);
 
-  button = gtk_toggle_button_new_with_mnemonic (_("_Test Your Settings"));
+  button = gtk_toggle_button_new_with_mnemonic (_("Test Your _Settings"));
   gtk_box_pack_start (GTK_BOX (box), button, FALSE, FALSE, 0);
   gtk_widget_set_visible (button, TRUE);
 
