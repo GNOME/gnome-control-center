d7dbcab34f6d822b8441e0dc55c221b8a7995a3a
Bastien Nocera <hadess@hadess.net>
2013-02-11 09:36:54 +0100

    shell: Add sound and user-accounts to the completion
    https://bugzilla.gnome.org/show_bug.cgi?id=693398

--- shell/list-panel.sh
+++ shell/list-panel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 LIST=""
-for i in $1/panels/*/gnome-*panel.desktop.in.in ; do
+for i in $1/panels/*/gnome-*panel.desktop.in.in $1/panels/*/data/gnome-*panel.desktop.in.in; do
 	basename=`basename $i`
 	LIST="$LIST `echo $basename | sed 's/gnome-//' | sed 's/-panel.desktop.in.in/ /'`"
 done
