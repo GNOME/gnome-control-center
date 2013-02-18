GNOME/gnome-control-center

commit 64467d7f0a4078ae918cbac04e45e6a93d9c517f

shell: Make the application a GtkApplication subclass
This promotes better encapsulation and allows us to move
application logic out of main() and rename the confusingly
named control-center.c to main.c

https://bugzilla.gnome.org/show_bug.cgi?id=692174

shell/cc-application.c
@@ -1,0 +1,346 @@
+/*
+ * Copyright © 2013 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#include "config.h"
+
+#include <stdlib.h>
+
+#include <glib.h>
+#include <glib/gi18n.h>
+#include <gio/gio.h>
+#include <libnotify/notify.h>
+#include <clutter-gtk/clutter-gtk.h>
+
+#include "cc-application.h"
+#include "cc-panel-loader.h"
+#include "cc-shell-log.h"
+#include "gnome-control-center.h"
+
+#ifdef HAVE_CHEESE
+#include <cheese-gtk.h>
+#endif /* HAVE_CHEESE */
+
+struct _CcApplicationPrivate
+{
+  GnomeControlCenter *window;
+};
+
+G_DEFINE_TYPE (CcApplication, cc_application, GTK_TYPE_APPLICATION)
+
+G_GNUC_NORETURN static gboolean
+option_version_cb (const gchar *option_name,
+                   const gchar *value,
+                   gpointer     data,
+                   GError     **error)
+{
+  g_print ("%s %s\n", PACKAGE, VERSION);
+  exit (0);
+}
+
+static char **start_panels = NULL;
+static char *search_str = NULL;
+static gboolean show_overview = FALSE;
+static gboolean verbose = FALSE;
+static gboolean show_help = FALSE;
+static gboolean show_help_gtk = FALSE;
+static gboolean show_help_all = FALSE;
+static gboolean list_panels = FALSE;
+
+const GOptionEntry all_options[] = {
+  { "version", 0, G_OPTION_FLAG_NO_ARG, G_OPTION_ARG_CALLBACK, option_version_cb, NULL, NULL },
+  { "verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose, N_("Enable verbose mode"), NULL },
+  { "overview", 'o', 0, G_OPTION_ARG_NONE, &show_overview, N_("Show the overview"), NULL },
+  { "search", 's', 0, G_OPTION_ARG_STRING, &search_str, N_("Search for the string"), "SEARCH" },
+  { "list", 'l', 0, G_OPTION_ARG_NONE, &list_panels, N_("List possible panel names and exit"), NULL },
+  { "help", 'h', G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_NONE, &show_help, N_("Show help options"), NULL },
+  { "help-all", 0, G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_NONE, &show_help_all, N_("Show help options"), NULL },
+  { "help-gtk", 0, G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_NONE, &show_help_gtk, N_("Show help options"), NULL },
+  { G_OPTION_REMAINING, '\0', 0, G_OPTION_ARG_FILENAME_ARRAY, &start_panels, N_("Panel to display"), N_("[PANEL] [ARGUMENT…]") },
+  { NULL, 0, 0, 0, NULL, NULL, NULL } /* end the list */
+};
+
+static void
+help_activated (GSimpleAction *action,
+                GVariant      *parameter,
+                gpointer       user_data)
+{
+  CcApplication *self = CC_APPLICATION (user_data);
+  CcPanel *panel;
+  GtkWidget *window;
+  const char *uri = NULL;
+
+  panel = cc_shell_get_active_panel (CC_SHELL (self->priv->window));
+  if (panel)
+    uri = cc_panel_get_help_uri (panel);
+
+  window = cc_shell_get_toplevel (CC_SHELL (self->priv->window));
+  gtk_show_uri (gtk_widget_get_screen (window),
+                uri ? uri : "help:gnome-help/prefs",
+                GDK_CURRENT_TIME, NULL);
+}
+
+static int
+cc_application_command_line (GApplication *application,
+                             GApplicationCommandLine *command_line)
+{
+  CcApplication *self = CC_APPLICATION (application);
+  int argc;
+  char **argv;
+  int retval = 0;
+  GOptionContext *context;
+  GError *error = NULL;
+
+  verbose = FALSE;
+  show_overview = FALSE;
+  show_help = FALSE;
+  start_panels = NULL;
+
+  argv = g_application_command_line_get_arguments (command_line, &argc);
+
+  context = g_option_context_new (N_("- Settings"));
+  g_option_context_add_main_entries (context, all_options, GETTEXT_PACKAGE);
+  g_option_context_set_translation_domain(context, GETTEXT_PACKAGE);
+  g_option_context_add_group (context, gtk_get_option_group (TRUE));
+  g_option_context_set_help_enabled (context, FALSE);
+
+  if (g_option_context_parse (context, &argc, &argv, &error) == FALSE)
+    {
+      g_print (_("%s\nRun '%s --help' to see a full list of available command line options.\n"),
+               error->message, argv[0]);
+      g_error_free (error);
+      g_option_context_free (context);
+      return 1;
+    }
+
+  if (show_help || show_help_all || show_help_gtk)
+    {
+      gchar *help;
+      GOptionGroup *group;
+
+      if (show_help || show_help_all)
+        group = NULL;
+      else
+        group = gtk_get_option_group (FALSE);
+
+      help = g_option_context_get_help (context, FALSE, group);
+      g_print ("%s", help);
+      g_free (help);
+      g_option_context_free (context);
+      return 0;
+    }
+
+  if (list_panels)
+    {
+      GList *panels, *l;
+
+      panels = cc_panel_loader_get_panels ();
+
+      g_print ("%s\n", _("Available panels:"));
+      for (l = panels; l != NULL; l = l->next)
+        g_print ("\t%s\n", (char *) l->data);
+
+      g_list_free (panels);
+
+      return 0;
+    }
+
+#ifdef HAVE_CHEESE
+  cheese_gtk_init (&argc, &argv);
+#endif /* HAVE_CHEESE */
+
+  cc_shell_log_set_debug (verbose);
+
+  gnome_control_center_show (self->priv->window, GTK_APPLICATION (application));
+
+  if (search_str)
+    {
+      gnome_control_center_set_search_item (self->priv->window, search_str);
+    }
+  else if (show_overview)
+    {
+      gnome_control_center_set_overview_page (self->priv->window);
+    }
+  else if (start_panels != NULL && start_panels[0] != NULL)
+    {
+      const char *start_id;
+      GError *err = NULL;
+
+      start_id = start_panels[0];
+
+      if (start_panels[1])
+        g_debug ("Extra argument: %s", start_panels[1]);
+      else
+        g_debug ("No extra argument");
+
+      if (!cc_shell_set_active_panel_from_id (CC_SHELL (self->priv->window), start_id, (const gchar**)start_panels+1, &err))
+        {
+          g_warning ("Could not load setting panel \"%s\": %s", start_id,
+                     (err) ? err->message : "Unknown error");
+          retval = 1;
+          if (err)
+            {
+              g_error_free (err);
+              err = NULL;
+            }
+        }
+    }
+
+  if (start_panels != NULL)
+    {
+      g_strfreev (start_panels);
+      start_panels = NULL;
+    }
+
+  show_overview = FALSE;
+
+  g_option_context_free (context);
+  g_strfreev (argv);
+
+  return retval;
+}
+
+static void
+cc_application_quit (GSimpleAction *simple,
+                     GVariant *parameter,
+                     gpointer user_data)
+{
+  CcApplication *self = CC_APPLICATION (user_data);
+
+  g_clear_object (&self->priv->window);
+}
+
+
+static void
+cc_application_activate (GApplication *application)
+{
+  CcApplication *self = CC_APPLICATION (application);
+
+  gnome_control_center_present (self->priv->window);
+}
+
+static void
+cc_application_startup (GApplication *application)
+{
+  CcApplication *self = CC_APPLICATION (application);
+  GMenu *menu;
+  GMenu *section;
+  GSimpleAction *action;
+
+  G_APPLICATION_CLASS (cc_application_parent_class)->startup (application);
+
+#ifdef HAVE_CHEESE
+  if (gtk_clutter_init (NULL, NULL) != CLUTTER_INIT_SUCCESS)
+    {
+      g_critical ("Unable to initialize Clutter");
+      return;
+    }
+#endif /* HAVE_CHEESE */
+
+  /* register a symbolic icon size for use in sidebar lists */
+  gtk_icon_size_register ("cc-sidebar-list", 24, 24);
+
+  notify_init ("gnome-control-center");
+
+  action = g_simple_action_new ("help", NULL);
+  g_action_map_add_action (G_ACTION_MAP (application), G_ACTION (action));
+  g_signal_connect (action, "activate", G_CALLBACK (help_activated), self);
+  g_object_unref (action);
+
+  action = g_simple_action_new ("quit", NULL);
+  g_action_map_add_action (G_ACTION_MAP (application), G_ACTION (action));
+  g_signal_connect (action, "activate", G_CALLBACK (cc_application_quit), self);
+  g_object_unref (action);
+
+  menu = g_menu_new ();
+
+  section = g_menu_new ();
+  g_menu_append (section, _("Help"), "app.help");
+  g_menu_append (section, _("Quit"), "app.quit");
+
+  g_menu_append_section (menu, NULL, G_MENU_MODEL (section));
+
+  gtk_application_set_app_menu (GTK_APPLICATION (application),
+                                G_MENU_MODEL (menu));
+
+  gtk_application_add_accelerator (GTK_APPLICATION (application),
+                                   "F1", "app.help", NULL);
+
+  self->priv->window = gnome_control_center_new ();
+}
+
+static GObject *
+cc_application_constructor (GType type,
+                            guint n_construct_params,
+                            GObjectConstructParam *construct_params)
+{
+  static GObject *self = NULL;
+
+  if (self == NULL)
+    {
+      self = G_OBJECT_CLASS (cc_application_parent_class)->constructor (type,
+                                                                        n_construct_params,
+                                                                        construct_params);
+      g_object_add_weak_pointer (self, (gpointer) &self);
+      return self;
+    }
+
+  return g_object_ref (self);
+}
+
+
+static void
+cc_application_dispose (GObject *object)
+{
+  G_OBJECT_CLASS (cc_application_parent_class)->dispose (object);
+}
+
+
+static void
+cc_application_init (CcApplication *self)
+{
+  self->priv = G_TYPE_INSTANCE_GET_PRIVATE (self,
+                                            CC_TYPE_APPLICATION,
+                                            CcApplicationPrivate);
+}
+
+
+static void
+cc_application_class_init (CcApplicationClass *class)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (class);
+  GApplicationClass *application_class = G_APPLICATION_CLASS (class);
+
+  object_class->constructor = cc_application_constructor;
+  object_class->dispose = cc_application_dispose;
+  application_class->activate = cc_application_activate;
+  application_class->startup = cc_application_startup;
+  application_class->command_line = cc_application_command_line;
+
+  g_type_class_add_private (class, sizeof (CcApplicationPrivate));
+}
+
+
+GtkApplication *
+cc_application_new (void)
+{
+  return g_object_new (CC_TYPE_APPLICATION,
+                       "application-id", "org.gnome.ControlCenter",
+                       "flags", G_APPLICATION_HANDLES_COMMAND_LINE,
+                       NULL);
+}

shell/Makefile.am
@@ -22,7 +22,9 @@ BUILT_SOURCES =			\
 
 gnome_control_center_SOURCES =			\
 	$(BUILT_SOURCES)			\
-	control-center.c			\
+	main.c					\
+	cc-application.c			\
+	cc-application.h			\
 	cc-shell-log.c				\
 	cc-shell-log.h				\
 	gnome-control-center.c			\

shell/cc-application.h
@@ -1,0 +1,70 @@
+/*
+ * Copyright © 2013 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
+#ifndef CC_APPLICATION_H
+#define CC_APPLICATION_H
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define CC_TYPE_APPLICATION (cc_application_get_type ())
+
+#define CC_APPLICATION(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
+   CC_TYPE_APPLICATION, CcApplication))
+
+#define CC_APPLICATION_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST ((klass), \
+   CC_TYPE_APPLICATION, CcApplicationClass))
+
+#define CC_IS_APPLICATION(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
+   CC_TYPE_APPLICATION))
+
+#define CC_IS_APPLICATION_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass), \
+   CC_TYPE_APPLICATION))
+
+#define CC_APPLICATION_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj), \
+   CC_TYPE_APPLICATION, CcApplicationClass))
+
+typedef struct _CcApplication        CcApplication;
+typedef struct _CcApplicationClass   CcApplicationClass;
+typedef struct _CcApplicationPrivate CcApplicationPrivate;
+
+struct _CcApplication
+{
+  GtkApplication parent_instance;
+  CcApplicationPrivate *priv;
+};
+
+struct _CcApplicationClass
+{
+  GtkApplicationClass parent_class;
+};
+
+GType                  cc_application_get_type               (void) G_GNUC_CONST;
+
+GtkApplication        *cc_application_new                    (void);
+
+G_END_DECLS
+
+#endif /* CC_APPLICATION_H */

shell/control-center.c
@@ -1,299 +1,0 @@
-/*
- * Copyright (c) 2009, 2010 Intel, Inc.
- * Copyright (c) 2010 Red Hat, Inc.
- *
- * The Control Center is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * The Control Center is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with the Control Center; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * Author: Thomas Wood <thos@gnome.org>
- */
-
-#include "config.h"
-
-#include <glib/gi18n.h>
-#include <stdlib.h>
-
-#include "gnome-control-center.h"
-#include "cc-panel-loader.h"
-
-#include <gtk/gtk.h>
-#include <string.h>
-#include <libnotify/notify.h>
-#include <egg-list-box.h>
-
-#ifdef GDK_WINDOWING_X11
-#include <X11/Xlib.h>
-#endif
-
-#ifdef HAVE_CHEESE
-#include <cheese-gtk.h>
-#endif /* HAVE_CHEESE */
-
-#include "cc-shell-log.h"
-
-G_GNUC_NORETURN static gboolean
-option_version_cb (const gchar *option_name,
-                   const gchar *value,
-                   gpointer     data,
-                   GError     **error)
-{
-  g_print ("%s %s\n", PACKAGE, VERSION);
-  exit (0);
-}
-
-static char **start_panels = NULL;
-static char *search_str = NULL;
-static gboolean show_overview = FALSE;
-static gboolean verbose = FALSE;
-static gboolean show_help = FALSE;
-static gboolean show_help_gtk = FALSE;
-static gboolean show_help_all = FALSE;
-static gboolean list_panels = FALSE;
-
-const GOptionEntry all_options[] = {
-  { "version", 0, G_OPTION_FLAG_NO_ARG, G_OPTION_ARG_CALLBACK, option_version_cb, NULL, NULL },
-  { "verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose, N_("Enable verbose mode"), NULL },
-  { "overview", 'o', 0, G_OPTION_ARG_NONE, &show_overview, N_("Show the overview"), NULL },
-  { "search", 's', 0, G_OPTION_ARG_STRING, &search_str, N_("Search for the string"), "SEARCH" },
-  { "list", 'l', 0, G_OPTION_ARG_NONE, &list_panels, N_("List possible panel names and exit"), NULL },
-  { "help", 'h', G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_NONE, &show_help, N_("Show help options"), NULL },
-  { "help-all", 0, G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_NONE, &show_help_all, N_("Show help options"), NULL },
-  { "help-gtk", 0, G_OPTION_FLAG_HIDDEN, G_OPTION_ARG_NONE, &show_help_gtk, N_("Show help options"), NULL },
-  { G_OPTION_REMAINING, '\0', 0, G_OPTION_ARG_FILENAME_ARRAY, &start_panels, N_("Panel to display"), N_("[PANEL] [ARGUMENT…]") },
-  { NULL, 0, 0, 0, NULL, NULL, NULL } /* end the list */
-};
-
-static int
-application_command_line_cb (GApplication  *application,
-                             GApplicationCommandLine  *command_line,
-                             GnomeControlCenter      *shell)
-{
-  int argc;
-  char **argv;
-  int retval = 0;
-  GOptionContext *context;
-  GError *error = NULL;
-
-  verbose = FALSE;
-  show_overview = FALSE;
-  show_help = FALSE;
-  start_panels = NULL;
-
-  argv = g_application_command_line_get_arguments (command_line, &argc);
-
-  context = g_option_context_new (N_("- Settings"));
-  g_option_context_add_main_entries (context, all_options, GETTEXT_PACKAGE);
-  g_option_context_set_translation_domain(context, GETTEXT_PACKAGE);
-  g_option_context_add_group (context, gtk_get_option_group (TRUE));
-  g_option_context_set_help_enabled (context, FALSE);
-
-  if (g_option_context_parse (context, &argc, &argv, &error) == FALSE)
-    {
-      g_print (_("%s\nRun '%s --help' to see a full list of available command line options.\n"),
-               error->message, argv[0]);
-      g_error_free (error);
-      g_option_context_free (context);
-      return 1;
-    }
-
-  if (show_help || show_help_all || show_help_gtk)
-    {
-      gchar *help;
-      GOptionGroup *group;
-
-      if (show_help || show_help_all)
-        group = NULL;
-      else
-        group = gtk_get_option_group (FALSE);
-
-      help = g_option_context_get_help (context, FALSE, group);
-      g_print ("%s", help);
-      g_free (help);
-      g_option_context_free (context);
-      return 0;
-    }
-
-  if (list_panels)
-    {
-      GList *panels, *l;
-
-      panels = cc_panel_loader_get_panels ();
-
-      g_print ("%s\n", _("Available panels:"));
-      for (l = panels; l != NULL; l = l->next)
-        g_print ("\t%s\n", (char *) l->data);
-
-      g_list_free (panels);
-
-      return 0;
-    }
-
-  g_option_context_free (context);
-
-#ifdef HAVE_CHEESE
-  cheese_gtk_init (&argc, &argv);
-#endif /* HAVE_CHEESE */
-
-  cc_shell_log_set_debug (verbose);
-
-  gnome_control_center_show (shell, GTK_APPLICATION (application));
-
-  if (search_str)
-    {
-      gnome_control_center_set_search_item (shell, search_str);
-    }
-  else if (show_overview)
-    {
-      gnome_control_center_set_overview_page (shell);
-    }
-  else if (start_panels != NULL && start_panels[0] != NULL)
-    {
-      const char *start_id;
-      GError *err = NULL;
-
-      start_id = start_panels[0];
-
-      if (start_panels[1])
-	g_debug ("Extra argument: %s", start_panels[1]);
-      else
-	g_debug ("No extra argument");
-
-      if (!cc_shell_set_active_panel_from_id (CC_SHELL (shell), start_id, (const gchar**)start_panels+1, &err))
-        {
-          g_warning ("Could not load setting panel \"%s\": %s", start_id,
-                     (err) ? err->message : "Unknown error");
-          retval = 1;
-          if (err)
-            {
-              g_error_free (err);
-              err = NULL;
-            }
-        }
-    }
-
-  gnome_control_center_present (shell);
-  gdk_notify_startup_complete ();
-
-  g_strfreev (argv);
-  if (start_panels != NULL)
-    {
-      g_strfreev (start_panels);
-      start_panels = NULL;
-    }
-  show_overview = FALSE;
-
-  return retval;
-}
-
-static void
-help_activated (GSimpleAction *action,
-                GVariant      *parameter,
-                gpointer       user_data)
-{
-  GnomeControlCenter *shell = user_data;
-  CcPanel *panel = cc_shell_get_active_panel (CC_SHELL (shell));
-  GtkWidget *window = cc_shell_get_toplevel (CC_SHELL (shell));
-  const char *uri = NULL;
-
-  if (panel)
-    uri = cc_panel_get_help_uri (panel);
-
-  gtk_show_uri (gtk_widget_get_screen (window),
-                uri ? uri : "help:gnome-help/prefs",
-                GDK_CURRENT_TIME, NULL);
-}
-
-static void
-quit_activated (GSimpleAction *action,
-                GVariant      *parameter,
-                gpointer       user_data)
-{
-  GnomeControlCenter *shell = user_data;
-  g_object_unref (shell);
-}
-
-static void
-application_startup_cb (GApplication       *application,
-                        GnomeControlCenter *shell)
-{
-  GMenu *menu, *section;
-  GAction *action;
-
-  action = G_ACTION (g_simple_action_new ("help", NULL));
-  g_action_map_add_action (G_ACTION_MAP (application), action);
-  g_signal_connect (action, "activate", G_CALLBACK (help_activated), shell);
-
-  action = G_ACTION (g_simple_action_new ("quit", NULL));
-  g_action_map_add_action (G_ACTION_MAP (application), action);
-  g_signal_connect (action, "activate", G_CALLBACK (quit_activated), shell);
-
-  menu = g_menu_new ();
-
-  section = g_menu_new ();
-  g_menu_append (section, _("Help"), "app.help");
-  g_menu_append (section, _("Quit"), "app.quit");
-
-  g_menu_append_section (menu, NULL, G_MENU_MODEL (section));
-
-  gtk_application_set_app_menu (GTK_APPLICATION (application),
-                                G_MENU_MODEL (menu));
-
-  gtk_application_add_accelerator (GTK_APPLICATION (application),
-                                   "F1", "app.help", NULL);
-
-  /* nothing else to do here, we don't want to show a window before
-   * we've looked at the commandline
-   */
-}
-
-int
-main (int argc, char **argv)
-{
-  GnomeControlCenter *shell;
-  GtkApplication *application;
-  int status;
-
-  bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
-  bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-  textdomain (GETTEXT_PACKAGE);
-
-#ifdef GDK_WINDOWING_X11
-  XInitThreads ();
-#endif
-
-  gtk_init (&argc, &argv);
-  cc_shell_log_init ();
-
-  g_type_ensure (egg_list_box_get_type ());
-
-  /* register a symbolic icon size for use in sidebar lists */
-  gtk_icon_size_register ("cc-sidebar-list", 24, 24);
-
-  notify_init ("gnome-control-center");
-
-  shell = gnome_control_center_new ();
-
-  /* enforce single instance of this application */
-  application = gtk_application_new ("org.gnome.ControlCenter", G_APPLICATION_HANDLES_COMMAND_LINE);
-  g_signal_connect (application, "startup",
-                    G_CALLBACK (application_startup_cb), shell);
-  g_signal_connect (application, "command-line",
-                    G_CALLBACK (application_command_line_cb), shell);
-
-  status = g_application_run (G_APPLICATION (application), argc, argv);
-
-  g_object_unref (application);
-
-  return status;
-}

shell/gnome-control-center.c
@@ -32,10 +32,6 @@
 #include <string.h>
 #include <libgd/gd-styled-text-renderer.h>
 
-#ifdef HAVE_CHEESE
-#include <clutter-gtk/clutter-gtk.h>
-#endif /* HAVE_CHEESE */
-
 #include "cc-panel.h"
 #include "cc-shell.h"
 #include "cc-shell-category-view.h"

shell/main.c
@@ -1,0 +1,57 @@
+/*
+ * Copyright (c) 2009, 2010 Intel, Inc.
+ * Copyright (c) 2010 Red Hat, Inc.
+ *
+ * The Control Center is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * The Control Center is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with the Control Center; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Author: Thomas Wood <thos@gnome.org>
+ */
+
+#include "config.h"
+
+#include <stdlib.h>
+#include <glib/gi18n.h>
+#include <gtk/gtk.h>
+#include <egg-list-box.h>
+
+#ifdef GDK_WINDOWING_X11
+#include <X11/Xlib.h>
+#endif
+
+#include "cc-application.h"
+
+int
+main (int argc, char **argv)
+{
+  GtkApplication *application;
+  int status;
+
+  bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+  bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+  textdomain (GETTEXT_PACKAGE);
+
+#ifdef GDK_WINDOWING_X11
+  XInitThreads ();
+#endif
+
+  g_type_ensure (egg_list_box_get_type ());
+
+  application = cc_application_new ();
+  status = g_application_run (G_APPLICATION (application), argc, argv);
+
+  g_object_unref (application);
+
+  return status;
+}

commit dd73671c243cfeb97fc7eeeb6e40a259a54b209e

shell: build fix (clutter-gtk includes)
Building without cheese broke with commit 64467d7

https://bugzilla.gnome.org/show_bug.cgi?id=694118

shell/cc-application.c
@@ -25,7 +25,6 @@
 #include <glib/gi18n.h>
 #include <gio/gio.h>
 #include <libnotify/notify.h>
-#include <clutter-gtk/clutter-gtk.h>
 
 #include "cc-application.h"
 #include "cc-panel-loader.h"
@@ -34,6 +33,7 @@
 
 #ifdef HAVE_CHEESE
 #include <cheese-gtk.h>
+#include <clutter-gtk/clutter-gtk.h>
 #endif /* HAVE_CHEESE */
 
 struct _CcApplicationPrivate