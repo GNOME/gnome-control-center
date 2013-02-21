a187aa51dd3e194de2f53fe9b393a7c10e0d3853
Ángel Guzmán Maeso <shakaran@gmail.com>
2013-02-21 15:43:05 +0100

    power: Fix deprecated functions
    From gtk_scrolled_window_add_with_viewport to gtk_container_add
    
    http://www.mail-archive.com/crash-catcher@lists.fedorahosted.org/msg03480.html
    
    https://bugzilla.gnome.org/show_bug.cgi?id=694362

--- panels/power/cc-power-panel.c
+++ panels/power/cc-power-panel.c
@@ -2108,7 +2108,7 @@ cc_power_panel_init (CcPowerPanel *self)
   gtk_container_add (GTK_CONTAINER (self), box);
   g_object_ref (widget);
   gtk_widget_unparent (widget);
-  gtk_scrolled_window_add_with_viewport (GTK_SCROLLED_WINDOW (box), widget);
+  gtk_container_add (GTK_CONTAINER (box), widget);
   g_object_unref (widget);
 
   priv->focus_adjustment = gtk_scrolled_window_get_vadjustment (GTK_SCROLLED_WINDOW (box));
