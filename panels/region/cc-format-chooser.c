077dcf4b4b890e916a10067226171a16feda9b1d
Rui Matos <tiagomatos@gmail.com>
2013-03-10 03:34:42 +0100

    region: Fix format chooser check marks being cleared
    https://bugzilla.gnome.org/show_bug.cgi?id=695536

--- panels/region/cc-format-chooser.c
+++ panels/region/cc-format-chooser.c
@@ -170,8 +170,6 @@ set_locale_id (GtkDialog   *chooser,
 
                 } else {
                         /* mark as unselected */
-                        gtk_image_clear (GTK_IMAGE (check));
-                        g_object_set (check, "icon-size", GTK_ICON_SIZE_MENU, NULL);
                         gtk_widget_hide (check);
                 }
         }
