GNOME/gnome-control-center

commit 05a4f6d3d9620782fa5d1a279ef84c11701247d1

sharing: Remove OBEX FTP support
As it's been removed from gnome-user-share.

https://bugzilla.gnome.org/show_bug.cgi?id=711730

panels/sharing/cc-sharing-panel.c
@@ -384,25 +384,11 @@ cc_sharing_panel_setup_bluetooth_sharing_dialog (CcSharingPanel *self)
   g_signal_connect_swapped (priv->bluetooth_killswitch, "state-changed",
                             G_CALLBACK (bluetooth_state_changed), self);
 
-  cc_sharing_panel_bind_switch_to_label (self,
-                                         WID ("share-public-folder-switch"),
-                                         WID ("bluetooth-sharing-status-label"));
-
-  cc_sharing_panel_bind_switch_to_widgets (WID ("share-public-folder-switch"),
-                                           WID ("only-share-trusted-devices-box"),
-                                           NULL);
-
   cc_sharing_panel_bind_switch_to_widgets (WID ("save-received-files-to-downloads-switch"),
                                            WID ("receive-files-grid"),
                                            NULL);
 
   settings = g_settings_new (FILE_SHARING_SCHEMA_ID);
-  g_settings_bind (settings, "bluetooth-enabled",
-                   WID ("share-public-folder-switch"), "active",
-                   G_SETTINGS_BIND_DEFAULT);
-  g_settings_bind (settings, "bluetooth-require-pairing",
-                   WID ("only-share-trusted-devices-switch"), "active",
-                   G_SETTINGS_BIND_DEFAULT);
   g_settings_bind (settings, "bluetooth-obexpush-enabled",
                    WID ("save-received-files-to-downloads-switch"), "active",
                    G_SETTINGS_BIND_DEFAULT);

panels/sharing/sharing.ui
@@ -65,37 +65,6 @@
               </packing>
             </child>
             <child>
-              <object class="GtkSwitch" id="share-public-folder-switch">
-                <property name="visible">True</property>
-                <property name="can_focus">True</property>
-              </object>
-              <packing>
-                <property name="left_attach">1</property>
-                <property name="top_attach">1</property>
-                <property name="width">1</property>
-                <property name="height">1</property>
-              </packing>
-            </child>
-            <child>
-              <object class="GtkLabel" id="label2">
-                <property name="visible">True</property>
-                <property name="can_focus">False</property>
-                <property name="halign">start</property>
-                <property name="hexpand">True</property>
-                <property name="label" translatable="yes">Share Public Folder</property>
-                <property name="mnemonic_widget">share-public-folder-switch</property>
-                <attributes>
-                 <attribute name="weight" value="bold"/>
-                </attributes>
-              </object>
-              <packing>
-                <property name="left_attach">0</property>
-                <property name="top_attach">1</property>
-                <property name="width">1</property>
-                <property name="height">1</property>
-              </packing>
-            </child>
-            <child>
               <object class="GtkGrid" id="receive-files-grid">
                 <property name="visible">True</property>
                 <property name="can_focus">False</property>
@@ -129,7 +98,7 @@
               </object>
               <packing>
                 <property name="left_attach">0</property>
-                <property name="top_attach">4</property>
+                <property name="top_attach">2</property>
                 <property name="width">2</property>
                 <property name="height">1</property>
               </packing>
@@ -147,7 +116,7 @@
               </object>
               <packing>
                 <property name="left_attach">0</property>
-                <property name="top_attach">3</property>
+                <property name="top_attach">1</property>
                 <property name="width">1</property>
                 <property name="height">1</property>
               </packing>
@@ -156,50 +125,12 @@
               <object class="GtkSwitch" id="save-received-files-to-downloads-switch">
                 <property name="visible">True</property>
                 <property name="can_focus">True</property>
+                <property name="halign">end</property>
               </object>
               <packing>
                 <property name="left_attach">1</property>
-                <property name="top_attach">3</property>
+                <property name="top_attach">1</property>
                 <property name="width">1</property>
-                <property name="height">1</property>
-              </packing>
-            </child>
-            <child>
-              <object class="GtkBox" id="only-share-trusted-devices-box">
-                <property name="visible">True</property>
-                <property name="can_focus">False</property>
-                <property name="spacing">6</property>
-                <child>
-                  <object class="GtkLabel" id="only-share-trusted-devices-label">
-                    <property name="visible">True</property>
-                    <property name="can_focus">False</property>
-                    <property name="halign">start</property>
-                    <property name="hexpand">True</property>
-                    <property name="label" translatable="yes">Only share with Trusted Devices</property>
-                    <property name="mnemonic_widget">only-share-trusted-devices-switch</property>
-                  </object>
-                  <packing>
-                    <property name="expand">False</property>
-                    <property name="fill">True</property>
-                    <property name="position">0</property>
-                  </packing>
-                </child>
-                <child>
-                  <object class="GtkSwitch" id="only-share-trusted-devices-switch">
-                    <property name="visible">True</property>
-                    <property name="can_focus">True</property>
-                  </object>
-                  <packing>
-                    <property name="expand">False</property>
-                    <property name="fill">True</property>
-                    <property name="position">1</property>
-                  </packing>
-                </child>
-              </object>
-              <packing>
-                <property name="left_attach">0</property>
-                <property name="top_attach">2</property>
-                <property name="width">2</property>
                 <property name="height">1</property>
               </packing>
             </child>

commit f5ec0cd8d6e41628d8a6663f448988aeec2885a2

sharing: Fix crash when turning off Sharing
Another fallout from:
05a4f6d3d9620782fa5d1a279ef84c11701247d1

https://bugzilla.gnome.org/show_bug.cgi?id=712338

panels/sharing/cc-sharing-panel.c
@@ -100,7 +100,7 @@ cc_sharing_panel_master_switch_notify (GtkSwitch      *gtkswitch,
       gtk_switch_set_active (GTK_SWITCH (WID ("share-public-folder-on-network-switch")),
                              FALSE);
       gtk_switch_set_active (GTK_SWITCH (WID ("share-media-switch")), FALSE);
-      gtk_switch_set_active (GTK_SWITCH (WID ("share-public-folder-switch")),
+      gtk_switch_set_active (GTK_SWITCH (WID ("save-received-files-to-downloads-switch")),
                              FALSE);
     }
 