df246cc8e75cac9054aed0e3f6004bd421088e5b
Bastien Nocera <hadess@hadess.net>
2014-09-05 12:49:00 +0200

    keyboard: Make it possible to stop watching for WM changes
    As we don't want to be doing that anymore if we're not in the keyboard
    panel anymore.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=736117

--- panels/keyboard/wm-common.h
+++ panels/keyboard/wm-common.h
@@ -10,8 +10,9 @@ gchar *wm_common_get_current_window_manager (void);
  * using _GNOME_WM_KEYBINDINGS if available, _NET_WM_NAME otherwise. */
 char **wm_common_get_current_keybindings (void);
 
-void   wm_common_register_window_manager_change (GFunc    func,
-						 gpointer data);
+gpointer   wm_common_register_window_manager_change   (GFunc    func,
+						       gpointer data);
+void       wm_common_unregister_window_manager_change (gpointer id);
 
 #endif /* WM_COMMON_H */
 
