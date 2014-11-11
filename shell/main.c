28a8539b7d862404b54861870c264ab51b6ea7f6
Debarshi Ray <debarshir@gnome.org>
2014-11-11 14:13:16 +0100

    main: Disable SSLv3 because of POODLE
    Since GOA is still using WebKit1, we need to set the environment
    variable ourself. We can stop setting it once we port to WebKit2
    because the network process will handle it for us.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=739960

--- shell/main.c
+++ shell/main.c
@@ -38,6 +38,9 @@ main (int argc, char **argv)
   GtkApplication *application;
   int status;
 
+  /* FIXME: remove this when GOA uses WebKit2 */
+  g_setenv("G_TLS_GNUTLS_PRIORITY", "NORMAL:%COMPAT:!VERS-SSL3.0", FALSE);
+
   bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
   bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
   textdomain (GETTEXT_PACKAGE);
