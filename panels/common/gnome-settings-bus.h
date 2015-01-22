9140b47fe370f95878a7599957394bd2dd83da8f
Bastien Nocera <hadess@hadess.net>
2015-01-22 15:48:31 +0100

    common: Fix build on non-Linux systems
    Only define HAVE_WAYLAND when GDK_WINDOWING_WAYLAND is defined.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=743266

--- panels/common/gnome-settings-bus.h
+++ panels/common/gnome-settings-bus.h
@@ -3,6 +3,8 @@
 
 #include <gdk/gdkx.h>
 
+#ifdef GDK_WINDOWING_WAYLAND
+
 #define HAVE_WAYLAND 1
 
 static inline gboolean
@@ -10,3 +12,5 @@ gnome_settings_is_wayland (void)
 {
   return !GDK_IS_X11_DISPLAY (gdk_display_get_default ());
 }
+
+#endif /* GDK_WINDOWING_WAYLAND */
