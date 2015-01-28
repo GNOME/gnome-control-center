15e8a9542fb9bd38f7a0a5a15efd418aff49218c
Bastien Nocera <hadess@hadess.net>
2015-01-28 16:20:23 +0100

    power: Fix initial state of Bluetooth switch
    The initial state of the switch was never set, so if it happened to be
    on, the switch didn't match reality.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=725654

--- panels/power/cc-power-panel.c
+++ panels/power/cc-power-panel.c
@@ -1663,6 +1663,8 @@ add_power_saving_section (CcPowerPanel *self)
 				G_CALLBACK (bt_powered_state_changed), self);
       g_signal_connect (G_OBJECT (priv->bt_switch), "notify::active",
 			G_CALLBACK (bt_switch_changed), self);
+
+      bt_powered_state_changed (self);
     }
 #endif
 
