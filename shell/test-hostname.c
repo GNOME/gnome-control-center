af5f2f3d7375c9a14bf91ddd45978ae47fca497d
Bastien Nocera <hadess@hadess.net>
2016-06-10 15:02:57 +0200

    shell: Port tests to GTest

--- shell/test-hostname.c
+++ shell/test-hostname.c
@@ -6,12 +6,72 @@
 
 #include "hostname-helper.h"
 
-int main (int argc, char **argv)
+static void
+test_hostname (void)
 {
+	char *contents;
 	char *result;
 	guint i;
-	char *contents;
 	char **lines;
+
+	if (g_file_get_contents (TEST_SRCDIR "/hostnames-test.txt", &contents, NULL, NULL) == FALSE) {
+		g_warning ("Failed to load '%s'", TEST_SRCDIR "/hostnames-test.txt");
+		g_test_fail ();
+		return;
+	}
+
+	lines = g_strsplit (contents, "\n", -1);
+	if (lines == NULL) {
+		g_warning ("Test file is empty");
+		g_test_fail ();
+		return;
+	}
+
+	for (i = 0; lines[i] != NULL; i++) {
+		char *utf8;
+		char **items;
+
+		if (*lines[i] == '#')
+			continue;
+		if (*lines[i] == '\0')
+			break;
+
+		items = g_strsplit (lines[i], "\t", -1);
+		utf8 = g_locale_from_utf8 (items[0], -1, NULL, NULL, NULL);
+		result = pretty_hostname_to_static (items[0], FALSE);
+		if (g_strcmp0 (result, items[2]) != 0) {
+			g_error ("Result for '%s' doesn't match '%s' (got: '%s')",
+				 utf8, items[2], result);
+			g_test_fail ();
+		} else {
+			g_debug ("Result for '%s' matches '%s'",
+				 utf8, result);
+		}
+		g_free (result);
+		g_free (utf8);
+
+		result = pretty_hostname_to_static (items[0], TRUE);
+		utf8 = g_locale_from_utf8 (items[0], -1, NULL, NULL, NULL);
+		if (g_strcmp0 (result, items[1]) != 0) {
+			g_error ("Result for '%s' doesn't match '%s' (got: '%s')",
+				 utf8, items[1], result);
+			g_test_fail ();
+		} else {
+			g_debug ("Result for '%s' matches '%s'",
+				 utf8, result);
+		}
+		g_free (result);
+		g_free (utf8);
+
+		g_strfreev (items);
+	}
+
+	g_strfreev (lines);
+	g_free (contents);
+}
+
+int main (int argc, char **argv)
+{
 	char *locale;
 
 	/* Running in some locales will
@@ -27,53 +87,9 @@ int main (int argc, char **argv)
 		return 0;
 	}
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+	g_test_init (&argc, &argv, NULL);
 
-	if (g_file_get_contents (argv[1], &contents, NULL, NULL) == FALSE) {
-		g_warning ("Failed to load '%s'", argv[1]);
-		return 1;
-	}
-	lines = g_strsplit (contents, "\n", -1);
-	if (lines == NULL) {
-		g_warning ("Test file is empty");
-		return 1;
-	}
+	g_test_add_func ("/shell/hostname", test_hostname);
 
-	for (i = 0; lines[i] != NULL; i++) {
-		char *utf8;
-		char **items;
-
-		if (*lines[i] == '#')
-			continue;
-		if (*lines[i] == '\0')
-			break;
-
-		items = g_strsplit (lines[i], "\t", -1);
-		utf8 = g_locale_from_utf8 (items[0], -1, NULL, NULL, NULL);
-		result = pretty_hostname_to_static (items[0], FALSE);
-		if (g_strcmp0 (result, items[2]) != 0)
-			g_error ("Result for '%s' doesn't match '%s' (got: '%s')",
-				 utf8, items[2], result);
-		else
-			g_debug ("Result for '%s' matches '%s'",
-				 utf8, result);
-		g_free (result);
-		g_free (utf8);
-
-		result = pretty_hostname_to_static (items[0], TRUE);
-		utf8 = g_locale_from_utf8 (items[0], -1, NULL, NULL, NULL);
-		if (g_strcmp0 (result, items[1]) != 0)
-			g_error ("Result for '%s' doesn't match '%s' (got: '%s')",
-				 utf8, items[1], result);
-		else
-			g_debug ("Result for '%s' matches '%s'",
-				 utf8, result);
-		g_free (result);
-		g_free (utf8);
-
-		g_strfreev (items);
-	}
-
-	g_strfreev (lines);
-
-	return 0;
+	return g_test_run ();
 }
