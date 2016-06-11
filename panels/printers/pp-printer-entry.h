37e37961e5577da0b4fbff78c4d2a03fa7618779
Felipe Borges <felipeborges@gnome.org>
2016-06-11 14:34:41 +0200

    printers: introduce PpPrinterEntry widget
    This commit introduces the following regressions:
    - no possibility of renaming properties such as printer names,
    location, or changing model/driver. This issue is going to be
    solved nextly by the introduction of the PpDetailsDialog.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=767600

---
+++ panels/printers/pp-printer-entry.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017 Red Hat, Inc
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Author: Felipe Borges <felipeborges@gnome.org>
+ */
+
+#ifndef PP_PRINTER_ENTRY_H
+#define PP_PRINTER_ENTRY_H
+
+#include <gtk/gtk.h>
+#include <cups/cups.h>
+
+#define PP_PRINTER_ENTRY_TYPE (pp_printer_entry_get_type ())
+#define PP_PRINTER_ENTRY(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), PP_PRINTER_ENTRY_TYPE, PpPrinterEntry))
+
+typedef struct _PpPrinterEntry      PpPrinterEntry;
+typedef struct _PpPrinterEntryClass PpPrinterEntryClass;
+
+GType       pp_printer_entry_get_type (void);
+
+PpPrinterEntry *pp_printer_entry_new  (cups_dest_t printer,
+                                       gboolean    is_authorized);
+
+#endif /* PP_PRINTER_ENTRY_H */
