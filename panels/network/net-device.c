b4ac30b51ef2124f0c846941cfa008f991c97a7c
Bastien Nocera <hadess@hadess.net>
2017-03-10 19:15:29 +0100

    network: Simplify object destruction

--- panels/network/net-device.c
+++ panels/network/net-device.c
@@ -268,8 +268,7 @@ net_device_finalize (GObject *object)
                 g_signal_handler_disconnect (priv->nm_device,
                                              priv->changed_id);
         }
-        if (priv->nm_device != NULL)
-                g_object_unref (priv->nm_device);
+        g_clear_object (&priv->nm_device);
 
         G_OBJECT_CLASS (net_device_parent_class)->finalize (object);
 }
