62c86c7712bab311999d1eb7def4e377b37fa27b
Jeremy Bicha <jbicha@ubuntu.com>
2017-08-07 11:32:33 -0400

    power: Restore Power Off option for Power Button
    https://bugzilla.gnome.org/show_bug.cgi?id=781108

--- panels/power/cc-power-panel.c
+++ panels/power/cc-power-panel.c
@@ -2065,6 +2065,7 @@ populate_power_button_model (GtkTreeModel *model,
     GsdPowerButtonActionType value;
   } actions[] = {
     { N_("Suspend"), GSD_POWER_BUTTON_ACTION_SUSPEND },
+    { N_("Power Off"), GSD_POWER_BUTTON_ACTION_INTERACTIVE },
     { N_("Hibernate"), GSD_POWER_BUTTON_ACTION_HIBERNATE },
     { N_("Nothing"), GSD_POWER_BUTTON_ACTION_NOTHING }
   };
