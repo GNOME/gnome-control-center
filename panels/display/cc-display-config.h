b13b3f6de5690258410b673306f31e912e9003b2
Benjamin Berg <bberg@redhat.com>
2017-12-22 14:41:25 +0100

    display: Move output utility functions into CcDisplayMonitor
    This adds the following API:
     * cc_display_config_get_ui_sorted_monitors
       Returns the monitors in UI order
     * cc_display_config_count_useful_monitors
       Counts the useful monitors (active and usable)
     * cc_display_monitor_is_useful
       Checks if a monitor is active and usable
     * cc_display_monitor_is_useable
       Check if a monitor is marked as useable
     * cc_display_monitor_set_usable
       Used to mark builtin monitors as unusable if the lid is closed
     * cc_display_monitor_get_ui_*
       Get the UI number and strings for display
    
    https://bugzilla.gnome.org/show_bug.cgi?id=786971

--- panels/display/cc-display-config.h
+++ panels/display/cc-display-config.h
@@ -141,6 +141,8 @@ struct _CcDisplayConfigClass
 
 
 GList *cc_display_config_get_monitors (CcDisplayConfig *config);
+GList *cc_display_config_get_ui_sorted_monitors (CcDisplayConfig *config);
+int cc_display_config_count_useful_monitors (CcDisplayConfig *config);
 gboolean cc_display_config_is_applicable (CcDisplayConfig *config);
 gboolean cc_display_config_equal (CcDisplayConfig *config,
                                   CcDisplayConfig *other);
@@ -184,6 +186,13 @@ void cc_display_monitor_set_mode (CcDisplayMonitor *monitor,
                                   CcDisplayMode    *mode);
 void cc_display_monitor_set_position (CcDisplayMonitor *monitor,
                                       int x, int y);
+
+gboolean cc_display_monitor_is_useful (CcDisplayMonitor *monitor);
+gboolean cc_display_monitor_is_usable (CcDisplayMonitor *monitor);
+void cc_display_monitor_set_usable (CcDisplayMonitor *monitor, gboolean is_usable);
+int cc_display_monitor_get_ui_number (CcDisplayMonitor *monitor);
+const char * cc_display_monitor_get_ui_name (CcDisplayMonitor *monitor);
+const char * cc_display_monitor_get_ui_number_name (CcDisplayMonitor *monitor);
 
 void cc_display_mode_get_resolution (CcDisplayMode *mode,
                                      int *width,
