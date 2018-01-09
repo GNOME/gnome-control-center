169a0b3d565181960493093cdda5feb79c31c14e
Felipe Borges <felipeborges@gnome.org>
2018-01-09 16:07:45 +0100

    user-accounts: Introduce a UI template for UmPhotoDialog
    https://bugzilla.gnome.org/show_bug.cgi?id=766670

--- panels/user-accounts/um-photo-dialog.c
+++ panels/user-accounts/um-photo-dialog.c
@@ -614,12 +614,16 @@ um_photo_dialog_dispose (GObject *object)
 static void
 um_photo_dialog_init (UmPhotoDialog *um)
 {
+        gtk_widget_init_template (GTK_WIDGET (um));
 }
 
 static void
 um_photo_dialog_class_init (UmPhotoDialogClass *klass)
 {
+        GtkWidgetClass *wclass = GTK_WIDGET_CLASS (klass);
         GObjectClass *oclass = G_OBJECT_CLASS (klass);
+
+        gtk_widget_class_set_template_from_resource (wclass, "/org/gnome/control-center/user-accounts/avatar-chooser.ui");
 
         oclass->dispose = um_photo_dialog_dispose;
 }
