598e863d0171960124c7a12b8e3b1a30e2332be1
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2018-03-28 16:39:13 -0300

    window: Profile panel creation times

--- shell/cc-window.c
+++ shell/cc-window.c
@@ -20,6 +20,8 @@
  * Author: Thomas Wood <thos@gnome.org>
  */
 
+#define G_LOG_DOMAIN "cc-window"
+
 #include <config.h>
 
 #include "cc-window.h"
@@ -31,6 +33,7 @@
 #include <gdk/gdkkeysyms.h>
 #include <gdk/gdkx.h>
 #include <string.h>
+#include <time.h>
 
 #include "cc-panel.h"
 #include "cc-shell.h"
@@ -117,13 +120,20 @@ activate_panel (CcWindow    *self,
                 const gchar *name,
                 GIcon       *gicon)
 {
+  g_autoptr (GTimer) timer = NULL;
   GtkWidget *box, *title_widget;
   const gchar *icon_name;
+  gdouble ellapsed_time;
 
   if (!id)
     return FALSE;
 
+  timer = g_timer_new ();
+
   g_settings_set_string (self->settings, "last-panel", id);
+
+  /* Begin the profile */
+  g_timer_start (timer);
 
   self->current_panel = GTK_WIDGET (cc_panel_loader_load_by_name (CC_SHELL (self), id, parameters));
   cc_shell_set_active_panel (CC_SHELL (self), CC_PANEL (self->current_panel));
@@ -155,6 +165,13 @@ activate_panel (CcWindow    *self,
   gtk_header_bar_set_custom_title (GTK_HEADER_BAR (self->panel_headerbar), title_widget);
 
   self->current_panel_box = box;
+
+  /* Finish profiling */
+  g_timer_stop (timer);
+
+  ellapsed_time = g_timer_elapsed (timer, NULL);
+
+  g_debug ("Time to open panel '%s': %lfs", name, ellapsed_time);
 
   return TRUE;
 }
