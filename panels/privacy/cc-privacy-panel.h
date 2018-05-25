b81b93193aa3665b37729c9515f6bc5d0002e665
Robert Ancell <robert.ancell@canonical.com>
2018-05-25 12:39:49 +1200

    privacy: Replace GObject boilerplace with G_DECLARE_TYPE

--- panels/privacy/cc-privacy-panel.h
+++ panels/privacy/cc-privacy-panel.h
@@ -25,45 +25,8 @@
 
 G_BEGIN_DECLS
 
-#define CC_TYPE_PRIVACY_PANEL cc_privacy_panel_get_type()
-
-#define CC_PRIVACY_PANEL(obj) \
-  (G_TYPE_CHECK_INSTANCE_CAST ((obj), \
-  CC_TYPE_PRIVACY_PANEL, CcPrivacyPanel))
-
-#define CC_PRIVACY_PANEL_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_CAST ((klass), \
-  CC_TYPE_PRIVACY_PANEL, CcPrivacyPanelClass))
-
-#define CC_IS_PRIVACY_PANEL(obj) \
-  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
-  CC_TYPE_PRIVACY_PANEL))
-
-#define CC_IS_PRIVACY_PANEL_CLASS(klass) \
-  (G_TYPE_CHECK_CLASS_TYPE ((klass), \
-  CC_TYPE_PRIVACY_PANEL))
-
-#define CC_PRIVACY_PANEL_GET_CLASS(obj) \
-  (G_TYPE_INSTANCE_GET_CLASS ((obj), \
-  CC_TYPE_PRIVACY_PANEL, CcPrivacyPanelClass))
-
-typedef struct _CcPrivacyPanel CcPrivacyPanel;
-typedef struct _CcPrivacyPanelClass CcPrivacyPanelClass;
-typedef struct _CcPrivacyPanelPrivate CcPrivacyPanelPrivate;
-
-struct _CcPrivacyPanel
-{
-  CcPanel parent;
-
-  CcPrivacyPanelPrivate *priv;
-};
-
-struct _CcPrivacyPanelClass
-{
-  CcPanelClass parent_class;
-};
-
-GType cc_privacy_panel_get_type (void) G_GNUC_CONST;
+#define CC_TYPE_PRIVACY_PANEL (cc_privacy_panel_get_type ())
+G_DECLARE_FINAL_TYPE (CcPrivacyPanel, cc_privacy_panel, CC, PRIVACY_PANEL, CcPanel)
 
 G_END_DECLS
 
