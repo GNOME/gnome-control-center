7524bdf289d40528a2be0e7fd62bb9eeff6de85a
Robert Ancell <robert.ancell@canonical.com>
2018-05-29 14:53:49 +1200

    display: Replace ifdefs with #pragma once

--- panels/display/cc-display-panel.h
+++ panels/display/cc-display-panel.h
@@ -18,9 +18,7 @@
  *
  */
 
-
-#ifndef _CC_DISPLAY_PANEL_H
-#define _CC_DISPLAY_PANEL_H
+#pragma once
 
 #include <shell/cc-panel.h>
 
@@ -30,5 +28,3 @@ G_BEGIN_DECLS
 G_DECLARE_FINAL_TYPE (CcDisplayPanel, cc_display_panel, CC, DISPLAY_PANEL, CcPanel)
 
 G_END_DECLS
-
-#endif /* _CC_DISPLAY_PANEL_H */
