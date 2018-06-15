c24790bcc1f3b6a56564d01e9be531415649258f
Robert Ancell <robert.ancell@canonical.com>
2018-06-15 14:16:20 +1200

    printers: Replace ifdefs with #pragma once

--- panels/printers/cc-printers-panel.h
+++ panels/printers/cc-printers-panel.h
@@ -16,9 +16,7 @@
  *
  */
 
-
-#ifndef _CC_PRINTERS_PANEL_H
-#define _CC_PRINTERS_PANEL_H
+#pragma once
 
 #include <shell/cc-panel.h>
 
@@ -28,5 +26,3 @@ G_BEGIN_DECLS
 G_DECLARE_FINAL_TYPE (CcPrintersPanel, cc_printers_panel, CC, PRINTERS_PANEL, CcPanel)
 
 G_END_DECLS
-
-#endif /* _CC_PRINTERS_PANEL_H */
