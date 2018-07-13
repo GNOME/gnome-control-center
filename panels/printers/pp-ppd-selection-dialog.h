09465579b717ece847f00a16c97727b566e70851
Robert Ancell <robert.ancell@canonical.com>
2018-07-13 14:02:01 +1200

    printers: Use const for manufacturer string

--- panels/printers/pp-ppd-selection-dialog.h
+++ panels/printers/pp-ppd-selection-dialog.h
@@ -30,7 +30,7 @@ typedef struct _PpPPDSelectionDialog PpPPDSelectionDialog;
 
 PpPPDSelectionDialog *pp_ppd_selection_dialog_new                  (GtkWindow                 *parent,
                                                                     PPDList                   *ppd_list,
-                                                                    gchar                     *manufacturer,
+                                                                    const gchar               *manufacturer,
                                                                     UserResponseCallback       user_callback,
                                                                     gpointer                   user_data);
 gchar                *pp_ppd_selection_dialog_get_ppd_name         (PpPPDSelectionDialog      *dialog);
