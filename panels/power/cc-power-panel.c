37a0b8e04f2277208fa271c98a3099546fcb6066
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2018-07-22 21:33:48 -0300

    power: Reword some strings
    Some of the current string related to saving power by
    turning something off are ambiguous, and can mean both
    "yes, turn off" or "yes, keep it on" at the same time.
    
    This commit slightly rewords those sentences in order
    to disambiguate them.
    
    https://gitlab.gnome.org/GNOME/gnome-control-center/issues/53
    
    [skip ci]

--- panels/power/cc-power-panel.c
+++ panels/power/cc-power-panel.c
@@ -1879,7 +1879,7 @@ add_power_saving_section (CcPowerPanel *self)
   gtk_label_set_use_underline (GTK_LABEL (label), TRUE);
   gtk_box_pack_start (GTK_BOX (box2), label, TRUE, TRUE, 0);
 
-  w = gtk_label_new (_("Turn off Wi-Fi to save power."));
+  w = gtk_label_new (_("Wi-Fi can be turned off to save power."));
   gtk_widget_set_halign (w, GTK_ALIGN_START);
   gtk_style_context_add_class (gtk_widget_get_style_context (w), GTK_STYLE_CLASS_DIM_LABEL);
   gtk_box_pack_start (GTK_BOX (box2), w, TRUE, TRUE, 0);
@@ -1909,7 +1909,7 @@ add_power_saving_section (CcPowerPanel *self)
   gtk_label_set_use_underline (GTK_LABEL (label), TRUE);
   gtk_box_pack_start (GTK_BOX (box2), label, TRUE, TRUE, 0);
 
-  w = gtk_label_new (_("Turn off mobile broadband (3G, 4G, LTE, etc.) to save power."));
+  w = gtk_label_new (_("Mobile broadband (LTE, 3G, 4G, etc.) can be turned off to save power."));
   gtk_widget_set_halign (w, GTK_ALIGN_START);
   gtk_style_context_add_class (gtk_widget_get_style_context (w), GTK_STYLE_CLASS_DIM_LABEL);
   gtk_box_pack_start (GTK_BOX (box2), w, TRUE, TRUE, 0);
@@ -1973,7 +1973,7 @@ add_power_saving_section (CcPowerPanel *self)
   gtk_label_set_use_underline (GTK_LABEL (label), TRUE);
   gtk_box_pack_start (GTK_BOX (box2), label, TRUE, TRUE, 0);
 
-  label = gtk_label_new (_("Turn off Bluetooth to save power."));
+  label = gtk_label_new (_("Bluetooth can be turned off to save power."));
   gtk_widget_set_halign (label, GTK_ALIGN_START);
   gtk_style_context_add_class (gtk_widget_get_style_context (label), GTK_STYLE_CLASS_DIM_LABEL);
   gtk_box_pack_start (GTK_BOX (box2), label, TRUE, TRUE, 0);
