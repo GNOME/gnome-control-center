c79ee4849a88832950a447f51f3d3d599659ff79
Adrien Plazas <kekun.plazas@laposte.net>
2018-09-18 08:39:50 +0200

    panel-list: Add the show-nested-list signal
    This signal will be emitted just before changing the panel list to a
    nested one.
    
    This will be used to know that the focus should be put on the panels
    list and not on the current panel.

--- shell/cc-panel-list.c
+++ shell/cc-panel-list.c
@@ -80,6 +80,7 @@ enum
 enum
 {
   SHOW_PANEL,
+  SHOW_NESTED_LIST,
   LAST_SIGNAL
 };
 
@@ -529,6 +530,7 @@ row_activated_cb (GtkWidget     *listbox,
   /* Details */
   if (row == self->details_row)
     {
+      g_signal_emit (self, signals[SHOW_NESTED_LIST], 0);
       cc_panel_list_set_view (self, CC_PANEL_LIST_DETAILS);
       goto out;
     }
@@ -536,6 +538,7 @@ row_activated_cb (GtkWidget     *listbox,
   /* Devices */
   if (row == self->devices_row)
     {
+      g_signal_emit (self, signals[SHOW_NESTED_LIST], 0);
       cc_panel_list_set_view (self, CC_PANEL_LIST_DEVICES);
       goto out;
     }
@@ -715,6 +718,18 @@ cc_panel_list_class_init (CcPanelListClass *klass)
                                       G_TYPE_NONE,
                                       1,
                                       G_TYPE_STRING);
+
+  /**
+   * CcPanelList:show-nested-list:
+   *
+   * Emited when nested list is selected.
+   */
+  signals[SHOW_NESTED_LIST] = g_signal_new ("show-nested-list",
+                                            CC_TYPE_PANEL_LIST,
+                                            G_SIGNAL_RUN_LAST,
+                                            0, NULL, NULL, NULL,
+                                            G_TYPE_NONE,
+                                            0);
 
   /**
    * CcPanelList:search-mode:
