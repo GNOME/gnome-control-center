d2d8a501cd5485cb6d2f5d1908a4bb50efa1e770
Robert Ancell <robert.ancell@canonical.com>
2018-10-01 15:25:06 +1300

    background: Don't use gtk_widget_show_all
    It is removed in GTK+ 4

--- panels/background/test-chooser-dialog.c
+++ panels/background/test-chooser-dialog.c
@@ -29,7 +29,7 @@ int main (int argc, char **argv)
 	gtk_window_set_modal (GTK_WINDOW (dialog), FALSE);
 	g_signal_connect (G_OBJECT (dialog), "response",
 			  G_CALLBACK (on_dialog_response), NULL);
-	gtk_widget_show_all (dialog);
+	gtk_widget_show (dialog);
 
 	gtk_main ();
 
