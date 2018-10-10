1eae27d3c490c49083ae7e92618ca2ddfa033dca
Robert Ancell <robert.ancell@canonical.com>
2018-10-10 16:08:37 +1300

    sound: Create new sound panel that uses the updated design
    Use the new designs:
    https://wiki.gnome.org/Design/SystemSettings/Sound
    
    The existing panel was very old and had a very traditional layout that didn't
    align with the new designs. Initialy this patch modified existing elements of
    the old panel but the change was so great that new widgets were written. Some
    of the widgets have algorithms taken from the old panel.
    
    The sound files and icons are from the old panel.

---
+++ panels/sound-new/cc-stream-list-box.c
@@ -0,0 +1,232 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2018 Canonical Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <pulse/pulseaudio.h>
+#include <gvc-mixer-sink.h>
+#include <gvc-mixer-source.h>
+
+#include "cc-stream-list-box.h"
+#include "cc-stream-row.h"
+
+struct _CcStreamListBox
+{
+  GtkListBox       parent_instance;
+
+  GtkSizeGroup    *label_size_group;
+  GvcMixerControl *mixer_control;
+  guint            stream_added_handler_id;
+  guint            stream_removed_handler_id;
+};
+
+G_DEFINE_TYPE (CcStreamListBox, cc_stream_list_box, GTK_TYPE_LIST_BOX)
+
+enum
+{
+  PROP_0,
+  PROP_LABEL_SIZE_GROUP
+};
+
+static gint
+sort_cb (GtkListBoxRow *row1,
+         GtkListBoxRow *row2,
+         gpointer       user_data)
+{
+  CcStreamListBox *self = user_data;
+  GvcMixerStream *stream1, *stream2, *event_sink;
+  g_autofree gchar *name1 = NULL;
+  g_autofree gchar *name2 = NULL;
+
+  stream1 = cc_stream_row_get_stream (CC_STREAM_ROW (row1));
+  stream2 = cc_stream_row_get_stream (CC_STREAM_ROW (row2));
+
+  /* Put the system sound events control at the top */
+  event_sink = gvc_mixer_control_get_event_sink_input (self->mixer_control);
+  if (stream1 == event_sink)
+    return -1;
+  else if (stream2 == event_sink)
+    return 1;
+
+  name1 = g_utf8_casefold (gvc_mixer_stream_get_name (stream1), -1);
+  name2 = g_utf8_casefold (gvc_mixer_stream_get_name (stream2), -1);
+
+  return g_strcmp0 (name1, name2);
+}
+
+static void
+stream_added_cb (CcStreamListBox *self,
+                 guint            id)
+{
+  GvcMixerStream *stream;
+  const gchar *app_id;
+  CcStreamRow *row;
+
+  stream = gvc_mixer_control_lookup_stream_id (self->mixer_control, id);
+  if (stream == NULL)
+    return;
+
+  app_id = gvc_mixer_stream_get_application_id (stream);
+
+  /* Skip master volume controls */
+  if (g_strcmp0 (app_id, "org.gnome.VolumeControl") == 0 ||
+      g_strcmp0 (app_id, "org.PulseAudio.pavucontrol") == 0)
+    {
+      return;
+    }
+
+  /* Skip streams that aren't volume controls */
+  if (GVC_IS_MIXER_SOURCE (stream) ||
+      GVC_IS_MIXER_SINK (stream) ||
+      gvc_mixer_stream_is_virtual (stream) ||
+      gvc_mixer_stream_is_event_stream (stream))
+    {
+      return;
+    }
+
+  row = cc_stream_row_new (self->label_size_group, stream, id);
+  gtk_widget_show (GTK_WIDGET (row));
+  gtk_list_box_row_set_activatable (GTK_LIST_BOX_ROW (row), FALSE);
+  gtk_container_add (GTK_CONTAINER (self), GTK_WIDGET (row));
+}
+
+static CcStreamRow *
+find_row (CcStreamListBox *self,
+          guint            id)
+{
+  g_autoptr(GList) children = NULL;
+  GList *link;
+
+  children = gtk_container_get_children (GTK_CONTAINER (self));
+  for (link = children; link; link = link->next)
+    {
+      CcStreamRow *row = link->data;
+
+      if (!CC_IS_STREAM_ROW (row))
+        continue;
+
+      if (id == cc_stream_row_get_id (row))
+        return row;
+    }
+
+  return NULL;
+}
+
+static void
+stream_removed_cb (CcStreamListBox *self,
+                   guint            id)
+{
+  CcStreamRow *row;
+
+  row = find_row (self, id);
+  if (row != NULL)
+    gtk_container_remove (GTK_CONTAINER (self), GTK_WIDGET (row));
+}
+
+static void
+cc_stream_list_box_set_property (GObject      *object,
+                                 guint         property_id,
+                                 const GValue *value,
+                                 GParamSpec   *pspec)
+{
+  CcStreamListBox *self = CC_STREAM_LIST_BOX (object);
+
+  switch (property_id) {
+  case PROP_LABEL_SIZE_GROUP:
+    self->label_size_group = g_value_dup_object (value);
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+static void
+cc_stream_list_box_get_property (GObject    *object,
+                                 guint       property_id,
+                                 GValue     *value,
+                                 GParamSpec *pspec)
+{
+  CcStreamListBox *self = CC_STREAM_LIST_BOX (object);
+
+  switch (property_id) {
+  case PROP_LABEL_SIZE_GROUP:
+    g_value_set_object (value, self->label_size_group);
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  }
+}
+
+static void
+cc_stream_list_box_dispose (GObject *object)
+{
+  CcStreamListBox *self = CC_STREAM_LIST_BOX (object);
+
+  g_clear_object (&self->mixer_control);
+
+  G_OBJECT_CLASS (cc_stream_list_box_parent_class)->dispose (object);
+}
+
+void
+cc_stream_list_box_class_init (CcStreamListBoxClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->set_property = cc_stream_list_box_set_property;
+  object_class->get_property = cc_stream_list_box_get_property;
+  object_class->dispose = cc_stream_list_box_dispose;
+
+  g_object_class_install_property (object_class, PROP_LABEL_SIZE_GROUP,
+                                   g_param_spec_object ("label-size-group",
+                                                        NULL,
+                                                        NULL,
+                                                        GTK_TYPE_SIZE_GROUP,
+                                                        G_PARAM_READWRITE | G_PARAM_CONSTRUCT));
+}
+
+void
+cc_stream_list_box_init (CcStreamListBox *self)
+{
+  gtk_list_box_set_selection_mode (GTK_LIST_BOX (self), GTK_SELECTION_NONE);
+  gtk_list_box_set_sort_func (GTK_LIST_BOX (self), sort_cb, self, NULL);
+}
+
+void
+cc_stream_list_box_set_mixer_control (CcStreamListBox *self,
+                                      GvcMixerControl *mixer_control)
+{
+  g_return_if_fail (CC_IS_STREAM_LIST_BOX (self));
+
+  if (self->mixer_control != NULL)
+    {
+      g_signal_handler_disconnect (self->mixer_control, self->stream_added_handler_id);
+      self->stream_added_handler_id = 0;
+      g_signal_handler_disconnect (self->mixer_control, self->stream_removed_handler_id);
+      self->stream_removed_handler_id = 0;
+    }
+  g_clear_object (&self->mixer_control);
+
+  self->mixer_control = g_object_ref (mixer_control);
+
+  self->stream_added_handler_id = g_signal_connect_object (self->mixer_control,
+                                                           "stream-added",
+                                                           G_CALLBACK (stream_added_cb),
+                                                           self, G_CONNECT_SWAPPED);
+  self->stream_removed_handler_id = g_signal_connect_object (self->mixer_control,
+                                                             "stream-removed",
+                                                             G_CALLBACK (stream_removed_cb),
+                                                             self, G_CONNECT_SWAPPED);
+}
---
+++ panels/sound-new/cc-volume-slider.c
@@ -0,0 +1,184 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2018 Canonical Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glib/gi18n.h>
+#include <math.h>
+#include <pulse/pulseaudio.h>
+#include <gvc-mixer-control.h>
+#include <canberra-gtk.h>
+
+#include "cc-sound-new-resources.h"
+#include "cc-volume-slider.h"
+
+struct _CcVolumeSlider
+{
+  GtkBox           parent_instance;
+
+  GtkToggleButton *mute_button;
+  GtkAdjustment   *volume_adjustment;
+  GtkScale        *volume_scale;
+
+  GvcMixerStream  *stream;
+  guint            notify_volume_handler_id;
+  guint            notify_is_muted_handler_id;
+};
+
+G_DEFINE_TYPE (CcVolumeSlider, cc_volume_slider, GTK_TYPE_BOX)
+
+static void
+volume_changed_cb (CcVolumeSlider *self)
+{
+  gdouble volume, rounded;
+
+  if (self->stream == NULL)
+    return;
+
+  volume = gtk_adjustment_get_value (self->volume_adjustment);
+  rounded = round (volume);
+
+  gtk_toggle_button_set_active (self->mute_button, volume == 0.0);
+  if (gvc_mixer_stream_set_volume (self->stream,
+                                   (pa_volume_t) rounded))
+    {
+      gvc_mixer_stream_push_volume (self->stream);
+    }
+}
+
+static void
+mute_button_toggled_cb (CcVolumeSlider *self)
+{
+  if (self->stream == NULL)
+    return;
+
+  gvc_mixer_stream_change_is_muted (self->stream, gtk_toggle_button_get_active (self->mute_button));
+}
+
+static void
+notify_volume_cb (CcVolumeSlider *self)
+{
+  g_signal_handlers_block_by_func (self->volume_adjustment, volume_changed_cb, self);
+  if (gtk_toggle_button_get_active (self->mute_button))
+    gtk_adjustment_set_value (self->volume_adjustment, 0.0);
+  else
+    gtk_adjustment_set_value (self->volume_adjustment, gvc_mixer_stream_get_volume (self->stream));
+  g_signal_handlers_unblock_by_func (self->volume_adjustment, volume_changed_cb, self);
+}
+
+static void
+notify_is_muted_cb (CcVolumeSlider *self)
+{
+  g_signal_handlers_block_by_func (self->mute_button, mute_button_toggled_cb, self);
+  gtk_toggle_button_set_active (self->mute_button, gvc_mixer_stream_get_is_muted (self->stream));
+  g_signal_handlers_unblock_by_func (self->mute_button, mute_button_toggled_cb, self);
+  notify_volume_cb (self);
+}
+
+static void
+cc_volume_slider_dispose (GObject *object)
+{
+  CcVolumeSlider *self = CC_VOLUME_SLIDER (object);
+
+  g_clear_object (&self->stream);
+
+  G_OBJECT_CLASS (cc_volume_slider_parent_class)->dispose (object);
+}
+
+void
+cc_volume_slider_class_init (CcVolumeSliderClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  object_class->dispose = cc_volume_slider_dispose;
+
+  gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/control-center/sound-new/cc-volume-slider.ui");
+
+  gtk_widget_class_bind_template_child (widget_class, CcVolumeSlider, mute_button);
+  gtk_widget_class_bind_template_child (widget_class, CcVolumeSlider, volume_adjustment);
+  gtk_widget_class_bind_template_child (widget_class, CcVolumeSlider, volume_scale);
+
+  gtk_widget_class_bind_template_callback (widget_class, mute_button_toggled_cb);
+  gtk_widget_class_bind_template_callback (widget_class, volume_changed_cb);
+}
+
+void
+cc_volume_slider_init (CcVolumeSlider *self)
+{
+  gdouble vol_max_norm;
+
+  g_resources_register (cc_sound_new_get_resource ());
+
+  gtk_widget_init_template (GTK_WIDGET (self));
+
+  vol_max_norm = gvc_mixer_control_get_vol_max_norm (NULL);
+  gtk_adjustment_set_upper (self->volume_adjustment, vol_max_norm);
+  gtk_adjustment_set_page_increment (self->volume_adjustment, vol_max_norm / 100.0);
+}
+
+void
+cc_volume_slider_set_stream (CcVolumeSlider *self,
+                             GvcMixerStream *stream)
+{
+  g_return_if_fail (CC_IS_VOLUME_SLIDER (self));
+
+  if (self->stream != NULL)
+    {
+      g_signal_handler_disconnect (self->stream, self->notify_volume_handler_id);
+      self->notify_volume_handler_id = 0;
+      g_signal_handler_disconnect (self->stream, self->notify_is_muted_handler_id);
+      self->notify_is_muted_handler_id = 0;
+    }
+  g_clear_object (&self->stream);
+
+  if (stream != NULL)
+    {
+      self->stream = g_object_ref (stream);
+
+      self->notify_volume_handler_id = g_signal_connect_object (stream,
+                                                                "notify::volume",
+                                                                G_CALLBACK (notify_volume_cb),
+                                                                self, G_CONNECT_SWAPPED);
+      self->notify_is_muted_handler_id = g_signal_connect_object (stream,
+                                                                  "notify::is-muted",
+                                                                  G_CALLBACK (notify_is_muted_cb),
+                                                                  self, G_CONNECT_SWAPPED);
+      notify_volume_cb (self);
+      notify_is_muted_cb (self);
+    }
+}
+
+void
+cc_volume_slider_set_is_amplified (CcVolumeSlider *self,
+                                   gboolean        is_amplified)
+{
+  g_return_if_fail (CC_IS_VOLUME_SLIDER (self));
+
+  gtk_scale_clear_marks (self->volume_scale);
+  if (is_amplified)
+    {
+      gtk_adjustment_set_upper (self->volume_adjustment, gvc_mixer_control_get_vol_max_amplified (NULL));
+      gtk_scale_add_mark (self->volume_scale,
+                          gvc_mixer_control_get_vol_max_norm (NULL),
+                          GTK_POS_BOTTOM,
+                          C_("volume", "100%"));
+    }
+  else
+    {
+      gtk_adjustment_set_upper (self->volume_adjustment, gvc_mixer_control_get_vol_max_norm (NULL));
+    }
+}
