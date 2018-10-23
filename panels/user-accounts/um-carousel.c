GNOME/gnome-control-center

commit 02559b0f697f1e87132adf68f01e81bda0696544

user-accounts: Don't use gtk_widget_show_all
It is removed in GTK+ 4

panels/user-accounts/um-carousel.c
@@ -305,12 +305,12 @@ um_carousel_add (GtkContainer *container,
 
                 page = g_strdup_printf ("%d", UM_CAROUSEL_ITEM (widget)->page);
                 self->last_box = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+                gtk_widget_show (self->last_box);
                 gtk_widget_set_valign (self->last_box, GTK_ALIGN_CENTER);
                 gtk_stack_add_named (self->stack, self->last_box, page);
         }
 
         gtk_box_pack_start (GTK_BOX (self->last_box), widget, TRUE, FALSE, 10);
-        gtk_widget_show_all (self->last_box);
 
         update_buttons_visibility (self);
 }

panels/user-accounts/um-fingerprint-dialog.c
@@ -730,7 +730,7 @@ enroll_fingerprints (GtkWindow *parent,
         g_object_set_data (G_OBJECT (WID("page3")), "name", "summary");
 
         data->ass = ass;
-        gtk_widget_show_all (ass);
+        gtk_widget_show (ass);
 }
 
 void

panels/user-accounts/um-history-dialog.c
@@ -180,14 +180,17 @@ add_record (GtkWidget *box, GDateTime *datetime, gchar *record_string, gint line
         str = g_strdup_printf(C_("login date-time", "%s, %s"), date, time);
 
         row = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 12);
+        gtk_widget_show (row);
         gtk_box_set_homogeneous (GTK_BOX (row), TRUE);
         gtk_container_set_border_width (GTK_CONTAINER (row), 6);
 
         label = gtk_label_new (record_string);
+        gtk_widget_show (label);
         gtk_widget_set_halign (label, GTK_ALIGN_START);
         gtk_box_pack_start (GTK_BOX (row), label, TRUE, TRUE, 0);
 
         label = gtk_label_new (str);
+        gtk_widget_show (label);
         gtk_widget_set_halign (label, GTK_ALIGN_START);
         gtk_box_pack_start (GTK_BOX (row), label, TRUE, TRUE, 0);
         g_free (str);
@@ -231,6 +234,7 @@ show_week (UmHistoryDialog *um)
 
         /* Add new session records */
         box = get_widget (um, "history-box");
+        gtk_widget_show (box);
         line = 0;
         for (;i >= 0; i--) {
                 history = g_array_index (login_history, UmLoginHistory, i);
@@ -257,8 +261,6 @@ show_week (UmHistoryDialog *um)
                         line++;
                 }
         }
-
-        gtk_widget_show_all (box);
 
         g_array_free (login_history, TRUE);
 }

panels/user-accounts/um-photo-dialog.c
@@ -113,7 +113,8 @@ um_photo_dialog_crop (UmPhotoDialog *um,
                           G_CALLBACK (crop_dialog_response), um);
 
         /* Content */
-        um->crop_area           = cc_crop_area_new ();
+        um->crop_area = cc_crop_area_new ();
+        gtk_widget_show (um->crop_area);
         cc_crop_area_set_min_size (CC_CROP_AREA (um->crop_area), 48, 48);
         cc_crop_area_set_constrain_aspect (CC_CROP_AREA (um->crop_area), TRUE);
         cc_crop_area_set_picture (CC_CROP_AREA (um->crop_area), pixbuf);
@@ -123,7 +124,7 @@ um_photo_dialog_crop (UmPhotoDialog *um,
 
         gtk_window_set_default_size (GTK_WINDOW (dialog), 400, 300);
 
-        gtk_widget_show_all (dialog);
+        gtk_widget_show (dialog);
 }
 
 static void

commit 368bdec5c75e375ad0f8ee623c479fdc0a0bd6dc

user-accounts: Fix user carousel not showing
This bug was introduced in 02559b0f697f1e87132adf68f01e81bda0696544

panels/user-accounts/um-carousel.c
@@ -310,6 +310,7 @@ um_carousel_add (GtkContainer *container,
                 gtk_stack_add_named (self->stack, self->last_box, page);
         }
 
+        gtk_widget_show_all (widget);
         gtk_box_pack_start (GTK_BOX (self->last_box), widget, TRUE, FALSE, 10);
 
         update_buttons_visibility (self);