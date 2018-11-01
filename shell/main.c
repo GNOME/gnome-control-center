c3cff35f9d69c7c106d5d2d1fb2f65f01e8c02af
Georges Basile Stavracas Neto <georges.stavracas@gmail.com>
2018-11-01 12:53:34 -0300

    main: Prevent running outside GNOME
    GNOME Settings relies on GNOME-specific services, and we cannot
    guarantee they'll be available on non-GNOME sessions.
    
    Thus, prevent Settings from running when the current desktop is
    not GNOME.
    
    Closes https://gitlab.gnome.org/GNOME/gnome-control-center/issues/248

--- shell/main.c
+++ shell/main.c
@@ -35,6 +35,12 @@
 
 #include "cc-application.h"
 
+static gboolean
+is_gnome (void)
+{
+  return g_strcmp0 (g_getenv ("XDG_CURRENT_DESKTOP"), "GNOME") == 0;
+}
+
 int
 main (int    argc,
       char **argv)
@@ -44,6 +50,13 @@ main (int    argc,
   bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
   bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
   textdomain (GETTEXT_PACKAGE);
+
+  /* Prevent running outside GNOME */
+  if (!is_gnome ())
+    {
+      g_critical (_("GNOME Settings cannot run outside the GNOME Desktop Environment, exiting"));
+      return 1;
+    }
 
 #ifdef GDK_WINDOWING_X11
   XInitThreads ();
