13bfd725cdd4db1f7dcea73bcca820344d2d5ba3
Robert Ancell <robert.ancell@canonical.com>
2018-11-08 14:17:43 +1300

    keyboard: Remove unused header file

--- panels/keyboard/gnome-keyboard-panel.h
+++
@@ -1,9 +0,0 @@
-/* This program was written with lots of love under the GPL by Jonathan
- * Blandford <jrb@gnome.org>
- */
-
-#include <gtk/gtk.h>
-#include <shell/cc-panel.h>
-
-void gnome_keybinding_properties_init (CcPanel *panel, GtkBuilder *builder);
-void gnome_keybinding_properties_dispose (CcPanel *panel);
