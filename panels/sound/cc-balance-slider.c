4f1d3ad3ff2baf28e52df19741d6f270f94c5b10
Robert Ancell <robert.ancell@canonical.com>
2018-11-19 12:57:52 +1300

    sound: Rename new sound panel to replace old one

--- panels/sound/cc-balance-slider.c
+++ panels/sound/cc-balance-slider.c
@@ -16,11 +16,11 @@
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "cc-sound-new-resources.h"
-#include "cc-fade-slider.h"
+#include "cc-sound-resources.h"
+#include "cc-balance-slider.h"
 #include "gvc-channel-map-private.h"
 
-struct _CcFadeSlider
+struct _CcBalanceSlider
 {
   GtkBox           parent_instance;
 
@@ -30,10 +30,10 @@ struct _CcFadeSlider
   guint            volume_changed_handler_id;
 };
 
-G_DEFINE_TYPE (CcFadeSlider, cc_fade_slider, GTK_TYPE_BOX)
+G_DEFINE_TYPE (CcBalanceSlider, cc_balance_slider, GTK_TYPE_BOX)
 
 static void
-changed_cb (CcFadeSlider *self)
+changed_cb (CcBalanceSlider *self)
 {
   gdouble               value;
   const pa_channel_map *pa_map;
@@ -45,59 +45,59 @@ changed_cb (CcFadeSlider *self)
   value = gtk_adjustment_get_value (self->adjustment);
   pa_map = gvc_channel_map_get_pa_channel_map (self->channel_map);
   pa_volume = *gvc_channel_map_get_cvolume (self->channel_map);
-  pa_cvolume_set_fade (&pa_volume, pa_map, value);
+  pa_cvolume_set_balance (&pa_volume, pa_map, value);
   gvc_channel_map_volume_changed (self->channel_map, &pa_volume, TRUE);
 }
 
 static void
-volume_changed_cb (CcFadeSlider *self)
+volume_changed_cb (CcBalanceSlider *self)
 {
   const gdouble *volumes;
 
   volumes = gvc_channel_map_get_volume (self->channel_map);
   g_signal_handlers_block_by_func (self->adjustment, volume_changed_cb, self);
-  gtk_adjustment_set_value (self->adjustment, volumes[FADE]);
+  gtk_adjustment_set_value (self->adjustment, volumes[BALANCE]);
   g_signal_handlers_unblock_by_func (self->adjustment, volume_changed_cb, self);
 }
 
 static void
-cc_fade_slider_dispose (GObject *object)
+cc_balance_slider_dispose (GObject *object)
 {
-  CcFadeSlider *self = CC_FADE_SLIDER (object);
+  CcBalanceSlider *self = CC_BALANCE_SLIDER (object);
 
   g_clear_object (&self->channel_map);
 
-  G_OBJECT_CLASS (cc_fade_slider_parent_class)->dispose (object);
+  G_OBJECT_CLASS (cc_balance_slider_parent_class)->dispose (object);
 }
 
 void
-cc_fade_slider_class_init (CcFadeSliderClass *klass)
+cc_balance_slider_class_init (CcBalanceSliderClass *klass)
 {
   GObjectClass   *object_class = G_OBJECT_CLASS (klass);
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
-  object_class->dispose = cc_fade_slider_dispose;
+  object_class->dispose = cc_balance_slider_dispose;
 
-  gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/control-center/sound-new/cc-fade-slider.ui");
+  gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/control-center/sound/cc-balance-slider.ui");
 
-  gtk_widget_class_bind_template_child (widget_class, CcFadeSlider, adjustment);
+  gtk_widget_class_bind_template_child (widget_class, CcBalanceSlider, adjustment);
 
   gtk_widget_class_bind_template_callback (widget_class, changed_cb);
 }
 
 void
-cc_fade_slider_init (CcFadeSlider *self)
+cc_balance_slider_init (CcBalanceSlider *self)
 {
-  g_resources_register (cc_sound_new_get_resource ());
+  g_resources_register (cc_sound_get_resource ());
 
   gtk_widget_init_template (GTK_WIDGET (self));
 }
 
 void
-cc_fade_slider_set_channel_map (CcFadeSlider  *self,
-                                GvcChannelMap *channel_map)
+cc_balance_slider_set_channel_map (CcBalanceSlider *self,
+                                   GvcChannelMap   *channel_map)
 {
-  g_return_if_fail (CC_IS_FADE_SLIDER (self));
+  g_return_if_fail (CC_IS_BALANCE_SLIDER (self));
 
   if (self->channel_map != NULL)
     {
