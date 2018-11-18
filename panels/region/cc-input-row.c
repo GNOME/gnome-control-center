d50001d560f1b9f225d8104f1ca8275ec4742d91
Robert Ancell <robert.ancell@canonical.com>
2018-11-19 11:30:41 +1300

    region: Make rows reorderable with drag and drop
    Drop toolbar - all functionality is now in rows.
    
    This matches the new designs in
    https://gitlab.gnome.org/Teams/Design/settings-mockups/blob/master/region-and-language/region-and-language.png

--- panels/region/cc-input-row.c
+++ panels/region/cc-input-row.c
@@ -25,8 +25,11 @@ struct _CcInputRow
 
   CcInputSource   *source;
 
+  GtkWidget       *drag_handle;
   GtkWidget       *name_label;
   GtkWidget       *settings_button;
+
+  GtkWidget       *drag_widget;
 };
 
 G_DEFINE_TYPE (CcInputRow, cc_input_row, GTK_TYPE_LIST_BOX_ROW)
@@ -35,11 +38,78 @@ enum
 {
   SIGNAL_SHOW_SETTINGS,
   SIGNAL_SHOW_LAYOUT,
+  SIGNAL_MOVE_ROW,
   SIGNAL_REMOVE_ROW,
   SIGNAL_LAST
 };
 
 static guint signals[SIGNAL_LAST] = { 0, };
+
+static void
+drag_begin_cb (CcInputRow     *row,
+               GdkDragContext *drag_context)
+{
+  GtkAllocation alloc;
+  gint x = 0, y = 0;
+
+  gtk_widget_get_allocation (GTK_WIDGET (row), &alloc);
+
+  gdk_window_get_device_position (gtk_widget_get_window (GTK_WIDGET (row)),
+                                  gdk_drag_context_get_device (drag_context),
+                                  &x, &y, NULL);
+
+  row->drag_widget = gtk_list_box_new ();
+  gtk_widget_show (GTK_WIDGET (row->drag_widget));
+  gtk_widget_set_size_request (row->drag_widget, alloc.width, alloc.height);
+  CcInputRow *drag_row = cc_input_row_new (row->source);
+  gtk_widget_show (GTK_WIDGET (drag_row));
+  gtk_container_add (GTK_CONTAINER (row->drag_widget), GTK_WIDGET (drag_row));
+  gtk_list_box_drag_highlight_row (GTK_LIST_BOX (row->drag_widget), GTK_LIST_BOX_ROW (drag_row));
+
+  gtk_drag_set_icon_widget (drag_context, GTK_WIDGET (row->drag_widget), x - alloc.x, y - alloc.y);
+}
+
+static void
+drag_end_cb (CcInputRow *row)
+{
+  g_clear_pointer (&row->drag_widget, gtk_widget_destroy);
+}
+
+static void
+drag_data_get_cb (CcInputRow       *row,
+                  GdkDragContext   *context,
+                  GtkSelectionData *selection_data,
+                  guint             info,
+                  guint             time_)
+{
+  gtk_selection_data_set (selection_data,
+                          gdk_atom_intern_static_string ("GTK_LIST_BOX_ROW"),
+                          32,
+                          (const guchar *)&row,
+                          sizeof (gpointer));
+}
+
+static void
+drag_data_received_cb (CcInputRow       *row,
+                       GdkDragContext   *context,
+                       gint              x,
+                       gint              y,
+                       GtkSelectionData *selection_data,
+                       guint             info,
+                       guint             time_)
+{
+  CcInputRow *source;
+
+  source = *((CcInputRow **) gtk_selection_data_get_data (selection_data));
+
+  if (source == row)
+    return;
+
+  g_signal_emit (source,
+                 signals[SIGNAL_MOVE_ROW],
+                 0,
+                 row);
+}
 
 static void
 settings_button_clicked_cb (CcInputRow *row)
@@ -88,6 +158,10 @@ cc_input_row_class_init (CcInputRowClass *klass)
   gtk_widget_class_bind_template_child (widget_class, CcInputRow, name_label);
   gtk_widget_class_bind_template_child (widget_class, CcInputRow, settings_button);
 
+  gtk_widget_class_bind_template_callback (widget_class, drag_data_get_cb);
+  gtk_widget_class_bind_template_callback (widget_class, drag_begin_cb);
+  gtk_widget_class_bind_template_callback (widget_class, drag_end_cb);
+  gtk_widget_class_bind_template_callback (widget_class, drag_data_received_cb);
   gtk_widget_class_bind_template_callback (widget_class, layout_button_clicked_cb);
   gtk_widget_class_bind_template_callback (widget_class, settings_button_clicked_cb);
   gtk_widget_class_bind_template_callback (widget_class, remove_button_clicked_cb);
@@ -112,6 +186,16 @@ cc_input_row_class_init (CcInputRowClass *klass)
                   G_TYPE_NONE,
                   0);
 
+  signals[SIGNAL_MOVE_ROW] =
+    g_signal_new ("move-row",
+                  G_TYPE_FROM_CLASS (object_class),
+                  G_SIGNAL_RUN_LAST,
+                  0,
+                  NULL, NULL,
+                  NULL,
+                  G_TYPE_NONE,
+                  1, CC_TYPE_INPUT_ROW);
+
   signals[SIGNAL_REMOVE_ROW] =
     g_signal_new ("remove-row",
                   G_TYPE_FROM_CLASS (object_class),
@@ -123,10 +207,18 @@ cc_input_row_class_init (CcInputRowClass *klass)
                   0);
 }
 
+static GtkTargetEntry entries[] =
+{
+  { "GTK_LIST_BOX_ROW", GTK_TARGET_SAME_APP, 0 }
+};
+
 void
 cc_input_row_init (CcInputRow *row)
 {
   gtk_widget_init_template (GTK_WIDGET (row));
+
+  gtk_drag_source_set (row->drag_handle, GDK_BUTTON1_MASK, entries, 1, GDK_ACTION_MOVE);
+  gtk_drag_dest_set (GTK_WIDGET (row), GTK_DEST_DEFAULT_ALL, entries, 1, GDK_ACTION_MOVE);
 }
 
 static void
