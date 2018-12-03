bfe554418d5abd5c9ca124ce7a322f249db817db
Robert Ancell <robert.ancell@canonical.com>
2018-12-04 10:46:35 +1300

    printers: Use self variable to match coding style

--- panels/printers/pp-printer.c
+++ panels/printers/pp-printer.c
@@ -115,7 +115,7 @@ pp_printer_class_init (PpPrinterClass *klass)
 }
 
 static void
-pp_printer_init (PpPrinter *printer)
+pp_printer_init (PpPrinter *self)
 {
 }
 
@@ -133,18 +133,18 @@ printer_rename_thread (GTask        *task,
                        gpointer      task_data,
                        GCancellable *cancellable)
 {
-  PpPrinter *printer = PP_PRINTER (source_object);
+  PpPrinter *self = PP_PRINTER (source_object);
   gboolean   result;
   gchar     *new_printer_name = task_data;
   gchar     *old_printer_name;
 
-  g_object_get (printer, "printer-name", &old_printer_name, NULL);
+  g_object_get (self, "printer-name", &old_printer_name, NULL);
 
   result = printer_rename (old_printer_name, new_printer_name);
 
   if (result)
     {
-      g_object_set (printer, "printer-name", new_printer_name, NULL);
+      g_object_set (self, "printer-name", new_printer_name, NULL);
     }
 
   g_free (old_printer_name);
@@ -157,7 +157,7 @@ printer_rename_dbus_cb (GObject      *source_object,
                         GAsyncResult *res,
                         gpointer      user_data)
 {
-  PpPrinter        *printer;
+  PpPrinter        *self;
   GVariant         *output;
   gboolean          result = FALSE;
   g_autoptr(GError) error = NULL;
@@ -173,8 +173,8 @@ printer_rename_dbus_cb (GObject      *source_object,
     {
       const gchar *ret_error;
 
-      printer = g_task_get_source_object (task);
-      g_object_get (printer, "printer-name", &old_printer_name, NULL);
+      self = g_task_get_source_object (task);
+      g_object_get (self, "printer-name", &old_printer_name, NULL);
 
       g_variant_get (output, "(&s)", &ret_error);
       if (ret_error[0] != '\0')
@@ -184,7 +184,7 @@ printer_rename_dbus_cb (GObject      *source_object,
       else
         {
           result = TRUE;
-          g_object_set (printer, "printer-name", g_task_get_task_data (task), NULL);
+          g_object_set (self, "printer-name", g_task_get_task_data (task), NULL);
         }
 
       g_task_return_boolean (task, result);
@@ -250,7 +250,7 @@ get_bus_cb (GObject      *source_object,
 }
 
 void
-pp_printer_rename_async (PpPrinter           *printer,
+pp_printer_rename_async (PpPrinter           *self,
                          const gchar         *new_printer_name,
                          GCancellable        *cancellable,
                          GAsyncReadyCallback  callback,
@@ -260,7 +260,7 @@ pp_printer_rename_async (PpPrinter           *printer,
 
   g_return_if_fail (new_printer_name != NULL);
 
-  task = g_task_new (G_OBJECT (printer), cancellable, callback, user_data);
+  task = g_task_new (G_OBJECT (self), cancellable, callback, user_data);
   g_task_set_task_data (task, g_strdup (new_printer_name), g_free);
 
   g_bus_get (G_BUS_TYPE_SYSTEM,
@@ -270,11 +270,11 @@ pp_printer_rename_async (PpPrinter           *printer,
 }
 
 gboolean
-pp_printer_rename_finish (PpPrinter     *printer,
+pp_printer_rename_finish (PpPrinter     *self,
                           GAsyncResult  *res,
                           GError       **error)
 {
-  g_return_val_if_fail (g_task_is_valid (res, printer), FALSE);
+  g_return_val_if_fail (g_task_is_valid (res, self), FALSE);
   g_object_unref (res);
 
   return g_task_propagate_boolean (G_TASK (res), error);
@@ -296,7 +296,7 @@ get_jobs_thread (GTask        *task,
   static gchar    *printer_attributes[] = { "auth-info-required" };
   GetJobsData     *get_jobs_data = task_data;
   cups_job_t      *jobs = NULL;
-  PpPrinter       *printer = PP_PRINTER (source_object);
+  PpPrinter       *self = PP_PRINTER (source_object);
   gboolean         auth_info_is_required;
   PpJob           *job;
   ipp_t           *job_request;
@@ -311,7 +311,7 @@ get_jobs_thread (GTask        *task,
   gint             num_jobs;
   gint             i, j;
 
-  g_object_get (printer, "printer-name", &printer_name, NULL);
+  g_object_get (self, "printer-name", &printer_name, NULL);
 
   num_jobs = cupsGetJobs (&jobs,
                           printer_name,
@@ -398,7 +398,7 @@ get_jobs_thread (GTask        *task,
 }
 
 void
-pp_printer_get_jobs_async (PpPrinter           *printer,
+pp_printer_get_jobs_async (PpPrinter           *self,
                            gboolean             myjobs,
                            gint                 which_jobs,
                            GCancellable        *cancellable,
@@ -412,7 +412,7 @@ pp_printer_get_jobs_async (PpPrinter           *printer,
   get_jobs_data->myjobs = myjobs;
   get_jobs_data->which_jobs = which_jobs;
 
-  task = g_task_new (G_OBJECT (printer), cancellable, callback, user_data);
+  task = g_task_new (G_OBJECT (self), cancellable, callback, user_data);
   g_task_set_task_data (task, get_jobs_data, g_free);
   g_task_set_return_on_cancel (task, TRUE);
   g_task_run_in_thread (task, get_jobs_thread);
@@ -420,11 +420,11 @@ pp_printer_get_jobs_async (PpPrinter           *printer,
 }
 
 GList *
-pp_printer_get_jobs_finish (PpPrinter          *printer,
+pp_printer_get_jobs_finish (PpPrinter          *self,
                             GAsyncResult       *res,
                             GError            **error)
 {
-  g_return_val_if_fail (g_task_is_valid (res, printer), NULL);
+  g_return_val_if_fail (g_task_is_valid (res, self), NULL);
 
   return g_task_propagate_pointer (G_TASK (res), error);
 }
@@ -510,14 +510,14 @@ pp_printer_delete_cb (GObject      *source_object,
 }
 
 void
-pp_printer_delete_async (PpPrinter           *printer,
+pp_printer_delete_async (PpPrinter           *self,
                          GCancellable        *cancellable,
                          GAsyncReadyCallback  callback,
                          gpointer             user_data)
 {
   GTask *task;
 
-  task = g_task_new (G_OBJECT (printer), cancellable, callback, user_data);
+  task = g_task_new (G_OBJECT (self), cancellable, callback, user_data);
 
   g_bus_get (G_BUS_TYPE_SYSTEM,
              cancellable,
@@ -526,11 +526,11 @@ pp_printer_delete_async (PpPrinter           *printer,
 }
 
 gboolean
-pp_printer_delete_finish (PpPrinter     *printer,
+pp_printer_delete_finish (PpPrinter     *self,
                           GAsyncResult  *res,
                           GError       **error)
 {
-  g_return_val_if_fail (g_task_is_valid (res, printer), FALSE);
+  g_return_val_if_fail (g_task_is_valid (res, self), FALSE);
 
   return g_task_propagate_boolean (G_TASK (res), error);
 }
@@ -556,11 +556,11 @@ print_file_thread (GTask        *task,
                    gpointer      task_data,
                    GCancellable *cancellable)
 {
+  PpPrinter    *self = PP_PRINTER (source_object);
   PrintFileData *print_file_data;
   cups_ptype_t  type = 0;
   cups_dest_t  *dest = NULL;
   const gchar  *printer_type = NULL;
-  PpPrinter    *printer;
   gboolean      ret = FALSE;
   gchar        *printer_name = NULL;
   gchar        *printer_uri = NULL;
@@ -568,9 +568,7 @@ print_file_thread (GTask        *task,
   ipp_t        *response = NULL;
   ipp_t        *request;
 
-  printer = PP_PRINTER (source_object);
-
-  g_object_get (printer, "printer-name", &printer_name, NULL);
+  g_object_get (self, "printer-name", &printer_name, NULL);
   dest = cupsGetNamedDest (CUPS_HTTP_DEFAULT, printer_name, NULL);
   if (dest != NULL)
     {
@@ -626,7 +624,7 @@ print_file_thread (GTask        *task,
 }
 
 void
-pp_printer_print_file_async (PpPrinter           *printer,
+pp_printer_print_file_async (PpPrinter           *self,
                              const gchar         *filename,
                              const gchar         *job_name,
                              GCancellable        *cancellable,
@@ -640,7 +638,7 @@ pp_printer_print_file_async (PpPrinter           *printer,
   print_file_data->filename = g_strdup (filename);
   print_file_data->job_name = g_strdup (job_name);
 
-  task = g_task_new (G_OBJECT (printer), cancellable, callback, user_data);
+  task = g_task_new (G_OBJECT (self), cancellable, callback, user_data);
 
   g_task_set_return_on_cancel (task, TRUE);
   g_task_set_task_data (task, print_file_data, (GDestroyNotify) print_file_data_free);
@@ -650,11 +648,11 @@ pp_printer_print_file_async (PpPrinter           *printer,
 }
 
 gboolean
-pp_printer_print_file_finish (PpPrinter     *printer,
+pp_printer_print_file_finish (PpPrinter     *self,
                               GAsyncResult  *res,
                               GError       **error)
 {
-  g_return_val_if_fail (g_task_is_valid (res, printer), FALSE);
+  g_return_val_if_fail (g_task_is_valid (res, self), FALSE);
 
   return g_task_propagate_boolean (G_TASK (res), error);
 }
