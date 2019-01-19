cec0f8cd9de94da0f7bcb69882505735eb470f06
Benjamin Berg <bberg@redhat.com>
2019-01-19 23:34:40 +0100

    display: Make display settings properties runtime configurable
    NOTE: This commit breaks the build as the cc_display_settings_new routine
    is changed but the panel file is not updated at the same time.

--- panels/display/cc-display-settings.h
+++ panels/display/cc-display-settings.h
@@ -28,12 +28,17 @@ G_BEGIN_DECLS
 #define CC_TYPE_DISPLAY_SETTINGS cc_display_settings_get_type ()
 G_DECLARE_FINAL_TYPE (CcDisplaySettings, cc_display_settings, CC, DISPLAY_SETTINGS, GtkListBox);
 
-CcDisplaySettings*  cc_display_settings_new                 (CcDisplayConfig      *config,
-                                                             gboolean              has_accelerometer);
+CcDisplaySettings*  cc_display_settings_new                 (void);
 
-CcDisplayMonitor*   cc_display_settings_get_selected_output (CcDisplaySettings    *settings);
-void                cc_display_settings_set_selected_output (CcDisplaySettings    *settings,
-                                                             CcDisplayMonitor     *output);
+gboolean            cc_display_settings_get_has_accelerometer (CcDisplaySettings    *settings);
+void                cc_display_settings_set_has_accelerometer (CcDisplaySettings    *settings,
+                                                               gboolean              has_accelerometer);
+CcDisplayConfig*    cc_display_settings_get_config            (CcDisplaySettings    *settings);
+void                cc_display_settings_set_config            (CcDisplaySettings    *settings,
+                                                               CcDisplayConfig      *config);
+CcDisplayMonitor*   cc_display_settings_get_selected_output   (CcDisplaySettings    *settings);
+void                cc_display_settings_set_selected_output   (CcDisplaySettings    *settings,
+                                                               CcDisplayMonitor     *output);
 
 G_END_DECLS
 
