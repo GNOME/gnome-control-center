79533395874a57cebd521e3e4f6aa5a47b9a4fe3
Adrien Plazas <kekun.plazas@laposte.net>
2019-01-27 18:06:05 +0100

    network: Make the rows adaptive
    This factorizes the row creation a bit and normalizes the margins and
    spacing, reducing the required width. This also makes labels like row
    titles and descriptions ellipsizable so the rows can reach narrower
    widths.

--- panels/network/net-device-ethernet.c
+++ panels/network/net-device-ethernet.c
@@ -320,8 +320,8 @@ add_row (NetDeviceEthernet *device, NMConnection *connection)
         widget = gtk_label_new (nm_connection_get_id (connection));
         gtk_widget_set_margin_start (widget, 12);
         gtk_widget_set_margin_end (widget, 12);
-        gtk_widget_set_margin_top (widget, 12);
-        gtk_widget_set_margin_bottom (widget, 12);
+        gtk_widget_set_margin_top (widget, 8);
+        gtk_widget_set_margin_bottom (widget, 8);
         gtk_box_pack_start (GTK_BOX (box), widget, FALSE, TRUE, 0);
 
         if (active) {
@@ -350,8 +350,8 @@ add_row (NetDeviceEthernet *device, NMConnection *connection)
         gtk_style_context_add_class (gtk_widget_get_style_context (widget), "image-button");
         gtk_widget_set_margin_start (widget, 12);
         gtk_widget_set_margin_end (widget, 12);
-        gtk_widget_set_margin_top (widget, 12);
-        gtk_widget_set_margin_bottom (widget, 12);
+        gtk_widget_set_margin_top (widget, 8);
+        gtk_widget_set_margin_bottom (widget, 8);
         gtk_widget_show (widget);
         gtk_container_add (GTK_CONTAINER (widget), image);
         gtk_widget_set_halign (widget, GTK_ALIGN_CENTER);
