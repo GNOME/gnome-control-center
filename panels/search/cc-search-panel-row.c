eaeb87c0c12ad414f3df6adb70101a68be1985b1
Felipe Borges <felipeborges@gnome.org>
2019-07-02 16:02:07 +0200

    search: Make rows reordable with drag and drop
    Fixes #589

--- panels/search/cc-search-panel-row.c
+++ panels/search/cc-search-panel-row.c
@@ -21,7 +21,139 @@
 
 #include "cc-search-panel-row.h"
 
+struct _CcSearchPanelRow
+{
+  GtkListBoxRow  parent_instance;
+
+  GAppInfo      *app_info;
+
+  GtkEventBox   *drag_handle;
+  GtkImage      *icon;
+  GtkLabel      *app_name;
+  GtkSwitch     *switcher;
+
+  GtkListBox    *drag_widget;
+};
+
 G_DEFINE_TYPE (CcSearchPanelRow, cc_search_panel_row, GTK_TYPE_LIST_BOX_ROW)
+
+enum
+{
+  SIGNAL_MOVE_ROW,
+  SIGNAL_LAST
+};
+
+static guint signals[SIGNAL_LAST] = { 0, };
+
+static void
+drag_begin_cb (CcSearchPanelRow *self,
+               GdkDragContext   *drag_context)
+{
+  CcSearchPanelRow *drag_row;
+  GtkAllocation alloc;
+  gint x = 0, y = 0;
+
+  gtk_widget_get_allocation (GTK_WIDGET (self), &alloc);
+
+  gdk_window_get_device_position (gtk_widget_get_window (GTK_WIDGET (self)),
+                                  gdk_drag_context_get_device (drag_context),
+                                  &x, &y, NULL);
+
+  self->drag_widget = GTK_LIST_BOX (gtk_list_box_new ());
+  gtk_widget_show (GTK_WIDGET (self->drag_widget));
+  gtk_widget_set_size_request (GTK_WIDGET (self->drag_widget), alloc.width, alloc.height);
+
+  drag_row = cc_search_panel_row_new (self->app_info);
+  gtk_widget_show (GTK_WIDGET (drag_row));
+  gtk_container_add (GTK_CONTAINER (self->drag_widget), GTK_WIDGET (drag_row));
+  gtk_list_box_drag_highlight_row (self->drag_widget, GTK_LIST_BOX_ROW (drag_row));
+
+  gtk_drag_set_icon_widget (drag_context, GTK_WIDGET (self->drag_widget), x - alloc.x, y - alloc.y);
+}
+
+static void
+drag_end_cb (CcSearchPanelRow *self)
+{
+  g_clear_pointer ((GtkWidget **) &self->drag_widget, gtk_widget_destroy);
+}
+
+static void
+drag_data_get_cb (CcSearchPanelRow *self,
+                  GdkDragContext   *context,
+                  GtkSelectionData *selection_data,
+                  guint             info,
+                  guint             time_)
+{
+  gtk_selection_data_set (selection_data,
+                          gdk_atom_intern_static_string ("GTK_LIST_BOX_ROW"),
+                          32,
+                          (const guchar *)&self,
+                          sizeof (gpointer));
+}
+
+static void
+drag_data_received_cb (CcSearchPanelRow *self,
+                       GdkDragContext   *context,
+                       gint              x,
+                       gint              y,
+                       GtkSelectionData *selection_data,
+                       guint             info,
+                       guint             time_)
+{
+  CcSearchPanelRow *source;
+
+  source = *((CcSearchPanelRow **) gtk_selection_data_get_data (selection_data));
+  if (source == self)
+    return;
+
+  g_signal_emit (source,
+                 signals[SIGNAL_MOVE_ROW],
+                 0,
+                 self);
+}
+
+static GtkTargetEntry entries[] =
+{
+  { "GTK_LIST_BOX_ROW", GTK_TARGET_SAME_APP, 0 }
+};
+
+static void
+cc_search_panel_row_class_init (CcSearchPanelRowClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/control-center/search/cc-search-panel-row.ui");
+
+  gtk_widget_class_bind_template_child (widget_class, CcSearchPanelRow, drag_handle);
+  gtk_widget_class_bind_template_child (widget_class, CcSearchPanelRow, icon);
+  gtk_widget_class_bind_template_child (widget_class, CcSearchPanelRow, app_name);
+  gtk_widget_class_bind_template_child (widget_class, CcSearchPanelRow, switcher);
+
+  gtk_widget_class_bind_template_callback (widget_class, drag_begin_cb);
+  gtk_widget_class_bind_template_callback (widget_class, drag_end_cb);
+  gtk_widget_class_bind_template_callback (widget_class, drag_data_get_cb);
+  gtk_widget_class_bind_template_callback (widget_class, drag_data_received_cb);
+
+  signals[SIGNAL_MOVE_ROW] =
+    g_signal_new ("move-row",
+                  G_TYPE_FROM_CLASS (object_class),
+                  G_SIGNAL_RUN_LAST,
+                  0,
+                  NULL, NULL,
+                  NULL,
+                  G_TYPE_NONE,
+                  1, CC_TYPE_SEARCH_PANEL_ROW);
+}
+
+static void
+cc_search_panel_row_init (CcSearchPanelRow *self)
+{
+  gtk_widget_init_template (GTK_WIDGET (self));
+
+  gtk_drag_source_set (GTK_WIDGET (self->drag_handle), GDK_BUTTON1_MASK, entries, 1, GDK_ACTION_MOVE);
+  gtk_drag_dest_set (GTK_WIDGET (self), GTK_DEST_DEFAULT_ALL, entries, 1, GDK_ACTION_MOVE);
+}
 
 CcSearchPanelRow *
 cc_search_panel_row_new (GAppInfo *app_info)
@@ -44,23 +176,19 @@ cc_search_panel_row_new (GAppInfo *app_info)
 
   gtk_label_set_text (self->app_name, g_app_info_get_name (app_info));
 
+  gtk_drag_source_set (GTK_WIDGET (self->drag_handle), GDK_BUTTON1_MASK, entries, 1, GDK_ACTION_MOVE);
+
   return self;
 }
 
-static void
-cc_search_panel_row_class_init (CcSearchPanelRowClass *klass)
+GAppInfo *
+cc_search_panel_row_get_app_info (CcSearchPanelRow *row)
 {
-  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
-
-  gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/control-center/search/cc-search-panel-row.ui");
-
-  gtk_widget_class_bind_template_child (widget_class, CcSearchPanelRow, icon);
-  gtk_widget_class_bind_template_child (widget_class, CcSearchPanelRow, app_name);
-  gtk_widget_class_bind_template_child (widget_class, CcSearchPanelRow, switcher);
+    return row->app_info;
 }
 
-static void
-cc_search_panel_row_init (CcSearchPanelRow *self)
+GtkWidget *
+cc_search_panel_row_get_switch (CcSearchPanelRow *row)
 {
-  gtk_widget_init_template (GTK_WIDGET (self));
+    return GTK_WIDGET (row->switcher);
 }
