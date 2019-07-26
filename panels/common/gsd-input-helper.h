7e6301d449d5e4615835b4f019156a59c2104c5d
Carlos Garnacho <carlosg@gnome.org>
2019-07-27 00:27:02 +0200

    common: Remove unused X11/XI1/XI2 API
    Those went unused over time. Time to clean those up.

--- panels/common/gsd-input-helper.h
+++ panels/common/gsd-input-helper.h
@@ -22,48 +22,10 @@ G_BEGIN_DECLS
 
 #include <glib.h>
 
-#include <X11/extensions/XInput.h>
-#include <X11/extensions/XIproto.h>
-
-#define WACOM_SERIAL_IDS_PROP "Wacom Serial IDs"
-
-/* Generic property setting code. Fill up the struct property with the property
- * data and pass it into device_set_property together with the device to be
- * changed.  Note: doesn't cater for non-zero offsets yet, but we don't have
- * any settings that require that.
- */
-typedef struct {
-        const char *name;       /* property name */
-        gint nitems;            /* number of items in data */
-        gint format;            /* CARD8 or CARD32 sized-items */
-        gint type;              /* Atom representing data type */
-        union {
-                const gchar *c; /* 8 bit data */
-                const gint *i;  /* 32 bit data */
-        } data;
-} PropertyHelper;
-
-gboolean  supports_xinput2_devices (int *opcode);
-gboolean  supports_xtest           (void);
-
-gboolean set_device_enabled       (int device_id,
-                                   gboolean enabled);
-
 gboolean  touchpad_is_present     (void);
 gboolean  touchscreen_is_present  (void);
 gboolean  mouse_is_present        (void);
 
-gboolean  device_set_property     (XDevice                *xdevice,
-                                   const char             *device_name,
-                                   PropertyHelper         *property);
-
 char *    xdevice_get_device_node  (int                     deviceid);
-int       xdevice_get_last_tool_id (int                     deviceid);
-gboolean  xdevice_get_dimensions   (int                     deviceid,
-                                    guint                  *width,
-                                    guint                  *height);
-void      xdevice_close      (XDevice                *xdevice);
-
-const char * xdevice_get_wacom_tool_type (int               deviceid);
 
 G_END_DECLS
