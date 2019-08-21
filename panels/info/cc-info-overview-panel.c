GNOME/gnome-control-center

commit 331f0d119b99baf3f2d1b7ddcf97bf8b435c9ec8

info panel: return "Unknown" on OS lacking os-release
Return "Unknown" instead of the empty string if os_info fails.

panels/info/cc-info-overview-panel.c
@@ -437,7 +437,7 @@ get_os_name (void)
   os_info = get_os_info ();
 
   if (!os_info)
-    return NULL;
+    g_strdup (_("Unknown"));
 
   name = g_hash_table_lookup (os_info, "NAME");
   version_id = g_hash_table_lookup (os_info, "VERSION_ID");

commit c3d33581304ad0df6c9c222600fe2ba8233cd33f

info: Fix leak when /etc/os-release not found
There is a missing return statement, this was broken in 331f0d1

panels/info/cc-info-overview-panel.c
@@ -437,7 +437,7 @@ get_os_name (void)
   os_info = get_os_info ();
 
   if (!os_info)
-    g_strdup (_("Unknown"));
+    return g_strdup (_("Unknown"));
 
   name = g_hash_table_lookup (os_info, "NAME");
   version_id = g_hash_table_lookup (os_info, "VERSION_ID");