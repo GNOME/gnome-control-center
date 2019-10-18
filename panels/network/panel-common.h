75b4e5e061599d4f0b725c33ed3239da4ff1c7a9
Robert Ancell <robert.ancell@canonical.com>
2019-10-18 15:03:20 +1300

    network: Simplify helper function

--- panels/network/panel-common.h
+++ panels/network/panel-common.h
@@ -28,7 +28,7 @@ G_BEGIN_DECLS
 
 gchar           *panel_device_status_to_localized_string       (NMDevice *nm_device,
                                                                 const gchar *speed);
-gchar           *panel_get_ip4_address_as_string               (NMIPConfig *config, const gchar *what);
+gchar           *panel_get_ip4_address_as_string               (NMIPConfig *config);
 gchar           *panel_get_ip4_dns_as_string                   (NMIPConfig *config);
 gchar           *panel_get_ip6_address_as_string               (NMIPConfig *config);
 
