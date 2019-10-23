06e38dac90a66c93cdfe0025cf842fd26e14d691
Robert Ancell <robert.ancell@canonical.com>
2019-10-23 14:56:18 +1300

    network: Tidy up use of modem object in NetDeviceMobile

--- panels/network/net-device-mobile.h
+++ panels/network/net-device-mobile.h
@@ -31,7 +31,8 @@ G_BEGIN_DECLS
 #define NET_TYPE_DEVICE_MOBILE (net_device_mobile_get_type ())
 G_DECLARE_FINAL_TYPE (NetDeviceMobile, net_device_mobile, NET, DEVICE_MOBILE, NetDevice)
 
-NetDeviceMobile *net_device_mobile_new (NMClient *client,
-                                        NMDevice *device);
+NetDeviceMobile *net_device_mobile_new (NMClient    *client,
+                                        NMDevice    *device,
+                                        GDBusObject *modem);
 
 G_END_DECLS
