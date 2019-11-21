b32eab65a60f14b3995e184589d97e08c393d358
Robert Ancell <robert.ancell@canonical.com>
2019-11-21 20:32:51 +1300

    background: Replace ifdefs with #pragma once

--- panels/background/cc-background-panel.h
+++ panels/background/cc-background-panel.h
@@ -18,9 +18,7 @@
  *
  */
 
-
-#ifndef _CC_BACKGROUND_PANEL_H
-#define _CC_BACKGROUND_PANEL_H
+#pragma once
 
 #include <shell/cc-panel.h>
 
@@ -30,5 +28,3 @@ G_BEGIN_DECLS
 G_DECLARE_FINAL_TYPE (CcBackgroundPanel, cc_background_panel, CC, BACKGROUND_PANEL, CcPanel)
 
 G_END_DECLS
-
-#endif /* _CC_BACKGROUND_PANEL_H */
