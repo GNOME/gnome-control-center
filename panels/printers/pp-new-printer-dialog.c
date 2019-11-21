3c20626abe3971cb899bff720f9fb62a711fe12f
Robert Ancell <robert.ancell@canonical.com>
2019-11-21 21:52:09 +1300

    printers: Replace explicit g_variant_unref calls with g_autoptr

--- panels/printers/pp-new-printer-dialog.c
+++ panels/printers/pp-new-printer-dialog.c
@@ -929,7 +929,7 @@ group_physical_devices_dbus_cb (GObject      *source_object,
 
   if (output)
     {
-      GVariant *array;
+      g_autoptr(GVariant) array = NULL;
 
       g_variant_get (output, "(@aas)", &array);
 
@@ -946,8 +946,6 @@ group_physical_devices_dbus_cb (GObject      *source_object,
               result[i] = device_uris;
               i++;
             }
-
-          g_variant_unref (array);
         }
     }
   else if (error &&
