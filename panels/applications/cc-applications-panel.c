c09652d85f42ce4e706aa7735540a0886d6ece2d
Robert Ancell <robert.ancell@canonical.com>
2019-12-16 15:03:32 +1300

    applications: Hide buttons that launch gnome-software if it is not available
    Fixes #417

--- panels/applications/cc-applications-panel.c
+++ panels/applications/cc-applications-panel.c
@@ -69,6 +69,7 @@ struct _CcApplicationsPanel
   GSettings       *search_settings;
 
   GtkListBox      *stack;
+  GtkWidget       *install_button;
 
   GtkWidget       *permission_section;
   GtkWidget       *permission_list;
@@ -132,6 +133,13 @@ enum
   PROP_0,
   PROP_PARAMETERS
 };
+
+static gboolean
+gnome_software_is_installed (void)
+{
+  g_autofree gchar *path = g_find_program_in_path ("gnome-software");
+  return path != NULL;
+}
 
 /* Callbacks */
 
@@ -1414,7 +1422,7 @@ update_panel (CcApplicationsPanel *self,
 
   gtk_label_set_label (GTK_LABEL (self->title_label), g_app_info_get_display_name (info));
   gtk_stack_set_visible_child_name (GTK_STACK (self->stack), "settings");
-  gtk_widget_show (self->header_button);
+  gtk_widget_set_visible (self->header_button, gnome_software_is_installed ());
 
   g_clear_pointer (&self->current_app_id, g_free);
   g_clear_pointer (&self->current_flatpak_id, g_free);
@@ -1707,6 +1715,7 @@ cc_applications_panel_class_init (CcApplicationsPanelClass *klass)
   gtk_widget_class_bind_template_child (widget_class, CcApplicationsPanel, handler_section);
   gtk_widget_class_bind_template_child (widget_class, CcApplicationsPanel, handler_reset);
   gtk_widget_class_bind_template_child (widget_class, CcApplicationsPanel, handler_list);
+  gtk_widget_class_bind_template_child (widget_class, CcApplicationsPanel, install_button);
   gtk_widget_class_bind_template_child (widget_class, CcApplicationsPanel, integration_list);
   gtk_widget_class_bind_template_child (widget_class, CcApplicationsPanel, integration_section);
   gtk_widget_class_bind_template_child (widget_class, CcApplicationsPanel, location);
@@ -1762,6 +1771,8 @@ cc_applications_panel_init (CcApplicationsPanel *self)
   g_resources_register (cc_applications_get_resource ());
 
   gtk_widget_init_template (GTK_WIDGET (self));
+
+  gtk_widget_set_visible (self->install_button, gnome_software_is_installed ());
 
   self->cancellable = g_cancellable_new ();
 
