9f9895f8de89af7de88fc74543dabb4f0717bb60
Adrien Plazas <kekun.plazas@laposte.net>
2020-03-06 09:29:38 +0100

    window: Use HdyLeaflet:folded
    Replace the dropped fold enum property by the folded boolean one, and
    directly bind booleans in UI files where possible.

--- shell/cc-window.c
+++ shell/cc-window.c
@@ -514,31 +514,28 @@ switch_to_previous_panel (CcWindow *self)
 
 /* Callbacks */
 static void
-update_fold_state (CcWindow *self)
+update_folded (CcWindow *self)
 {
   GtkWidget *header_child = hdy_leaflet_get_visible_child (self->header_box);
-  HdyFold fold = hdy_leaflet_get_fold (self->header_box);
+  gboolean folded = hdy_leaflet_get_folded (self->header_box);
 
-  hdy_header_group_set_focus (self->header_group, fold == HDY_FOLD_FOLDED ? GTK_HEADER_BAR (header_child) : NULL);
-
-  gtk_widget_set_visible (GTK_WIDGET (self->back_revealer), fold == HDY_FOLD_FOLDED);
-  gtk_revealer_set_reveal_child (self->back_revealer, fold == HDY_FOLD_FOLDED);
+  hdy_header_group_set_focus (self->header_group, folded ? GTK_HEADER_BAR (header_child) : NULL);
 }
 
 static void
 notify_header_visible_child_cb (CcWindow *self)
 {
-  update_fold_state (self);
+  update_folded (self);
 }
 
 static void
-notify_fold_cb (CcWindow *self)
+notify_folded_cb (CcWindow *self)
 {
-  update_fold_state (self);
+  update_folded (self);
 }
 
 static void
-on_main_leaflet_fold_changed_cb (CcWindow *self)
+on_main_leaflet_folded_changed_cb (CcWindow *self)
 {
   GtkSelectionMode selection_mode;
 
@@ -546,7 +543,7 @@ on_main_leaflet_fold_changed_cb (CcWindow *self)
 
   selection_mode = GTK_SELECTION_SINGLE;
 
-  if (hdy_leaflet_get_fold (self->main_leaflet) == HDY_FOLD_FOLDED)
+  if (hdy_leaflet_get_folded (self->main_leaflet))
     selection_mode = GTK_SELECTION_NONE;
 
   cc_panel_list_set_selection_mode (self->panel_list, selection_mode);
@@ -920,8 +917,8 @@ cc_window_class_init (CcWindowClass *klass)
   gtk_widget_class_bind_template_callback (widget_class, back_button_clicked_cb);
   gtk_widget_class_bind_template_callback (widget_class, gdk_window_set_cb);
   gtk_widget_class_bind_template_callback (widget_class, notify_header_visible_child_cb);
-  gtk_widget_class_bind_template_callback (widget_class, notify_fold_cb);
-  gtk_widget_class_bind_template_callback (widget_class, on_main_leaflet_fold_changed_cb);
+  gtk_widget_class_bind_template_callback (widget_class, notify_folded_cb);
+  gtk_widget_class_bind_template_callback (widget_class, on_main_leaflet_folded_changed_cb);
   gtk_widget_class_bind_template_callback (widget_class, on_development_warning_dialog_responded_cb);
   gtk_widget_class_bind_template_callback (widget_class, previous_button_clicked_cb);
   gtk_widget_class_bind_template_callback (widget_class, search_entry_activate_cb);
@@ -949,7 +946,7 @@ cc_window_init (CcWindow *self)
   if (in_flatpak_sandbox ())
     gtk_style_context_add_class (gtk_widget_get_style_context (GTK_WIDGET (self)), "devel");
 
-  update_fold_state (self);
+  update_folded (self);
 }
 
 CcWindow *
