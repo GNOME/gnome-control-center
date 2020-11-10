5cbc30cc1b81669d24142c4d27afe6adfe940e3d
Robert Ancell <robert.ancell@canonical.com>
2020-11-10 15:03:47 +1300

    printers: Fix string leaks in new printer dialog

--- panels/printers/pp-new-printer-dialog.c
+++ panels/printers/pp-new-printer-dialog.c
@@ -1938,8 +1938,8 @@ ppd_selection_cb (GtkDialog *_dialog,
   PpNewPrinterDialog        *self = user_data;
   PpNewPrinter              *new_printer;
   GList                     *original_names_list = NULL;
-  gchar                     *ppd_name;
-  gchar                     *ppd_display_name;
+  g_autofree gchar          *ppd_name = NULL;
+  g_autofree gchar          *ppd_display_name = NULL;
   guint                      window_id = 0;
   gint                       acquisition_method;
 
