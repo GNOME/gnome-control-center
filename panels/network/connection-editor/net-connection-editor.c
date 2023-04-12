GNOME/gnome-control-center

commit 6f1567f23696eb4a5f52721606c80686265c64c0

network/connection-editor: destroy the editor when closing
If an user opens an connection editor and then closes it, repeat this
multiple times causes lots of unused hidden editor windows.

Fix that by destroying the connection editor instead of hiding them
when closing.

panels/network/connection-editor/net-connection-editor.c
@@ -81,7 +81,7 @@ static void page_changed (NetConnectionEditor *self);
 static void
 cancel_editing (NetConnectionEditor *self)
 {
-        gtk_widget_hide (GTK_WIDGET (self));
+        gtk_window_destroy (GTK_WINDOW (self));
         g_signal_emit (self, signals[DONE], 0, FALSE);
 }
 

commit 14b3652289df4f7d1a44c9607a3880916ef74a56

network/connection-editor: fix crash when removing a connection
This was caused by commit 6f1567f23.

panels/network/connection-editor/net-connection-editor.c
@@ -169,8 +169,8 @@ static void page_changed (NetConnectionEditor *self);
 static void
 cancel_editing (NetConnectionEditor *self)
 {
-        gtk_window_destroy (GTK_WINDOW (self));
         g_signal_emit (self, signals[DONE], 0, FALSE);
+        gtk_window_destroy (GTK_WINDOW (self));
 }
 
 static void