809eba3a08bced23c1d6b47f041b6010e44fa8a1
Gotam Gorabh <gautamy672@gmail.com>
2023-05-27 23:58:24 +0530

    system: Add new sytem panel
    This is the basic panel structure which will contain the
    pages for the "System" panel, per mockups in
    https://gitlab.gnome.org/Teams/Design/settings-mockups/-/blob/master/system/system-panel.png
    
    see https://gitlab.gnome.org/GNOME/gnome-control-center/-/issues/2241

---
+++ panels/system/cc-system-panel.c
@@ -0,0 +1,56 @@
+/*
+ * cc-system-panel.c
+ *
+ * Copyright 2023 Gotam Gorabh <gautamy672@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include <config.h>
+
+#include <glib/gi18n-lib.h>
+
+#include "cc-list-row.h"
+#include "cc-system-panel.h"
+#include "cc-system-resources.h"
+
+struct _CcSystemPanel
+{
+  CcPanel    parent_instance;
+  
+  AdwPreferencesPage *main_page;
+  AdwLeaflet         *main_leaflet;
+};
+
+CC_PANEL_REGISTER (CcSystemPanel, cc_system_panel)
+
+static void
+cc_system_panel_class_init (CcSystemPanelClass *klass)
+{
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/control-center/system/cc-system-panel.ui");
+
+  gtk_widget_class_bind_template_child (widget_class, CcSystemPanel, main_page);
+  gtk_widget_class_bind_template_child (widget_class, CcSystemPanel, main_leaflet);
+}
+
+static void
+cc_system_panel_init (CcSystemPanel *self)
+{
+  g_resources_register (cc_system_get_resource ());
+  gtk_widget_init_template (GTK_WIDGET (self));
+}
