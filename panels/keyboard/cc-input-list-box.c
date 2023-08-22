bb8c08221c9f55f45058b50458aff873184a07a2
Carlos Garnacho <carlosg@gnome.org>
2023-08-22 12:39:43 +0200

    keyboard: Call "tecla" previewer through full path
    Make the binary called be either the one found at build time,
    or the one installed as a subproject, instead of relying on
    PATH to find the executable.

--- panels/keyboard/cc-input-list-box.c
+++ panels/keyboard/cc-input-list-box.c
@@ -240,10 +240,10 @@ row_layout_cb (CcInputListBox *self,
   layout_variant = cc_input_source_get_layout_variant (source);
 
   if (layout_variant && layout_variant[0])
-    commandline = g_strdup_printf ("tecla \"%s+%s\"",
+    commandline = g_strdup_printf (KEYBOARD_PREVIEWER_EXEC " \"%s+%s\"",
 				   layout, layout_variant);
   else
-    commandline = g_strdup_printf ("tecla %s",
+    commandline = g_strdup_printf (KEYBOARD_PREVIEWER_EXEC " %s",
 				   layout);
 
   g_spawn_command_line_async (commandline, NULL);
