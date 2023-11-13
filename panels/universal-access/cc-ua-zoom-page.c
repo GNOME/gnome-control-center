3f78abad08c678621bd0360a5caeae74f2d34bbb
Automeris naranja <automerisnaranja@tutanota.com>
2023-11-13 20:42:07 -0300

    ua-zoom-page: Use an AdwSwitchRow to enable crosshairs
    Closes https://gitlab.gnome.org/GNOME/gnome-control-center/-/issues/2744

--- panels/universal-access/cc-ua-zoom-page.c
+++ panels/universal-access/cc-ua-zoom-page.c
@@ -51,7 +51,7 @@ struct _CcUaZoomPage
   AdwComboRow        *zoom_screen_area_row;
   AdwComboRow        *zoom_follow_behaviour_row;
 
-  GtkSwitch          *crosshair_switch;
+  AdwSwitchRow       *crosshair_row;
   AdwSwitchRow       *crosshair_overlap_mouse_row;
   GtkScale           *crosshair_thickness_scale;
   GtkScale           *crosshair_length_scale;
@@ -333,7 +333,7 @@ cc_ua_zoom_page_class_init (CcUaZoomPageClass *klass)
   gtk_widget_class_bind_template_child (widget_class, CcUaZoomPage, zoom_screen_area_row);
   gtk_widget_class_bind_template_child (widget_class, CcUaZoomPage, zoom_follow_behaviour_row);
 
-  gtk_widget_class_bind_template_child (widget_class, CcUaZoomPage, crosshair_switch);
+  gtk_widget_class_bind_template_child (widget_class, CcUaZoomPage, crosshair_row);
   gtk_widget_class_bind_template_child (widget_class, CcUaZoomPage, crosshair_overlap_mouse_row);
   gtk_widget_class_bind_template_child (widget_class, CcUaZoomPage, crosshair_thickness_scale);
   gtk_widget_class_bind_template_child (widget_class, CcUaZoomPage, crosshair_length_scale);
@@ -373,7 +373,7 @@ cc_ua_zoom_page_init (CcUaZoomPage *self)
 
   /* Cross hairs */
   g_settings_bind (self->magnifier_settings, "show-cross-hairs",
-                   self->crosshair_switch, "active",
+                   self->crosshair_row, "active",
                    G_SETTINGS_BIND_DEFAULT);
   g_settings_bind (self->magnifier_settings, "cross-hairs-clip",
                    self->crosshair_overlap_mouse_row, "active",
