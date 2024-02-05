a926a8b387faf4dbc37505edb58d0421d42edcec
Corey Berla <corey@berla.me>
2024-02-04 20:04:38 -0800

    search-locations-dialog: Set title for non-existent bookmarks
    Show the bookmark name, but keep the switch invisible.
    
    Fixes: https://gitlab.gnome.org/GNOME/gnome-control-center/-/issues/1916

--- panels/search/cc-search-locations-dialog.c
+++ panels/search/cc-search-locations-dialog.c
@@ -506,9 +506,6 @@ place_query_info_ready (GObject *source,
                                 switch_tracker_get_mapping,
                                 switch_tracker_set_mapping,
                                 place, NULL);
-
-  adw_preferences_row_set_title (ADW_PREFERENCES_ROW (widgets->row),
-                                 place->display_name);
 }
 
 static void
@@ -565,6 +562,7 @@ create_row_for_place (CcSearchLocationsDialog *self, Place *place)
   gtk_widget_set_valign (widgets->switch_, GTK_ALIGN_CENTER);
   adw_action_row_add_suffix (ADW_ACTION_ROW (widgets->row), widgets->switch_);
   adw_action_row_set_activatable_widget (ADW_ACTION_ROW (widgets->row), widgets->switch_);
+  adw_preferences_row_set_title (ADW_PREFERENCES_ROW (widgets->row), place->display_name);
 
   g_object_set_data_full (G_OBJECT (widgets->row), "place", place, (GDestroyNotify) place_free);
 
