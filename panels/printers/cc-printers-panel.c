5cd007b7d28a94acd9475821e43c6cf11b0acda8
Automeris naranja <automerisnaranja@tutanota.com>
2024-02-08 05:32:48 -0300

    printers: Add padding to the sidebar items from "Printer Options"

--- panels/printers/cc-printers-panel.c
+++ panels/printers/cc-printers-panel.c
@@ -1227,9 +1227,18 @@ cc_printers_panel_class_init (CcPrintersPanelClass *klass)
 static void
 cc_printers_panel_init (CcPrintersPanel *self)
 {
+  g_autoptr(GtkCssProvider) provider = NULL;
+
   gtk_widget_init_template (GTK_WIDGET (self));
 
   g_resources_register (cc_printers_get_resource ());
+
+  provider = gtk_css_provider_new ();
+  gtk_css_provider_load_from_resource (provider,
+                                       "/org/gnome/control-center/printers/printers.css");
+  gtk_style_context_add_provider_for_display (gdk_display_get_default (),
+                                              GTK_STYLE_PROVIDER (provider),
+                                              GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 
   /* initialize main data structure */
   self->reference = g_object_new (G_TYPE_OBJECT, NULL);
