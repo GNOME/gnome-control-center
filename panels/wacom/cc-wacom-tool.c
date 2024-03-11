GNOME/gnome-control-center

commit 42b39cb956197486272f391925529c8f2dd0b06a

wacom: Prefer the stylus over the eraser
Pens with an integrated eraser share the serial number with the
corresponding pen but differ in the tool ID. libwacom provides a
"paired_id" mapping to look up one from the other.

If the user brings the eraser into proximity the very first time the
tool is seen, the eraser's tool ID is stored in the keyfile instead of
the associated stylus id. This leads to otherwise wrong behavior since
erasers share the serial number with the stylus but don't have any
buttons, don't have any erasers themselves, etc.

Fix this by looking up a matching stylus when we see an eraser. This
effectively filters our CcWacomTool to always be the pen, which is an
inadvertent assumption anyway.

panels/wacom/cc-wacom-tool.c
@@ -147,6 +147,41 @@ cc_wacom_tool_class_init (CcWacomToolClass *klass)
 	g_object_class_install_properties (object_class, N_PROPS, props);
 }
 
+static const WacomStylus *
+find_stylus_for_id (WacomDeviceDatabase  *wacom_db,
+		    CcWacomTool		*tool)
+{
+	const WacomStylus *eraser;
+	const gint *all_ids;
+	gint n_supported;
+	gint i;
+
+	eraser = libwacom_stylus_get_for_id (wacom_db, tool->id);
+	if (!libwacom_stylus_is_eraser (eraser))
+		return eraser;
+
+	all_ids = cc_wacom_device_get_supported_tools (tool->device, &n_supported);
+	for (i = 0; i < n_supported; i++) {
+		const WacomStylus *stylus;
+		const gint *paired_ids;
+		gint n_paired;
+		gint j;
+
+		if (all_ids[i] == tool->id)
+			continue;
+
+		stylus = libwacom_stylus_get_for_id (wacom_db, all_ids[i]);
+		paired_ids = libwacom_stylus_get_paired_ids (stylus, &n_paired);
+		for (j = 0; j < n_paired; j++) {
+			if (paired_ids[j] == tool->id) {
+				return libwacom_stylus_get_for_id (wacom_db, all_ids[i]);
+			}
+		}
+	}
+
+	return eraser;
+}
+
 static gboolean
 cc_wacom_tool_initable_init (GInitable     *initable,
 			     GCancellable  *cancellable,
@@ -167,10 +202,13 @@ cc_wacom_tool_initable_init (GInitable     *initable,
 			tool->id = ids[0];
 	}
 
-	if (tool->id == 0)
+	if (tool->id == 0) {
 		tool->wstylus = libwacom_stylus_get_for_id (wacom_db, 0xfffff);
-	else
-		tool->wstylus = libwacom_stylus_get_for_id (wacom_db, tool->id);
+	} else {
+		tool->wstylus = find_stylus_for_id (wacom_db, tool);
+		if (tool->wstylus)
+			tool->id = libwacom_stylus_get_id (tool->wstylus);
+	}
 
 	if (!tool->wstylus) {
 		g_set_error (error, 0, 0, "Stylus description not found");

commit 8b45201b9c01e92d863b39aa1a117d33017f7816

wacom: Accept eraser types of button when matching stylus IDs
Unlike tip erasers, styli with eraser buttons (which is the vast
majority anyway) have the same tool ID as the pen so the first
matching stylus we find is the right one.

This avoid a warning when a AES stylus is connected - we're constructing
the stylus based on our cache information which is just ID and serial
but no device - meaning the device lookup cannot work.

Fixes: 42b39cb95619 ("wacom: Prefer the stylus over the eraser")

panels/wacom/cc-wacom-tool.c
@@ -157,7 +157,8 @@ find_stylus_for_id (WacomDeviceDatabase  *wacom_db,
 	gint i;
 
 	eraser = libwacom_stylus_get_for_id (wacom_db, tool->id);
-	if (!libwacom_stylus_is_eraser (eraser))
+	if (!libwacom_stylus_is_eraser (eraser) ||
+	    libwacom_stylus_get_eraser_type (eraser) == WACOM_ERASER_BUTTON)
 		return eraser;
 
 	all_ids = cc_wacom_device_get_supported_tools (tool->device, &n_supported);