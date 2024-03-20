808bb413481d8cd9f6649ebafc6310882bac1d78
Felipe Borges <felipeborges@gnome.org>
2024-03-20 12:16:13 +0100

    system: Load subpages on demand
    Let's manually wire the row activation to the panel creation so that
    we can create panels on demand.
    
    This also makes calls to subpages directly avoid initializing the
    other panels.
    
    Fixes #2857

--- panels/system/cc-system-panel.c
+++ panels/system/cc-system-panel.c
@@ -40,6 +40,12 @@ struct _CcSystemPanel
 
   AdwNavigationView *navigation;
 
+  AdwActionRow *about_row;
+  AdwActionRow *datetime_row;
+  AdwActionRow *region_row;
+  AdwActionRow *remote_desktop_row;
+  AdwActionRow *users_row;
+
   GtkWidget *remote_login_dialog;
   AdwNavigationPage *software_updates_group;
 };
@@ -133,14 +139,53 @@ on_subpage_set (CcSystemPanel *self)
 {
   AdwNavigationPage *subpage;
   g_autofree gchar *tag = NULL;
+  GType page_type = G_TYPE_INVALID;
 
   g_object_get (self, "subpage", &tag, NULL);
   if (!tag)
     return;
 
-  subpage = adw_navigation_view_find_page (self->navigation, tag);
+  if (g_str_equal (tag, "about"))
+    page_type = CC_TYPE_ABOUT_PAGE;
+  else if (g_str_equal (tag, "datetime"))
+    page_type = CC_TYPE_DATE_TIME_PAGE;
+  else if (g_str_equal (tag, "region"))
+    page_type = CC_TYPE_REGION_PAGE;
+  else if (g_str_equal (tag, "remote-desktop"))
+    page_type = CC_TYPE_REMOTE_DESKTOP_PAGE;
+  else if (g_str_equal (tag, "users"))
+    page_type = CC_TYPE_USERS_PAGE;
+
+  if (page_type == G_TYPE_INVALID)
+    return;
+
+  subpage = ADW_NAVIGATION_PAGE (g_object_new (page_type, NULL));
   if (subpage)
     adw_navigation_view_push (self->navigation, subpage);
+}
+
+static void
+on_page_activated (AdwActionRow *row,
+                   gpointer      user_data)
+{
+  CcSystemPanel *self = CC_SYSTEM_PANEL (user_data);
+  GType page_type = G_TYPE_INVALID;
+
+  if (row == self->about_row)
+    page_type = CC_TYPE_ABOUT_PAGE;
+  else if (row == self->datetime_row)
+    page_type = CC_TYPE_DATE_TIME_PAGE;
+  else if (row == self->region_row)
+    page_type = CC_TYPE_REGION_PAGE;
+  else if (row == self->remote_desktop_row)
+    page_type = CC_TYPE_REMOTE_DESKTOP_PAGE;
+  else if (row == self->users_row)
+    page_type = CC_TYPE_USERS_PAGE;
+
+  if (!page_type)
+    return;
+
+  adw_navigation_view_push (self->navigation, g_object_new (page_type, NULL));
 }
 
 static void
@@ -150,11 +195,17 @@ cc_system_panel_class_init (CcSystemPanelClass *klass)
 
   gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/control-center/system/cc-system-panel.ui");
 
+  gtk_widget_class_bind_template_child (widget_class, CcSystemPanel, about_row);
+  gtk_widget_class_bind_template_child (widget_class, CcSystemPanel, datetime_row);
+  gtk_widget_class_bind_template_child (widget_class, CcSystemPanel, region_row);
+  gtk_widget_class_bind_template_child (widget_class, CcSystemPanel, remote_desktop_row);
+  gtk_widget_class_bind_template_child (widget_class, CcSystemPanel, users_row);
   gtk_widget_class_bind_template_child (widget_class, CcSystemPanel, navigation);
   gtk_widget_class_bind_template_child (widget_class, CcSystemPanel, software_updates_group);
 
   gtk_widget_class_bind_template_callback (widget_class, cc_system_page_open_software_update);
   gtk_widget_class_bind_template_callback (widget_class, on_secure_shell_row_clicked);
+  gtk_widget_class_bind_template_callback (widget_class, on_page_activated);
 
   g_type_ensure (CC_TYPE_ABOUT_PAGE);
   g_type_ensure (CC_TYPE_DATE_TIME_PAGE);
