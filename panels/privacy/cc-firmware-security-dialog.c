c7a324d0ed22b52d7a8186a4a568d37496ebd0b6
Michael Catanzaro <mcatanzaro@redhat.com>
2024-03-21 12:14:34 -0500

    privacy: use yellow lock rather than green checkmark for HSI-1
    This more clearly indicates that HSI-1 is firmly between HSI-0 and HSI-2
    in goodness/badness. It is secure, so the lock icon is locked. It's just
    not as great as HSI-2, so it's yellow rather than green.

--- panels/privacy/cc-firmware-security-dialog.c
+++ panels/privacy/cc-firmware-security-dialog.c
@@ -84,6 +84,8 @@ set_dialog_item_layer1 (CcFirmwareSecurityDialog *self,
         gtk_widget_add_css_class (self->firmware_security_dialog_icon, "error");
         break;
       case 1:
+        gtk_widget_add_css_class (self->firmware_security_dialog_icon, "warning");
+        break;
       case 2:
       case 3:
       case 4:
@@ -116,7 +118,7 @@ update_dialog (CcFirmwareSecurityDialog *self)
     case 1:
       gtk_window_set_default_size (GTK_WINDOW (&self->parent), 380, 380);
       set_dialog_item_layer1 (self,
-                              "emblem-default-symbolic",
+                              "channel-secure-symbolic",
                               _("Basic Security Checks Passed"),
                               /* TRANSLATORS: This description describes the device passing the
                                  minimum requirement of security check.*/
