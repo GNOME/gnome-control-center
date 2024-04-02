115fb91c0394801014a48780f6fe6cb52fd6e3b1
Automeris naranja <automerisnaranja@tutanota.com>
2024-04-02 15:59:58 -0300

    wifi: Show a status page if there are no saved Wi-Fi networks
    When forgetting all networks from the "Saved Wi-Fi Networks"
    dialog, the dialog will close after the AdwToast times out,
    which is weird.
    
    To fix this, always show this dialog and don't make the
    "Saved Networks" row insensitive if there are no saved networks.

--- panels/network/net-device-wifi.c
+++ panels/network/net-device-wifi.c
@@ -68,6 +68,7 @@ struct _NetDeviceWifi
         AdwPreferencesGroup     *saved_networks_box;
         CcWifiConnectionList    *saved_networks_list;
         AdwDialog               *saved_networks_dialog;
+        GtkStack                *saved_networks_stack;
         AdwToastOverlay         *saved_networks_toast_overlay;
         AdwToast                *saved_networks_undo_toast;
         GPtrArray               *saved_networks_forgotten_rows;
@@ -369,10 +370,14 @@ nm_device_wifi_refresh_ui (NetDeviceWifi *self)
 
         status = panel_device_status_to_localized_string (self->device, NULL);
         adw_window_title_set_subtitle (self->wifi_headerbar_title, status);
+
+        if (cc_wifi_connection_list_is_empty (self->saved_networks_list))
+                gtk_stack_set_visible_child_name (self->saved_networks_stack, "no-saved-networks-page");
+        else
+                gtk_stack_set_visible_child_name (self->saved_networks_stack, "saved-networks-page");
+
         /* update list of APs */
         show_wifi_list (self);
-
-        gtk_widget_set_visible (GTK_WIDGET (self->saved_network_row), !cc_wifi_connection_list_is_empty (self->saved_networks_list));
 }
 
 static void
@@ -810,11 +815,6 @@ really_forgotten (GObject              *source_object,
                 g_warning ("failed to delete connection %s: %s",
                            nm_object_get_path (NM_OBJECT (source_object)),
                            error->message);
-
-        if (cc_wifi_connection_list_is_empty (self->saved_networks_list)) {
-            adw_dialog_close (self->saved_networks_dialog);
-            gtk_widget_set_visible (GTK_WIDGET (self->saved_network_row), FALSE);
-        }
 }
 
 static void
@@ -1095,6 +1095,7 @@ net_device_wifi_class_init (NetDeviceWifiClass *klass)
         gtk_widget_class_bind_template_child (widget_class, NetDeviceWifi, listbox_box);
         gtk_widget_class_bind_template_child (widget_class, NetDeviceWifi, stack);
         gtk_widget_class_bind_template_child (widget_class, NetDeviceWifi, saved_networks_dialog);
+        gtk_widget_class_bind_template_child (widget_class, NetDeviceWifi, saved_networks_stack);
         gtk_widget_class_bind_template_child (widget_class, NetDeviceWifi, saved_networks_toast_overlay);
         gtk_widget_class_bind_template_child (widget_class, NetDeviceWifi, saved_networks_box);
 
@@ -1173,7 +1174,6 @@ net_device_wifi_new (CcPanel *panel, NMClient *client, NMDevice *device)
 
         /* Set up the Saved Networks list */
         list = cc_wifi_connection_list_new (self->client, NM_DEVICE_WIFI (device), FALSE, FALSE, FALSE, TRUE, FALSE);
-        cc_wifi_connection_list_set_placeholder_text (list, _("No saved networks"));
         self->saved_networks_list = g_object_ref_sink (list);
         adw_preferences_group_add (self->saved_networks_box, GTK_WIDGET (list));
 
