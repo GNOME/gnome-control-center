1f0da3ca656e3cb6293500c15d9070c8abf95f3d
Automeris naranja <automerisnaranja@tutanota.com>
2024-04-14 22:45:34 -0300

    network: Use a placeholder widget in the VPN list box
    Using a GtkStack isn't necessary to display the "Not set up"
    row. Instead, use a GtkListBox placeholder widget in the main
    VPN list box.
    
    This improves consistency with the placeholders from "Privacy >
    Location/Camera/Microphone" when there are no apps; same thing
    in "Search > Search Locations > Custom Locations" when no custom
    locations were added.
    
    https://docs.gtk.org/gtk4/class.ListBox.html#gtklistbox-as-gtkbuildable

--- panels/network/cc-network-panel.c
+++ panels/network/cc-network-panel.c
@@ -70,10 +70,8 @@ struct _CcNetworkPanel
         GtkWidget        *box_vpn;
         GtkWidget        *box_wired;
         GtkWidget        *container_bluetooth;
-        GtkWidget        *empty_listbox;
         GtkWidget        *proxy_row;
         GtkWidget        *save_button;
-        GtkWidget        *vpn_stack;
         GtkWidget        *toolbar_view;
 
         /* wireless dialog stuff */
@@ -356,13 +354,6 @@ handle_argv (CcNetworkPanel *self)
 }
 
 static void
-update_vpn_section (CcNetworkPanel *self)
-{
-        gtk_stack_set_visible_child (GTK_STACK (self->vpn_stack),
-                                     self->vpns->len == 0 ? self->empty_listbox : self->box_vpn);
-}
-
-static void
 update_bluetooth_section (CcNetworkPanel *self)
 {
         gtk_widget_set_visible (self->container_bluetooth, self->bluetooth_devices->len > 0);
@@ -480,9 +471,6 @@ panel_remove_device (CcNetworkPanel *self, NMDevice *device)
         g_hash_table_remove (self->nm_device_to_device, device);
 
         gtk_box_remove (GTK_BOX (gtk_widget_get_parent (net_device)), net_device);
-
-        /* update vpn widgets */
-        update_vpn_section (self);
 
         /* update device_bluetooth widgets */
         update_bluetooth_section (self);
@@ -605,9 +593,6 @@ panel_add_vpn_device (CcNetworkPanel *self, NMConnection *connection)
 
         /* store in the devices array */
         g_ptr_array_add (self->vpns, net_vpn);
-
-        /* update vpn widgets */
-        update_vpn_section (self);
 }
 
 static void
@@ -642,7 +627,6 @@ client_connection_removed_cb (CcNetworkPanel *self, NMConnection *connection)
                 if (net_vpn_get_connection (vpn) == connection) {
                         g_ptr_array_remove (self->vpns, vpn);
                         gtk_list_box_remove (GTK_LIST_BOX (self->box_vpn), GTK_WIDGET (vpn));
-                        update_vpn_section (self);
                         return;
                 }
         }
@@ -718,9 +702,7 @@ cc_network_panel_class_init (CcNetworkPanelClass *klass)
         gtk_widget_class_bind_template_child (widget_class, CcNetworkPanel, box_vpn);
         gtk_widget_class_bind_template_child (widget_class, CcNetworkPanel, box_wired);
         gtk_widget_class_bind_template_child (widget_class, CcNetworkPanel, container_bluetooth);
-        gtk_widget_class_bind_template_child (widget_class, CcNetworkPanel, empty_listbox);
         gtk_widget_class_bind_template_child (widget_class, CcNetworkPanel, proxy_row);
-        gtk_widget_class_bind_template_child (widget_class, CcNetworkPanel, vpn_stack);
         gtk_widget_class_bind_template_child (widget_class, CcNetworkPanel, toolbar_view);
 
         gtk_widget_class_bind_template_callback (widget_class, create_connection_cb);
