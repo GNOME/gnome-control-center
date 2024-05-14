6a96efe5f81bf641ad83241fa26219c217c17e2a
Matthijs Velsink <mvelsink@gnome.org>
2024-05-14 22:29:41 +0200

    privacy,usage: Port to CcNumberRow
    This saves a lot of code and translatable strings. Also, this will make
    it easier to change the list values in the future, as they will be added
    to the list even if a value is set that is not in the list.
    
    By binding to the GSettings keys directly, even more code can be
    removed.

--- panels/privacy/usage/cc-usage-page.c
+++ panels/privacy/usage/cc-usage-page.c
@@ -19,8 +19,7 @@
  */
 
 #include "cc-usage-page.h"
-#include "cc-usage-page-enums.h"
-#include "cc-util.h"
+#include "cc-number-row.h"
 
 #include <gio/gdesktopappinfo.h>
 #include <glib/gi18n.h>
@@ -34,83 +33,14 @@ struct _CcUsagePage
   AdwDialog    *clear_file_history_dialog;
   AdwDialog    *delete_temp_files_dialog;
   AdwDialog    *empty_trash_dialog;
-  AdwComboRow  *purge_after_combo;
+  CcNumberRow  *purge_after_row;
   AdwSwitchRow *purge_temp_row;
   AdwSwitchRow *purge_trash_row;
   AdwSwitchRow *recently_used_row;
-  AdwComboRow  *retain_history_combo;
+  CcNumberRow  *retain_history_row;
 };
 
 G_DEFINE_TYPE (CcUsagePage, cc_usage_page, ADW_TYPE_NAVIGATION_PAGE)
-
-static char *
-purge_after_name_cb (AdwEnumListItem *item,
-                     gpointer         user_data)
-{
-  switch (adw_enum_list_item_get_value (item))
-    {
-    case CC_USAGE_PAGE_PURGE_AFTER_1_HOUR:
-      /* Translators: Option for "Automatically Delete Period" in "Trash & Temporary Files" group */
-      return g_strdup (C_("purge_files", "1 hour"));
-    case CC_USAGE_PAGE_PURGE_AFTER_1_DAY:
-      /* Translators: Option for "Automatically Delete Period" in "Trash & Temporary Files" group */
-      return g_strdup (C_("purge_files", "1 day"));
-    case CC_USAGE_PAGE_PURGE_AFTER_2_DAYS:
-      /* Translators: Option for "Automatically Delete Period" in "Trash & Temporary Files" group */
-      return g_strdup (C_("purge_files", "2 days"));
-    case CC_USAGE_PAGE_PURGE_AFTER_3_DAYS:
-      /* Translators: Option for "Automatically Delete Period" in "Trash & Temporary Files" group */
-      return g_strdup (C_("purge_files", "3 days"));
-    case CC_USAGE_PAGE_PURGE_AFTER_4_DAYS:
-      /* Translators: Option for "Automatically Delete Period" in "Trash & Temporary Files" group */
-      return g_strdup (C_("purge_files", "4 days"));
-    case CC_USAGE_PAGE_PURGE_AFTER_5_DAYS:
-      /* Translators: Option for "Automatically Delete Period" in "Trash & Temporary Files" group */
-      return g_strdup (C_("purge_files", "5 days"));
-    case CC_USAGE_PAGE_PURGE_AFTER_6_DAYS:
-      /* Translators: Option for "Automatically Delete Period" in "Trash & Temporary Files" group */
-      return g_strdup (C_("purge_files", "6 days"));
-    case CC_USAGE_PAGE_PURGE_AFTER_7_DAYS:
-      /* Translators: Option for "Automatically Delete Period" in "Trash & Temporary Files" group */
-      return g_strdup (C_("purge_files", "7 days"));
-    case CC_USAGE_PAGE_PURGE_AFTER_14_DAYS:
-      /* Translators: Option for "Automatically Delete Period" in "Trash & Temporary Files" group */
-      return g_strdup (C_("purge_files", "14 days"));
-    case CC_USAGE_PAGE_PURGE_AFTER_30_DAYS:
-      /* Translators: Option for "Automatically Delete Period" in "Trash & Temporary Files" group */
-      return g_strdup (C_("purge_files", "30 days"));
-    default:
-      return NULL;
-    }
-}
-
-static void
-purge_after_combo_changed_cb (AdwComboRow *combo_row,
-                              GParamSpec  *pspec,
-                              CcUsagePage *self)
-{
-  AdwEnumListItem *item;
-  CcUsagePagePurgeAfter value;
-
-  item = ADW_ENUM_LIST_ITEM (adw_combo_row_get_selected_item (combo_row));
-  value = adw_enum_list_item_get_value (item);
-
-  g_settings_set (self->privacy_settings, "old-files-age", "u", value);
-}
-
-static void
-set_purge_after_value_for_combo (AdwComboRow *combo_row,
-                                 CcUsagePage *self)
-{
-  AdwEnumListModel *model;
-  guint value;
-
-  model = ADW_ENUM_LIST_MODEL (adw_combo_row_get_model (combo_row));
-
-  g_settings_get (self->privacy_settings, "old-files-age", "u", &value);
-  adw_combo_row_set_selected (combo_row,
-                              adw_enum_list_model_find_position (model, value));
-}
 
 static void
 on_empty_trash_response_cb (void)
@@ -150,62 +80,10 @@ cc_usage_page_finalize (GObject *object)
   G_OBJECT_CLASS (cc_usage_page_parent_class)->finalize (object);
 }
 
-static char *
-retain_history_name_cb (AdwEnumListItem *item,
-                        gpointer         user_data)
-{
-  switch (adw_enum_list_item_get_value (item))
-    {
-    case CC_USAGE_PAGE_RETAIN_HISTORY_1_DAY:
-      /* Translators: Option for "File History Duration" in "File History" group */
-      return g_strdup (C_("retain_history", "1 day"));
-    case CC_USAGE_PAGE_RETAIN_HISTORY_7_DAYS:
-      /* Translators: Option for "File History Duration" in "File History" group */
-      return g_strdup (C_("retain_history", "7 days"));
-    case CC_USAGE_PAGE_RETAIN_HISTORY_30_DAYS:
-      /* Translators: Option for "File History Duration" in "File History" group */
-      return g_strdup (C_("retain_history", "30 days"));
-    case CC_USAGE_PAGE_RETAIN_HISTORY_FOREVER:
-      /* Translators: Option for "File History Duration" in "File History" group */
-      return g_strdup (C_("retain_history", "Forever"));
-    default:
-      return NULL;
-    }
-}
-
-static void
-retain_history_combo_changed_cb (AdwComboRow *combo_row,
-                                 GParamSpec  *pspec,
-                                 CcUsagePage *self)
-{
-  AdwEnumListItem *item;
-  CcUsagePageRetainHistory value;
-
-  item = ADW_ENUM_LIST_ITEM (adw_combo_row_get_selected_item (combo_row));
-  value = adw_enum_list_item_get_value (item);
-
-  g_settings_set (self->privacy_settings, "recent-files-max-age", "i", value);
-}
-
-static void
-set_retain_history_value_for_combo (AdwComboRow *combo_row,
-                                    CcUsagePage *self)
-{
-  AdwEnumListModel *model;
-  gint value;
-
-  model = ADW_ENUM_LIST_MODEL (adw_combo_row_get_model (combo_row));
-
-  g_settings_get (self->privacy_settings, "recent-files-max-age", "i", &value);
-  adw_combo_row_set_selected (combo_row,
-                              adw_enum_list_model_find_position (model, value));
-}
-
 static void
 cc_usage_page_init (CcUsagePage *self)
 {
-  g_type_ensure (CC_TYPE_USAGE_PAGE_PURGE_AFTER);
-  g_type_ensure (CC_TYPE_USAGE_PAGE_RETAIN_HISTORY);
+  g_type_ensure (CC_TYPE_NUMBER_ROW);
 
   gtk_widget_init_template (GTK_WIDGET (self));
 
@@ -217,11 +95,13 @@ cc_usage_page_init (CcUsagePage *self)
                    "active",
                    G_SETTINGS_BIND_DEFAULT);
 
-  set_retain_history_value_for_combo (self->retain_history_combo, self);
+  cc_number_row_bind_settings (self->retain_history_row,
+                               self->privacy_settings,
+                               "recent-files-max-age");
 
   g_settings_bind (self->privacy_settings,
                    "remember-recent-files",
-                   self->retain_history_combo,
+                   self->retain_history_row,
                    "sensitive",
                    G_SETTINGS_BIND_GET);
 
@@ -233,7 +113,9 @@ cc_usage_page_init (CcUsagePage *self)
                    self->purge_temp_row, "active",
                    G_SETTINGS_BIND_DEFAULT);
 
-  set_purge_after_value_for_combo (self->purge_after_combo, self);
+  cc_number_row_bind_settings (self->purge_after_row,
+                               self->privacy_settings,
+                               "old-files-age");
 }
 
 static void
@@ -255,18 +137,13 @@ cc_usage_page_class_init (CcUsagePageClass *klass)
   gtk_widget_class_bind_template_child (widget_class, CcUsagePage, clear_file_history_dialog);
   gtk_widget_class_bind_template_child (widget_class, CcUsagePage, delete_temp_files_dialog);
   gtk_widget_class_bind_template_child (widget_class, CcUsagePage, empty_trash_dialog);
-  gtk_widget_class_bind_template_child (widget_class, CcUsagePage, purge_after_combo);
+  gtk_widget_class_bind_template_child (widget_class, CcUsagePage, purge_after_row);
   gtk_widget_class_bind_template_child (widget_class, CcUsagePage, purge_temp_row);
   gtk_widget_class_bind_template_child (widget_class, CcUsagePage, purge_trash_row);
   gtk_widget_class_bind_template_child (widget_class, CcUsagePage, recently_used_row);
-  gtk_widget_class_bind_template_child (widget_class, CcUsagePage, retain_history_combo);
-
+  gtk_widget_class_bind_template_child (widget_class, CcUsagePage, retain_history_row);
 
   gtk_widget_class_bind_template_callback (widget_class, on_clear_history_response_cb);
   gtk_widget_class_bind_template_callback (widget_class, on_empty_trash_response_cb);
   gtk_widget_class_bind_template_callback (widget_class, on_purge_temp_response_cb);
-  gtk_widget_class_bind_template_callback (widget_class, retain_history_combo_changed_cb);
-  gtk_widget_class_bind_template_callback (widget_class, retain_history_name_cb);
-  gtk_widget_class_bind_template_callback (widget_class, purge_after_combo_changed_cb);
-  gtk_widget_class_bind_template_callback (widget_class, purge_after_name_cb);
 }
