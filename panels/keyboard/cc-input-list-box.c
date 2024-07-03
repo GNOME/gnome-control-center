GNOME/gnome-control-center

commit e33bbec7e9ed4dc6461cd77d3131abceee597cc6

input-list-box: Use a GtkListBox placeholder widget
Use a GtkListBox placeholder widget to show the
"No input source selected" message. Also, remove the
hexpand and vexpand properties from the list box, which
currently aren't doing anything.

panels/keyboard/cc-input-list-box.c
@@ -44,7 +44,6 @@ struct _CcInputListBox {
 
   GtkListBoxRow   *add_input_row;
   GtkListBox      *listbox;
-  AdwActionRow    *no_inputs_row;
 
   GCancellable    *cancellable;
 
@@ -305,8 +304,6 @@ add_input_row (CcInputListBox *self, CcInputSource *source)
 {
   CcInputRow *row;
 
-  gtk_widget_set_visible (GTK_WIDGET (self->no_inputs_row), FALSE);
-
   row = cc_input_row_new (source);
   g_signal_connect_object (row, "show-settings", G_CALLBACK (row_settings_cb), self, G_CONNECT_SWAPPED);
   g_signal_connect_object (row, "show-layout", G_CALLBACK (row_layout_cb), self, G_CONNECT_SWAPPED);
@@ -322,11 +319,6 @@ add_input_sources (CcInputListBox *self,
 {
   GVariantIter iter;
   const gchar *type, *id;
-
-  if (g_variant_n_children (sources) < 1) {
-    gtk_widget_set_visible (GTK_WIDGET (self->no_inputs_row), TRUE);
-    return;
-  }
 
   g_variant_iter_init (&iter, sources);
   while (g_variant_iter_next (&iter, "(&s&s)", &type, &id)) {
@@ -691,7 +683,6 @@ add_input_sources_from_localed (CcInputListBox *self)
     g_autoptr(CcInputSourceXkb) source = cc_input_source_xkb_new (self->xkb_info, layouts[i], variant);
     add_input_row (self, CC_INPUT_SOURCE (source));
   }
-  gtk_widget_set_visible (GTK_WIDGET (self->no_inputs_row), n == 0);
 }
 
 static void
@@ -767,7 +758,6 @@ cc_input_list_box_class_init (CcInputListBoxClass *klass)
 
   gtk_widget_class_bind_template_child (widget_class, CcInputListBox, add_input_row);
   gtk_widget_class_bind_template_child (widget_class, CcInputListBox, listbox);
-  gtk_widget_class_bind_template_child (widget_class, CcInputListBox, no_inputs_row);
 
   gtk_widget_class_bind_template_callback (widget_class, input_row_activated_cb);
   gtk_widget_class_bind_template_callback (widget_class, keynav_failed_cb);

panels/keyboard/cc-input-list-box.ui
@@ -1,23 +1,26 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <interface>
-  <!-- interface-requires gtk+ 3.0 -->
   <template class="CcInputListBox" parent="AdwBin">
 
     <child>
       <object class="GtkListBox" id="listbox">
-        <property name="hexpand">True</property>
-        <property name="vexpand">True</property>
         <property name="selection-mode">none</property>
         <signal name="row_activated" handler="input_row_activated_cb" object="CcInputListBox" swapped="yes"/>
         <signal name="keynav_failed" handler="keynav_failed_cb" object="CcInputListBox" swapped="yes"/>
+        <child type="placeholder">
+          <object class="GtkLabel">
+            <property name="label" translatable="yes">No input source selected</property>
+            <property name="wrap">true</property>
+            <property name="max-width-chars">50</property>
+            <style>
+              <class name="dim-label" />
+              <class name="cc-placeholder-row" />
+            </style>
+          </object>
+        </child>
         <style>
           <class name="boxed-list"/>
         </style>
-        <child>
-          <object class="AdwActionRow" id="no_inputs_row">
-            <property name="title" translatable="yes">No input source selected</property>
-          </object>
-        </child>
         <child>
           <object class="GtkListBoxRow" id="add_input_row">
             <property name="selectable">False</property>

commit e076418c2f297ccedb03345f0bd8ee38b52aa3c9

keyboard: Fix input row move buttons
Commit 322830e3 tried to fix the "move up", "move down" buttons on input
rows as they should be disabled when they are invalid for top or bottom
rows. However, that fix was broken by commit e33bbec7, as the GtkListBox
now starts counting from 0 with the removed "No input source selected"
child.

So we fix that for the top row, and simplify the logic for the bottom
row as well.

Also, there is no need to track button state for the input rows
themselves, we can handle that all in the parent CcInputListBox widget,
partially reverting 322830e3.

For `move_down_cb()` on CcInputRow, we swap the rows on the signal call
for consistency, but that changes nothing to the result.

Fixes #3078

panels/keyboard/cc-input-list-box.c
@@ -226,6 +226,7 @@ row_settings_cb (CcInputListBox *self,
 }
 
 static void move_input (CcInputListBox *self, CcInputRow *source, CcInputRow *dest);
+static void update_input_rows (CcInputListBox *self);
 
 static void
 row_moved_cb (CcInputListBox *self,
@@ -233,6 +234,7 @@ row_moved_cb (CcInputListBox *self,
               CcInputRow    *row)
 {
   move_input (self, row, dest_row);
+  update_input_rows (self);
 }
 
 static void remove_input (CcInputListBox *self, CcInputRow *row);
@@ -269,9 +271,8 @@ update_input_rows (CcInputListBox *self)
     cc_input_row_set_removable (row, n_input_rows > 1);
     cc_input_row_set_draggable (row, n_input_rows > 1);
 
-    gtk_widget_action_set_enabled (GTK_WIDGET (row), "row.move-up", row_idx != 1);
-    gtk_widget_action_set_enabled (GTK_WIDGET (row), "row.move-down", GTK_LIST_BOX_ROW (gtk_widget_get_next_sibling (child)) != self->add_input_row);
-
+    gtk_widget_action_set_enabled (GTK_WIDGET (row), "row.move-up", row_idx > 0);
+    gtk_widget_action_set_enabled (GTK_WIDGET (row), "row.move-down", row_idx < (n_input_rows - 1));
   }
 }
 

panels/keyboard/cc-input-row.c
@@ -110,11 +110,7 @@ move_up_cb (GtkWidget  *widget,
   GtkListBoxRow *previous_row = gtk_list_box_get_row_at_index (list_box, previous_idx);
 
   if (previous_row == NULL || !CC_IS_INPUT_ROW (previous_row))
-    {
-      gtk_widget_action_set_enabled (widget, "row.move-up", FALSE);
-      gtk_widget_action_set_enabled (GTK_WIDGET (gtk_list_box_get_row_at_index (list_box, 0)), "row.move-up", TRUE);
-      return;
-    }
+    return;
 
   g_signal_emit (self,
                  signals[SIGNAL_MOVE_ROW],
@@ -133,16 +129,12 @@ move_down_cb (GtkWidget  *widget,
   GtkListBoxRow *next_row = gtk_list_box_get_row_at_index (list_box, next_idx);
 
   if (next_row == NULL || !CC_IS_INPUT_ROW (next_row))
-    {
-      gtk_widget_action_set_enabled (widget, "row.move-down", FALSE);
-      gtk_widget_action_set_enabled (GTK_WIDGET (gtk_list_box_get_row_at_index (list_box, next_idx-1)), "row.move-down", TRUE);
-      return;
-    }
+    return;
 
-  g_signal_emit (next_row,
+  g_signal_emit (self,
                  signals[SIGNAL_MOVE_ROW],
                  0,
-                 self);
+                 next_row);
 }
 
 static void