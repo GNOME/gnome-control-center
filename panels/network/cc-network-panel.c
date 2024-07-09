dc51d3075cb6aefe402d2bacb275e63761467d2b
Felipe Borges <felipeborges@gnome.org>
2024-07-09 15:09:49 +0200

    network: Move "Airplane Mode" setting to Network panel
    This temporarily breaks the WiFi panel Airplane mode status page
    but that is not going to be a problem once the WiFi panel is a
    subpage of the Network panel. In this case the WiFi page won't
    be reachable when in airplane mode.

--- panels/network/cc-network-panel.c
+++ panels/network/cc-network-panel.c
@@ -73,6 +73,11 @@ struct _CcNetworkPanel
         GtkWidget        *proxy_row;
         GtkWidget        *save_button;
 
+        /* RFKill (Airplane Mode) */
+        GDBusProxy         *rfkill_proxy;
+        AdwSwitchRow       *rfkill_row;
+        GtkWidget          *rfkill_widget;
+
         /* wireless dialog stuff */
         CmdlineOperation  arg_operation;
         gchar            *arg_device;
@@ -213,6 +218,7 @@ cc_network_panel_dispose (GObject *object)
         g_clear_pointer (&self->mobile_devices, g_ptr_array_unref);
         g_clear_pointer (&self->vpns, g_ptr_array_unref);
         g_clear_pointer (&self->nm_device_to_device, g_hash_table_destroy);
+        g_clear_object (&self->rfkill_proxy);
 
         G_OBJECT_CLASS (cc_network_panel_parent_class)->dispose (object);
 }
@@ -674,6 +680,109 @@ create_connection_cb (CcNetworkPanel *self)
         gtk_window_present (GTK_WINDOW (editor));
 }
 
+/* Airplane Mode */
+static inline gboolean
+get_cached_rfkill_property (CcNetworkPanel *self,
+                            const gchar    *property)
+{
+  g_autoptr(GVariant) result = NULL;
+
+  result = g_dbus_proxy_get_cached_property (self->rfkill_proxy, property);
+  return result ? g_variant_get_boolean (result) : FALSE;
+}
+
+static void rfkill_switch_notify_activate_cb (CcNetworkPanel *self);
+
+static void
+sync_airplane_mode_switch (CcNetworkPanel *self)
+{
+  gboolean enabled, should_show, hw_enabled;
+
+  enabled = get_cached_rfkill_property (self, "HasAirplaneMode");
+  should_show = get_cached_rfkill_property (self, "ShouldShowAirplaneMode");
+
+  gtk_widget_set_visible (GTK_WIDGET (self->rfkill_widget), enabled && should_show);
+  if (!enabled || !should_show)
+    return;
+
+  enabled = get_cached_rfkill_property (self, "AirplaneMode");
+  hw_enabled = get_cached_rfkill_property (self, "HardwareAirplaneMode");
+
+  enabled |= hw_enabled;
+
+  if (enabled != adw_switch_row_get_active (self->rfkill_row))
+    {
+      g_signal_handlers_block_by_func (self->rfkill_row,
+                                       rfkill_switch_notify_activate_cb,
+                                       self);
+      g_object_set (self->rfkill_row, "active", enabled, NULL);
+      g_signal_handlers_unblock_by_func (self->rfkill_row,
+                                         rfkill_switch_notify_activate_cb,
+                                         self);
+  }
+
+  gtk_widget_set_sensitive (GTK_WIDGET (self->rfkill_row), !hw_enabled);
+}
+
+static void
+on_rfkill_proxy_properties_changed_cb (CcNetworkPanel *self)
+{
+  g_debug ("Rfkill properties changed");
+
+  sync_airplane_mode_switch (self);
+}
+
+static void
+rfkill_proxy_acquired_cb (GObject      *source_object,
+                          GAsyncResult *res,
+                          gpointer      user_data)
+{
+  CcNetworkPanel *self;
+  GDBusProxy *proxy;
+  g_autoptr(GError) error = NULL;
+
+  proxy = cc_object_storage_create_dbus_proxy_finish (res, &error);
+
+  if (error)
+    {
+      if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+        g_printerr ("Error creating rfkill proxy: %s\n", error->message);
+
+      return;
+    }
+
+  self = CC_NETWORK_PANEL (user_data);
+
+  self->rfkill_proxy = proxy;
+
+  g_signal_connect_object (proxy,
+                           "g-properties-changed",
+                           G_CALLBACK (on_rfkill_proxy_properties_changed_cb),
+                           self,
+                           G_CONNECT_SWAPPED);
+
+  sync_airplane_mode_switch (self);
+}
+
+static void
+rfkill_switch_notify_activate_cb (CcNetworkPanel *self)
+{
+  gboolean enable;
+
+  enable = adw_switch_row_get_active (self->rfkill_row);
+
+  g_dbus_proxy_call (self->rfkill_proxy,
+                     "org.freedesktop.DBus.Properties.Set",
+                     g_variant_new_parsed ("('org.gnome.SettingsDaemon.Rfkill',"
+                                           "'AirplaneMode', %v)",
+                                           g_variant_new_boolean (enable)),
+                     G_DBUS_CALL_FLAGS_NONE,
+                     -1,
+                     cc_panel_get_cancellable (CC_PANEL (self)),
+                     NULL,
+                     NULL);
+}
+
 static void
 cc_network_panel_map (GtkWidget *widget)
 {
@@ -711,8 +820,11 @@ cc_network_panel_class_init (CcNetworkPanelClass *klass)
         gtk_widget_class_bind_template_child (widget_class, CcNetworkPanel, box_wired);
         gtk_widget_class_bind_template_child (widget_class, CcNetworkPanel, container_bluetooth);
         gtk_widget_class_bind_template_child (widget_class, CcNetworkPanel, proxy_row);
+        gtk_widget_class_bind_template_child (widget_class, CcNetworkPanel, rfkill_row);
+        gtk_widget_class_bind_template_child (widget_class, CcNetworkPanel, rfkill_widget);
 
         gtk_widget_class_bind_template_callback (widget_class, create_connection_cb);
+        gtk_widget_class_bind_template_callback (widget_class, rfkill_switch_notify_activate_cb);
 
         g_type_ensure (CC_TYPE_LIST_ROW);
         g_type_ensure (CC_TYPE_NET_PROXY_PAGE);
@@ -770,6 +882,16 @@ cc_network_panel_init (CcNetworkPanel *self)
                         g_warning ("Error connecting to ModemManager: %s",
                                    error->message);
         }
+
+        /* Acquire Airplane Mode proxy */
+        cc_object_storage_create_dbus_proxy (G_BUS_TYPE_SESSION,
+                                             G_DBUS_PROXY_FLAGS_NONE,
+                                             "org.gnome.SettingsDaemon.Rfkill",
+                                             "/org/gnome/SettingsDaemon/Rfkill",
+                                             "org.gnome.SettingsDaemon.Rfkill",
+                                             cc_panel_get_cancellable (CC_PANEL (self)),
+                                             rfkill_proxy_acquired_cb,
+                                             self);
 
         /* add remote settings such as VPN settings as virtual devices */
         g_signal_connect_object (self->client, NM_CLIENT_CONNECTION_ADDED,
