GNOME/gnome-control-center

commit a284b9c77cc72bfe271fc746a1ccdce8a0307d6a

system, users: Reword password strength feedback strings
According to the mockups in
https://gitlab.gnome.org/Teams/Design/settings-mockups/-/blob/master/users/add-user.png

This removes the final period in all password hint sentences.

panels/system/users/pw-utils.c
@@ -91,45 +91,45 @@ pw_error_hint (gint error)
 {
         switch (error) {
         case PWQ_ERROR_SAME_PASSWORD:
-                return C_("Password hint", "The new password needs to be different from the old one.");
+                return C_("Password hint", "The new password needs to be different from the old one");
         case PWQ_ERROR_CASE_CHANGES_ONLY:
-                return C_("Password hint", "Try changing some letters and numbers.");
+                return C_("Password hint", "Try changing some letters and numbers");
         case PWQ_ERROR_TOO_SIMILAR:
-                return C_("Password hint", "Try changing the password a bit more.");
+                return C_("Password hint", "Try changing the password a bit more");
         case PWQ_ERROR_USER_CHECK:
-                return C_("Password hint", "A password without your user name would be stronger.");
+                return C_("Password hint", "A password without your user name would be stronger");
         case PWQ_ERROR_GECOS_CHECK:
-                return C_("Password hint", "Try to avoid using your name in the password.");
+                return C_("Password hint", "Try to avoid using your name in the password");
         case PWQ_ERROR_BAD_WORDS:
-                return C_("Password hint", "Try to avoid some of the words included in the password.");
+                return C_("Password hint", "Try to avoid some of the words included in the password");
         case PWQ_ERROR_ROTATED:
-                return C_("Password hint", "Try changing the password a bit more.");
+                return C_("Password hint", "Try changing the password a bit more");
         case PWQ_ERROR_CRACKLIB_CHECK:
-                return C_("Password hint", "Try to avoid common words.");
+                return C_("Password hint", "Avoid common words");
         case PWQ_ERROR_PALINDROME:
-                return C_("Password hint", "Try to avoid reordering existing words.");
+                return C_("Password hint", "Try to avoid reordering existing words");
         case PWQ_ERROR_MIN_DIGITS:
-                return C_("Password hint", "Try to use more numbers.");
+                return C_("Password hint", "Try to use more numbers");
         case PWQ_ERROR_MIN_UPPERS:
-                return C_("Password hint", "Try to use more uppercase letters.");
+                return C_("Password hint", "Try to use more uppercase letters");
         case PWQ_ERROR_MIN_LOWERS:
-                return C_("Password hint", "Try to use more lowercase letters.");
+                return C_("Password hint", "Try to use more lowercase letters");
         case PWQ_ERROR_MIN_OTHERS:
-                return C_("Password hint", "Try to use more special characters, like punctuation.");
+                return C_("Password hint", "Try to use more special characters, like punctuation");
         case PWQ_ERROR_MIN_CLASSES:
-                return C_("Password hint", "Try to use a mixture of letters, numbers and punctuation.");
+                return C_("Password hint", "Try to use a mixture of letters, numbers and punctuation");
         case PWQ_ERROR_MAX_CONSECUTIVE:
-                return C_("Password hint", "Try to avoid repeating the same character.");
+                return C_("Password hint", "Try to avoid repeating the same character");
         case PWQ_ERROR_MAX_CLASS_REPEAT:
-                return C_("Password hint", "Try to avoid repeating the same type of character: you need to mix up letters, numbers and punctuation.");
+                return C_("Password hint", "Try to avoid repeating the same type of character: you need to mix up letters, numbers and punctuation");
         case PWQ_ERROR_MAX_SEQUENCE:
-                return C_("Password hint", "Try to avoid sequences like 1234 or abcd.");
+                return C_("Password hint", "Try to avoid sequences like 1234 or abcd");
         case PWQ_ERROR_MIN_LENGTH:
-                return C_("Password hint", "Password needs to be longer. Try to add more letters, numbers and punctuation.");
+                return C_("Password hint", "Password needs to be longer");
         case PWQ_ERROR_EMPTY_PASSWORD:
-                return C_("Password hint", "Mix uppercase and lowercase and try to use a number or two.");
+                return C_("Password hint", "Mix uppercase and lowercase and try to use a number or two");
         default:
-                return C_("Password hint", "Adding more letters, numbers and punctuation will make the password stronger.");
+                return C_("Password hint", "Valid passsword. Try adding more letters, numbers and punctuation");
         }
 }
 
@@ -167,6 +167,8 @@ pw_strength (const gchar  *password,
 
         if (length && length < pw_min_length())
                 *hint = pw_error_hint (PWQ_ERROR_MIN_LENGTH);
+        else if (level == 5)
+                *hint = _("Great password!");
         else
                 *hint = pw_error_hint (rv);
 

commit d23ab7ecb8c251c76ddedbd1bcbd109aee2f02c9

users: Fix typo in "password" string
This was accidentally introduced in commit a284b9c77cc.

panels/system/users/pw-utils.c
@@ -140,7 +140,7 @@ pw_error_hint (gint error)
         case PWQ_ERROR_EMPTY_PASSWORD:
                 return C_("Password hint", "Mix uppercase and lowercase and try to use a number or two");
         default:
-                return C_("Password hint", "Valid passsword. Try adding more letters, numbers and punctuation");
+                return C_("Password hint", "Valid password. Try adding more letters, numbers and punctuation");
         }
 }
 