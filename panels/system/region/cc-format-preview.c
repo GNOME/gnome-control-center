b4edf6fbe5235e98507986eb883b6596af542be8
Hari Rana <theevilskeleton@riseup.net>
2024-08-07 09:38:10 -0400

    format-chooser: Adjust style
    The `region.css` file was added to style region-specific dialogs
    and widgets. The format-chooser dialog is set with the CSS name
    "formatchooser", and the format-preview preferences group is set
    to "formatpreview". The style hack of removing the `top-bar`
    class from the revealer was replaced with custom styling.
    
    This wraps every `AdwActionRow` directly within a `GtkBox`, to
    prevent showing row cards.
    
    The header title was tweaked to inherit the font size.

--- panels/system/region/cc-format-preview.c
+++ panels/system/region/cc-format-preview.c
@@ -32,7 +32,7 @@
 #include <glib/gi18n.h>
 
 struct _CcFormatPreview {
-  GtkBox     parent_instance;
+  AdwPreferencesGroup     parent_instance;
 
   AdwActionRow *date_format_row;
   AdwActionRow *date_time_format_row;
@@ -50,7 +50,7 @@ enum
   PROP_REGION
 };
 
-G_DEFINE_TYPE (CcFormatPreview, cc_format_preview, GTK_TYPE_BOX)
+G_DEFINE_TYPE (CcFormatPreview, cc_format_preview, ADW_TYPE_PREFERENCES_GROUP)
 
 static void
 display_date (AdwActionRow *row, GDateTime *dt, const gchar *format)
@@ -245,12 +245,23 @@ cc_format_preview_class_init (CcFormatPreviewClass *klass)
   gtk_widget_class_bind_template_child (widget_class, CcFormatPreview, number_format_row);
   gtk_widget_class_bind_template_child (widget_class, CcFormatPreview, paper_format_row);
   gtk_widget_class_bind_template_child (widget_class, CcFormatPreview, time_format_row);
+
+  gtk_widget_class_set_css_name (widget_class, "formatpreview");
 }
 
 void
 cc_format_preview_init (CcFormatPreview *self)
 {
+  g_autoptr(GtkCssProvider) provider = NULL;
+
   gtk_widget_init_template (GTK_WIDGET (self));
+
+  provider = gtk_css_provider_new ();
+  gtk_css_provider_load_from_resource (provider,
+                                       "/org/gnome/control-center/system/region/region.css");
+  gtk_style_context_add_provider_for_display (gdk_display_get_default (),
+                                              GTK_STYLE_PROVIDER (provider),
+                                              GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 }
 
 void
