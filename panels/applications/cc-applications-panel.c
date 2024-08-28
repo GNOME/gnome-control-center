fa994e0824b2551fa49d0b730b34225ffcbebf27
Michael Catanzaro <mcatanzaro@redhat.com>
2024-08-28 07:49:15 -0500

    applications: null check permissions
    Currently it looks like we crash if the permission variant is a non-null
    strv containing no strings.
    
    Probably fixes #3168

--- panels/applications/cc-applications-panel.c
+++ panels/applications/cc-applications-panel.c
@@ -408,7 +408,7 @@ get_notification_allowed (CcApplicationsPanel *self,
       *set = perms != NULL;
       /* FIXME: needs unreleased xdg-desktop-portals to write permissions on use */
       *set = TRUE;
-      *allowed = perms == NULL || strcmp (perms[0], "no") != 0;
+      *allowed = perms == NULL || g_strcmp0 (perms[0], "no") != 0;
     }
 }
 
@@ -473,7 +473,7 @@ get_background_allowed (CcApplicationsPanel *self,
 {
   g_auto(GStrv) perms = get_portal_permissions (self, "background", "background", app_id);
   *set = TRUE;
-  *allowed = perms == NULL || strcmp (perms[0], "no") != 0;
+  *allowed = perms == NULL || g_strcmp0 (perms[0], "no") != 0;
 }
 
 static void
@@ -504,7 +504,7 @@ get_wallpaper_allowed (CcApplicationsPanel *self,
   g_auto(GStrv) perms = get_portal_permissions (self, "wallpaper", "wallpaper", app_id);
 
   *set = perms != NULL;
-  *allowed = perms == NULL || strcmp (perms[0], "no") != 0;
+  *allowed = perms == NULL || g_strcmp0 (perms[0], "no") != 0;
 }
 
 static void
@@ -535,7 +535,7 @@ get_screenshot_allowed (CcApplicationsPanel *self,
   g_auto(GStrv) perms = get_portal_permissions (self, "screenshot", "screenshot", app_id);
 
   *set = perms != NULL;
-  *allowed = perms == NULL || strcmp (perms[0], "no") != 0;
+  *allowed = perms == NULL || g_strcmp0 (perms[0], "no") != 0;
 }
 
 static void
@@ -572,7 +572,7 @@ get_shortcuts_allowed (CcApplicationsPanel *self,
    * string value here.
    */
   *set = perms != NULL;
-  *granted = (perms != NULL) && g_ascii_strcasecmp (perms[0], "GRANTED") == 0;
+  *granted = (perms != NULL) && (perms[0] != NULL) && g_ascii_strcasecmp (perms[0], "GRANTED") == 0;
 }
 
 static void
@@ -613,7 +613,7 @@ get_device_allowed (CcApplicationsPanel *self,
   perms = get_portal_permissions (self, "devices", device, app_id);
 
   *set = perms != NULL;
-  *allowed = perms == NULL || strcmp (perms[0], "no") != 0;
+  *allowed = perms == NULL || g_strcmp0 (perms[0], "no") != 0;
 }
 
 static void
@@ -663,7 +663,7 @@ get_location_allowed (CcApplicationsPanel *self,
   perms = get_portal_permissions (self, "location", "location", app_id);
 
   *set = perms != NULL;
-  *allowed = perms == NULL || strcmp (perms[0], "NONE") != 0;
+  *allowed = perms == NULL || g_strcmp0 (perms[0], "NONE") != 0;
 }
 
 static void
