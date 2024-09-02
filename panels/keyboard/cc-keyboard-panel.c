661e6db83f96130d8e9b4c3aef8b206b25def44e
Nathan Pratta Teodosio <nathan.teodosio@canonical.com>
2024-09-02 14:20:08 +0200

    keyboard: Add None option for alternate key (lv3).
    The window only offers actual keys for the 3rd level, by default, right alt.
    
    A user might nonetheless want right alt to be just an alt, not a lv3 key.
    And so for all other modifiers. So offer the "None" option.

--- panels/keyboard/cc-keyboard-panel.c
+++ panels/keyboard/cc-keyboard-panel.c
@@ -63,6 +63,7 @@ static const CcXkbModifier LV3_MODIFIER = {
   N_("Alternate Characters Key"),
   N_("The alternate characters key can be used to enter additional characters. These are sometimes printed as a third-option on your keyboard."),
   (CcXkbOption[]){
+    { NC_("keyboard key", "None"),        "lv3:ralt_alt" },
     { NC_("keyboard key", "Left Alt"),    "lv3:lalt_switch" },
     { NC_("keyboard key", "Right Alt"),   "lv3:ralt_switch" },
     { NC_("keyboard key", "Left Super"),  "lv3:lwin_switch" },
