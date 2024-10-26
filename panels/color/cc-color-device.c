aa5e210dc9f9bc30f758e0a5a7b9fbd3325f3c9c
Automeris naranja <104251-automerisnaranja@users.noreply.gitlab.gnome.org>
2024-10-25 21:12:51 -0300

    color-device: Rename "color_device" to "self"

--- panels/color/cc-color-device.c
+++ panels/color/cc-color-device.c
@@ -58,7 +58,7 @@ enum
 static guint signals [SIGNAL_LAST] = { 0 };
 
 static void
-cc_color_device_refresh (CcColorDevice *color_device)
+cc_color_device_refresh (CcColorDevice *self)
 {
   g_autofree gchar *title = NULL;
   g_autoptr(GPtrArray) profiles = NULL;
@@ -66,56 +66,56 @@ cc_color_device_refresh (CcColorDevice *color_device)
   g_autofree gchar *name2 = NULL;
 
   /* add switch and expander if there are profiles, otherwise use a label */
-  profiles = cd_device_get_profiles (color_device->device);
+  profiles = cd_device_get_profiles (self->device);
   if (profiles == NULL)
     return;
 
-  title = cc_color_device_get_title (color_device->device);
-  adw_preferences_row_set_title (ADW_PREFERENCES_ROW (color_device), title);
+  title = cc_color_device_get_title (self->device);
+  adw_preferences_row_set_title (ADW_PREFERENCES_ROW (self), title);
 
-  gtk_widget_set_visible (color_device->widget_switch, profiles->len > 0);
-  gtk_widget_set_visible (color_device->widget_button, profiles->len > 0);
-  gtk_button_set_icon_name (GTK_BUTTON (color_device->widget_button),
-                                color_device->expanded ? "pan-down-symbolic" : "pan-end-symbolic");
-  gtk_widget_set_visible (color_device->widget_nocalib, profiles->len == 0);
-  gtk_widget_set_sensitive (color_device->widget_button, cd_device_get_enabled (color_device->device));
-  gtk_switch_set_active (GTK_SWITCH (color_device->widget_switch),
-                         cd_device_get_enabled (color_device->device));
+  gtk_widget_set_visible (self->widget_switch, profiles->len > 0);
+  gtk_widget_set_visible (self->widget_button, profiles->len > 0);
+  gtk_button_set_icon_name (GTK_BUTTON (self->widget_button),
+                            self->expanded ? "pan-down-symbolic" : "pan-end-symbolic");
+  gtk_widget_set_visible (self->widget_nocalib, profiles->len == 0);
+  gtk_widget_set_sensitive (self->widget_button, cd_device_get_enabled (self->device));
+  gtk_switch_set_active (GTK_SWITCH (self->widget_switch),
+                         cd_device_get_enabled (self->device));
 
   name1 = g_strdup_printf (_("Enable color management for %s"), title);
-  gtk_accessible_update_property (GTK_ACCESSIBLE (color_device->widget_switch),
+  gtk_accessible_update_property (GTK_ACCESSIBLE (self->widget_switch),
                                   GTK_ACCESSIBLE_PROPERTY_LABEL, name1,
                                   -1);
 
   name2 = g_strdup_printf (_("Show color profiles for %s"), title);
-  gtk_accessible_update_property (GTK_ACCESSIBLE (color_device->widget_button),
+  gtk_accessible_update_property (GTK_ACCESSIBLE (self->widget_button),
                                   GTK_ACCESSIBLE_PROPERTY_LABEL, name2,
                                   -1);
 }
 
 CdDevice *
-cc_color_device_get_device (CcColorDevice *color_device)
+cc_color_device_get_device (CcColorDevice *self)
 {
-  g_return_val_if_fail (CC_IS_COLOR_DEVICE (color_device), NULL);
-  return color_device->device;
+  g_return_val_if_fail (CC_IS_COLOR_DEVICE (self), NULL);
+  return self->device;
 }
 
 const gchar *
-cc_color_device_get_sortable (CcColorDevice *color_device)
+cc_color_device_get_sortable (CcColorDevice *self)
 {
-  g_return_val_if_fail (CC_IS_COLOR_DEVICE (color_device), NULL);
-  return color_device->sortable;
+  g_return_val_if_fail (CC_IS_COLOR_DEVICE (self), NULL);
+  return self->sortable;
 }
 
 static void
 cc_color_device_get_property (GObject *object, guint param_id,
                               GValue *value, GParamSpec *pspec)
 {
-  CcColorDevice *color_device = CC_COLOR_DEVICE (object);
+  CcColorDevice *self = CC_COLOR_DEVICE (object);
   switch (param_id)
     {
       case PROP_DEVICE:
-        g_value_set_object (value, color_device->device);
+        g_value_set_object (value, self->device);
         break;
       default:
         G_OBJECT_WARN_INVALID_PROPERTY_ID (object, param_id, pspec);
@@ -127,12 +127,12 @@ static void
 cc_color_device_set_property (GObject *object, guint param_id,
                               const GValue *value, GParamSpec *pspec)
 {
-  CcColorDevice *color_device = CC_COLOR_DEVICE (object);
+  CcColorDevice *self = CC_COLOR_DEVICE (object);
 
   switch (param_id)
     {
       case PROP_DEVICE:
-        color_device->device = g_value_dup_object (value);
+        self->device = g_value_dup_object (value);
         break;
       default:
         G_OBJECT_WARN_INVALID_PROPERTY_ID (object, param_id, pspec);
@@ -143,86 +143,86 @@ cc_color_device_set_property (GObject *object, guint param_id,
 static void
 cc_color_device_finalize (GObject *object)
 {
-  CcColorDevice *color_device = CC_COLOR_DEVICE (object);
+  CcColorDevice *self = CC_COLOR_DEVICE (object);
 
-  g_free (color_device->sortable);
-  g_object_unref (color_device->device);
+  g_free (self->sortable);
+  g_object_unref (self->device);
 
   G_OBJECT_CLASS (cc_color_device_parent_class)->finalize (object);
 }
 
 void
-cc_color_device_set_expanded (CcColorDevice *color_device,
+cc_color_device_set_expanded (CcColorDevice *self,
                               gboolean expanded)
 {
   /* same as before */
-  if (color_device->expanded == expanded)
+  if (self->expanded == expanded)
     return;
 
   /* refresh */
-  color_device->expanded = expanded;
-  g_signal_emit (color_device,
+  self->expanded = expanded;
+  g_signal_emit (self,
                  signals[SIGNAL_EXPANDED_CHANGED], 0,
-                 color_device->expanded);
-  cc_color_device_refresh (color_device);
+                 self->expanded);
+  cc_color_device_refresh (self);
 }
 
 static void
-cc_color_device_notify_enable_device_cb (CcColorDevice *color_device)
+cc_color_device_notify_enable_device_cb (CcColorDevice *self)
 {
   gboolean enable;
   gboolean ret;
   g_autoptr(GError) error = NULL;
 
-  enable = gtk_switch_get_active (GTK_SWITCH (color_device->widget_switch));
-  g_debug ("Set %s to %i", cd_device_get_id (color_device->device), enable);
-  ret = cd_device_set_enabled_sync (color_device->device,
+  enable = gtk_switch_get_active (GTK_SWITCH (self->widget_switch));
+  g_debug ("Set %s to %i", cd_device_get_id (self->device), enable);
+  ret = cd_device_set_enabled_sync (self->device,
                                     enable, NULL, &error);
   if (!ret)
     g_warning ("failed to %s to the device: %s",
                enable ? "enable" : "disable", error->message);
 
   /* if expanded, close */
-  cc_color_device_set_expanded (color_device, FALSE);
+  cc_color_device_set_expanded (self, FALSE);
 }
 
 static void
-cc_color_device_changed_cb (CcColorDevice *color_device)
+cc_color_device_changed_cb (CcColorDevice *self)
 {
-  cc_color_device_refresh (color_device);
+  cc_color_device_refresh (self);
 }
 
 static void
-cc_color_device_clicked_expander_cb (CcColorDevice *color_device)
+cc_color_device_clicked_expander_cb (CcColorDevice *self)
 {
-  color_device->expanded = !color_device->expanded;
-  cc_color_device_refresh (color_device);
-  g_signal_emit (color_device, signals[SIGNAL_EXPANDED_CHANGED], 0,
-                 color_device->expanded);
+  self->expanded = !self->expanded;
+  cc_color_device_refresh (self);
+  g_signal_emit (self, signals[SIGNAL_EXPANDED_CHANGED], 0,
+                 self->expanded);
 }
 
 static void
 cc_color_device_constructed (GObject *object)
 {
-  CcColorDevice *color_device = CC_COLOR_DEVICE (object);
+  CcColorDevice *self = CC_COLOR_DEVICE (object);
   g_autofree gchar *sortable_tmp = NULL;
 
   /* watch the device for changes */
-  g_signal_connect_object (color_device->device, "changed",
-                           G_CALLBACK (cc_color_device_changed_cb), color_device, G_CONNECT_SWAPPED);
+  g_signal_connect_object (self->device, "changed",
+                           G_CALLBACK (cc_color_device_changed_cb), self, G_CONNECT_SWAPPED);
 
   /* calculate sortable -- FIXME: we have to hack this as EggListBox
    * does not let us specify a GtkSortType:
    * https://bugzilla.gnome.org/show_bug.cgi?id=691341 */
-  sortable_tmp = cc_color_device_get_sortable_base (color_device->device);
-  color_device->sortable = g_strdup_printf ("%sXX", sortable_tmp);
+  sortable_tmp = cc_color_device_get_sortable_base (self->device);
+  self->sortable = g_strdup_printf ("%sXX", sortable_tmp);
 
-  cc_color_device_refresh (color_device);
+  cc_color_device_refresh (self);
 
   /* watch to see if the user flicked the switch */
-  g_signal_connect_object (color_device->widget_switch, "notify::active",
+  g_signal_connect_object (self->widget_switch, "notify::active",
                            G_CALLBACK (cc_color_device_notify_enable_device_cb),
-                           color_device, G_CONNECT_SWAPPED);
+                           self, G_CONNECT_SWAPPED);
 }
 
 static void
@@ -258,9 +258,9 @@ cc_color_device_class_init (CcColorDeviceClass *klass)
 }
 
 static void
-cc_color_device_init (CcColorDevice *color_device)
+cc_color_device_init (CcColorDevice *self)
 {
-  gtk_widget_init_template (GTK_WIDGET (color_device));
+  gtk_widget_init_template (GTK_WIDGET (self));
 }
 
 GtkWidget *
@@ -270,4 +270,3 @@ cc_color_device_new (CdDevice *device)
                        "device", device,
                        NULL);
 }
-
