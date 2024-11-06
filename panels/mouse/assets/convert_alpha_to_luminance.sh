dd4e25ab6983230238e923e71c6695f158fb7113
Matthijs Velsink <mvelsink@gnome.org>
2024-11-06 23:08:05 +0100

    mouse: Convert VP9 webm with alpha to monochrome variants
    Loading VP9 with an alpha channel requires the `vp9alphadecodebin`
    plugin, which is from the GStreamer Bad Plug-ins. These are not always
    installed.
    
    Instead, convert the alpha value of these videos to monochrome, and then
    using the previous commit their masked value will be identical.
    
    To make this work, we do need to ensure bt709 color space, otherwise we
    would run into https://gitlab.gnome.org/GNOME/gtk/-/issues/7124, which
    is a result of
    https://gitlab.freedesktop.org/gstreamer/gstreamer/-/issues/3479.
    
    Closes #3173
    
    (cherry picked from commit 8de3dd0f4b1a211c4381caa45877d7ad067a2f97)

---
+++ panels/mouse/assets/convert_alpha_to_luminance.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Requires at least one input file'
+    exit 0
+fi
+
+convert () {
+    ffmpeg -c:v libvpx-vp9 -i "$1" -vf "
+    geq=r='alpha(X, Y)':g='alpha(X, Y)':b='alpha(X, Y)'
+    " -metadata:s:v:0 alpha_mode=0 -pix_fmt yuv420p -colorspace bt709 -crf 5 -b:v 0 _tmp.webm
+
+    mv _tmp.webm "$1"
+}
+
+for file in "$@"
+do
+    convert "$file"
+done
