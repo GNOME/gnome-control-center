b0833a8d743e4dc1259f85c75c4ecee7c50611b4
Felipe Borges <felipeborges@gnome.org>
2024-11-07 14:17:20 +0100

    common, permission-infobar: Set default title strings
    Preset the banner title on the widget itself so that panels don't need
    to overwrite it.
    
    This also decouples the "Unlock" button label setting from the
    cc_permission_infobar_set_title method so that the widget can function
    even when consumers don't call cc_permission_infobar_set_title.

--- panels/common/cc-permission-infobar.c
+++ panels/common/cc-permission-infobar.c
@@ -50,6 +50,11 @@ on_permission_changed (CcPermissionInfobar *self)
   gboolean is_authorized = g_permission_get_allowed (self->permission);
 
   adw_banner_set_revealed (self->banner, !is_authorized);
+  if (!is_authorized)
+    {
+      adw_banner_set_title (self->banner, _("Some settings are locked"));
+      adw_banner_set_button_label (self->banner, _("_Unlock…"));
+    }
 }
 
 static void
@@ -156,9 +161,6 @@ static void
 cc_permission_infobar_init (CcPermissionInfobar *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
-
-  /* Set the default title. */
-  cc_permission_infobar_set_title (self, NULL);
 }
 
 void
@@ -195,12 +197,5 @@ cc_permission_infobar_set_title (CcPermissionInfobar *self,
 {
   g_return_if_fail (CC_IS_PERMISSION_INFOBAR (self));
 
-  if (self->permission == NULL)
-    title = _("Error — some settings cannot be unlocked");
-
-  if (title == NULL)
-    title = _("Unlock to Change Settings");
-
   adw_banner_set_title (self->banner, title);
-  adw_banner_set_button_label (self->banner, self->permission ? _("_Unlock…") : NULL);
 }
