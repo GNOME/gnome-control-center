7a92e56a4bb9e74b65d8334e65a903a82b0c6bf6
Automeris naranja <automeris@tuta.io>
2024-11-25 21:44:34 -0300

    xkb-modifier-page: Make the code better readable in "add_radio_buttons"

--- panels/keyboard/cc-xkb-modifier-page.c
+++ panels/keyboard/cc-xkb-modifier-page.c
@@ -242,16 +242,18 @@ add_radio_buttons (CcXkbModifierPage *self)
   for (i = 0; options[i].label && options[i].xkb_option; i++)
     {
       row = adw_action_row_new ();
-      adw_preferences_group_add (self->options_group, row);
+      adw_preferences_row_set_title (ADW_PREFERENCES_ROW (row), options[i].label);
 
       radio_button = gtk_check_button_new ();
       gtk_widget_set_valign (radio_button, GTK_ALIGN_CENTER);
       gtk_check_button_set_group (GTK_CHECK_BUTTON (radio_button), GTK_CHECK_BUTTON (last_button));
       g_object_set_data (G_OBJECT (radio_button), "xkb-option", options[i].xkb_option);
       g_signal_connect_object (radio_button, "toggled", (GCallback)on_active_radio_changed_cb, self, G_CONNECT_SWAPPED);
+
       adw_action_row_add_prefix (ADW_ACTION_ROW (row), radio_button);
-      adw_preferences_row_set_title (ADW_PREFERENCES_ROW (row), options[i].label);
       adw_action_row_set_activatable_widget (ADW_ACTION_ROW (row), radio_button);
+
+      adw_preferences_group_add (self->options_group, row);
 
       last_button = radio_button;
       group = g_slist_prepend (group, radio_button);
