c4a8396a17efaf463abb089451e04856a4220e17
Matthijs Velsink <mvelsink@gnome.org>
2025-05-13 08:38:22 +0200

    system: Refactor and improve password generation
    We don't have to load the entire file for every word in the password.
    Instead, we load the whole file line by line into an array. This also
    gives us the linecount automatically.
    
    With that, we can also fix the slight bias in the random integer
    generation, as just taking the modulo with the linecount is not
    completely accurate. Using the glib function to generate a random
    integer in a range does give uniform randomness.
    
    (cherry picked from commit 50fb63a13f7bb10bf599843f9969b042caa8c4b6)

--- panels/system/cc-password-utils.c
+++ panels/system/cc-password-utils.c
@@ -22,16 +22,20 @@
 #include <glib.h>
 #include <glob.h>
 
-static char *
-get_word_at_line (GFile *file,
-                  guint  line_number)
+static GPtrArray *
+get_ascii_lines_from_uri (const char *uri)
 {
-  g_autoptr (GError) error = NULL;
-  g_autoptr (GFileInputStream) input_stream = NULL;
-  g_autoptr (GDataInputStream) data = NULL;
+  g_autoptr(GFile) file = NULL;
+  g_autoptr(GError) error = NULL;
+  g_autoptr(GFileInputStream) input_stream = NULL;
+  g_autoptr(GDataInputStream) data = NULL;
+  g_autoptr(GPtrArray) ascii_lines = NULL;
   g_autofree char *line = NULL;
-  gsize length;
-  guint line_count = 0;
+
+  file = g_file_new_for_uri (uri);
+
+  if (!file)
+    return NULL;
 
   input_stream = g_file_read (file, NULL, &error);
 
@@ -42,14 +46,13 @@ get_word_at_line (GFile *file,
     }
 
   data = g_data_input_stream_new (G_INPUT_STREAM (input_stream));
+  ascii_lines = g_ptr_array_new_with_free_func (g_free);
 
-  while ((line = g_data_input_stream_read_line_utf8 (data, &length, NULL, &error)) != NULL)
+  while ((line = g_data_input_stream_read_line_utf8 (data, NULL, NULL, &error)) != NULL)
     {
-      if (line_count == line_number)
-        return g_str_to_ascii (line, NULL);
+      g_ptr_array_add (ascii_lines, g_str_to_ascii (line, NULL));
 
       g_clear_pointer (&line, g_free);
-      line_count++;
     }
 
   if (error != NULL)
@@ -58,47 +61,37 @@ get_word_at_line (GFile *file,
       return NULL;
     }
 
-  if (line_count == 0)
-    {
-      g_warning ("File is empty");
-      return NULL;
-    }
-
-  if (line_number >= line_count)
-    return get_word_at_line (file, line_number % line_count);
-
-  return NULL;
+  return g_steal_pointer (&ascii_lines);
 }
 
 char *
 cc_generate_password (void)
 {
   g_autoptr(GString) password_string = NULL;
-  g_autoptr(GFile) file = NULL;
-  static const int number_of_words = 5;
+  g_autoptr(GPtrArray) ascii_lines = NULL;
+  static const int number_of_password_words = 5;
+  int number_of_file_words;
 
-  file = g_file_new_for_uri ("resource://org/gnome/control-center/system/wordlist.txt");
-
-  if (!file)
+  ascii_lines = get_ascii_lines_from_uri ("resource://org/gnome/control-center/system/wordlist.txt");
+  
+  if (!ascii_lines)
     return NULL;
 
+  number_of_file_words = ascii_lines->len;
   password_string = g_string_new (NULL);
 
-  for (int i = 0; i < number_of_words; i++)
+  for (int i = 0; i < number_of_password_words; i++)
     {
-      int word_offset = g_random_int ();
-      g_autofree char *word = NULL;
+      int word_offset = g_random_int_range (0, number_of_file_words);
+      char *word;
 
-      word = get_word_at_line (file, word_offset);
-
-      if (word == NULL)
-        return NULL;
+      word = ascii_lines->pdata[word_offset];
 
       /* Capitalize every first letter */
       word[0] = g_ascii_toupper (word[0]);
 
       g_string_append (password_string, word);
     }
-    
+
   return g_string_free_and_steal (g_steal_pointer (&password_string));
 }
