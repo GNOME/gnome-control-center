3ca056ef9d4a98d6b0841ac37821aee789d1aa89
Matthijs Velsink <mvelsink@gnome.org>
2025-07-01 13:33:44 +0000

    power: Move general section above Power saving
    So that the widget doesn't jump when the suspend power consumption
    notice is revealed. Making the suspend notice the bottom of the page
    solves it.
    
    Fixes #3373
    
    Co-authored-by: Matthijs Velsink <mvelsink@gnome.org>
    Part-of: <https://gitlab.gnome.org/GNOME/gnome-control-center/-/merge_requests/3143>
    
    
    (cherry picked from commit a918122f5d2b767b65e30086a59f4edd47bce083)
    
    Co-authored-by: Felipe Borges <felipeborges@gnome.org>

--- panels/power/cc-power-panel.c
+++ panels/power/cc-power-panel.c
@@ -1183,13 +1183,11 @@ switch_to_single_page_layout (CcPowerPanel *self)
   adw_preferences_page_remove (self->power_saving_page, self->suspend_on_ac_group);
   adw_preferences_page_remove (self->power_saving_page, self->suspend_notice_group);
 
-  adw_preferences_page_remove (self->general_page, self->general_section);
   adw_preferences_page_add (self->general_page, self->power_saving_group);
   adw_preferences_page_add (self->general_page, self->blank_screen_group);
   adw_preferences_page_add (self->general_page, self->suspend_on_battery_group);
   adw_preferences_page_add (self->general_page, self->suspend_on_ac_group);
   adw_preferences_page_add (self->general_page, self->suspend_notice_group);
-  adw_preferences_page_add (self->general_page, self->general_section);
 
   /* Reset title and hide view switcher by hiding stack page */
   gtk_stack_set_visible_child_name (self->title_stack, "title");
