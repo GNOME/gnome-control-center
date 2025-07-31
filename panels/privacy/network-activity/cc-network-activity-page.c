0452e347a66206980b11f1a58b1383c78236379c
James Henstridge <james@jamesh.id.au>
2025-07-31 15:36:05 +0200

    privacy, network-activity: Wire network login detection toggle
    This patch is heavily based on the Ubuntu downstream one found at
    https://salsa.debian.org/gnome-team/gnome-control-center/-/blob/ubuntu/plucky/debian/patches/ubuntu/connectivity-add-network-connectivity-checking-toggle.patch?ref_type=heads
    
    For this reason, I am adding the original commit author and appending
    my name below.
    
    Co-authored-by: Felipe Borges <felipeborges@gnome.org>

--- panels/privacy/network-activity/cc-network-activity-page.c
+++ panels/privacy/network-activity/cc-network-activity-page.c
@@ -19,22 +19,85 @@
  */
 
 #include "cc-network-activity-page.h"
+#include "shell/cc-object-storage.h"
 
 #include <glib/gi18n.h>
 
+#include <NetworkManager.h>
 struct _CcNetworkActivityPage
 {
   AdwNavigationPage    parent_instance;
+
+  AdwSwitchRow        *network_login_dectection_row;
+
+  GCancellable        *cancellable;
+  NMClient            *nm_client;
+
 };
 
 G_DEFINE_TYPE (CcNetworkActivityPage, cc_network_activity_page, ADW_TYPE_NAVIGATION_PAGE)
 
 static void
+setup_nm_client (CcNetworkActivityPage *self,
+                 NMClient              *client)
+{
+  self->nm_client = client;
+
+  g_object_bind_property (self->nm_client, NM_CLIENT_CONNECTIVITY_CHECK_AVAILABLE,
+                          self->network_login_dectection_row, "sensitive",
+                          G_BINDING_SYNC_CREATE);
+
+  g_object_bind_property (self->nm_client, NM_CLIENT_CONNECTIVITY_CHECK_ENABLED,
+                          self->network_login_dectection_row, "active",
+                          G_BINDING_BIDIRECTIONAL | G_BINDING_SYNC_CREATE);
+}
+
+static void
+nm_client_ready_cb (GObject      *source_object,
+                    GAsyncResult *res,
+                    gpointer      user_data)
+{
+  CcNetworkActivityPage *self;
+  NMClient *client;
+  g_autoptr(GError) error = NULL;
+
+  client = nm_client_new_finish (res, &error);
+  if (!client) {
+    if (!g_error_matches (error, G_IO_ERROR, G_IO_ERROR_CANCELLED)) {
+      g_warning ("Failed to create NetworkManager client: %s", error->message);
+      return;
+    }
+  }
+
+  self = user_data;
+
+  setup_nm_client (self, client);
+  cc_object_storage_add_object (CC_OBJECT_NMCLIENT, client);
+}
+
+static void
+cc_network_activity_page_finalize (GObject *object)
+{
+  CcNetworkActivityPage *self = CC_NETWORK_ACTIVITY_PAGE (object);
+
+  g_cancellable_cancel (self->cancellable);
+  g_clear_object (&self->nm_client);
+
+  G_OBJECT_CLASS (cc_network_activity_page_parent_class)->finalize (object);
+}
+
+
+static void
 cc_network_activity_page_class_init (CcNetworkActivityPageClass *klass)
 {
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
+  object_class->finalize = cc_network_activity_page_finalize;
+
   gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/control-center/privacy/network-activity/cc-network-activity-page.ui");
+
+  gtk_widget_class_bind_template_child (widget_class, CcNetworkActivityPage, network_login_dectection_row);
 }
 
 static void
@@ -42,4 +105,11 @@ cc_network_activity_page_init (CcNetworkActivityPage *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
 
+  self->cancellable = g_cancellable_new ();
+
+  if (cc_object_storage_has_object (CC_OBJECT_NMCLIENT)) {
+    setup_nm_client (self, cc_object_storage_get_object (CC_OBJECT_NMCLIENT));
+  } else {
+    nm_client_new_async (self->cancellable, nm_client_ready_cb, self);
+  }
 }
