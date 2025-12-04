40ddbd5b3bfad61d06f3a4d1979724f9308ead4e
Philip Withnall <pwithnall@gnome.org>
2025-12-04 15:56:08 +0000

    wellbeing: Fix SPDX license identifier mismatch
    Looks like I consistently typoed the SPDX license identifier in these
    files when first writing them. The SPDX identifier is meant to match the
    verbose license terms above, so fixing this mistake does not mean we’re
    changing the license of these files (so should be straightforward to
    merge). It just means we’re fixing the machine readable representation
    of it.
    
    Also change a typo in the verbose license terms to fix the LGPL version.
    Realistically nobody uses LGPL 2.0 (as opposed to version 2.1), and it
    certainly wasn’t my intention to use LGPL 2.0 when writing these files.
    It probably arose from copying the license terms from a GPL 2.0 file and
    not changing them properly.
    
    Signed-off-by: Philip Withnall <pwithnall@gnome.org>
    
    Fixes: #3627

--- panels/wellbeing/cc-wellbeing-panel.c
+++ panels/wellbeing/cc-wellbeing-panel.c
@@ -6,7 +6,7 @@
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -19,7 +19,7 @@
  * Authors:
  *  - Philip Withnall <pwithnall@gnome.org>
  *
- * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
 #include "config.h"
