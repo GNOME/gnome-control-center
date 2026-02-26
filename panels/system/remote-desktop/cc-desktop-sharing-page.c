GNOME/gnome-control-center

commit 5940af5b204d62de68e67258ea7551abb1d8219b

remote-desktop, desktop-sharing: Handle when keyring is locked
The desktop sharing credentials can't be loaded neither stored when the
session keyring is locked.

So let's show a permission banner asking the user to "Unlock" and keep
the Login Details widgets sensitive when locked.

Fixes #3547

panels/system/remote-desktop/cc-desktop-sharing-page.c
@@ -60,10 +60,12 @@ struct _CcDesktopSharingPage {
 
   GsdRemoteDesktopRdpServer *rdp_server;
 
+  AdwBanner    *keyring_infobar;
   AdwSwitchRow *desktop_sharing_row;
   AdwSwitchRow *remote_control_row;
   AdwActionRow *hostname_row;
   AdwActionRow *port_row;
+  GtkWidget    *login_details_group;
   GtkWidget    *username_entry;
   GtkWidget    *password_entry;
   AdwButtonRow *generate_password_button_row;
@@ -387,16 +389,10 @@ on_password_copy_clicked (CcDesktopSharingPage *self,
 }
 
 static void
-setup_desktop_sharing_page (CcDesktopSharingPage *self)
+load_credentials (CcDesktopSharingPage *self)
 {
   g_autofree gchar *username = NULL;
   g_autofree gchar *password = NULL;
-  g_autofree char *hostname = NULL;
-
-  self->rdp_settings = g_settings_new (GNOME_REMOTE_DESKTOP_RDP_SCHEMA_ID);
-
-  hostname = get_hostname ();
-  adw_action_row_set_subtitle (self->hostname_row, hostname);
 
   username = cc_grd_lookup_rdp_username (self->cancellable);
   password = cc_grd_lookup_rdp_password (self->cancellable);
@@ -405,14 +401,7 @@ setup_desktop_sharing_page (CcDesktopSharingPage *self)
   if (password != NULL)
     gtk_editable_set_text (GTK_EDITABLE (self->password_entry), password);
 
-  g_signal_connect_swapped (self->username_entry,
-                            "notify::text",
-                            G_CALLBACK (on_credentials_changed),
-                            self);
-  g_signal_connect_swapped (self->password_entry,
-                            "notify::text",
-                            G_CALLBACK (on_credentials_changed),
-                            self);
+  /* No credentials available. Let's create them. */
   if (username == NULL)
     {
       struct passwd *pw = getpwuid (getuid ());
@@ -429,7 +418,125 @@ setup_desktop_sharing_page (CcDesktopSharingPage *self)
       if (pw != NULL)
         gtk_editable_set_text (GTK_EDITABLE (self->password_entry), pw);
     }
+}
 
+static void
+unlock_gnome_keyring_cb (GObject      *source_object,
+                         GAsyncResult *res,
+                         gpointer      user_data)
+{
+  CcDesktopSharingPage *self = CC_DESKTOP_SHARING_PAGE (user_data);
+  g_autoptr(GList) unlocked = NULL;
+
+  secret_service_unlock_finish (NULL, res, &unlocked, NULL);
+  /* If the keyring is locked,  */
+  if (unlocked == NULL)
+      return;
+
+  adw_banner_set_revealed (self->keyring_infobar, FALSE);
+  gtk_widget_set_sensitive (self->login_details_group, TRUE);
+  gtk_widget_set_sensitive (GTK_WIDGET (self->generate_password_button_row), TRUE);
+
+  load_credentials (self);
+}
+
+static void
+unlock_gnome_keyring (CcDesktopSharingPage *self)
+{
+  g_autoptr(SecretService) service = NULL;
+  g_autoptr(GList) collections_to_unlock = NULL;
+  g_autoptr(SecretCollection) collection = NULL;
+  g_autoptr(GError) error = NULL;
+
+  service = secret_service_get_sync (SECRET_SERVICE_LOAD_COLLECTIONS, NULL, &error);
+  if (!service)
+    {
+      g_debug ("Can't connect to secret service: %s", error->message);
+      return;
+    }
+
+  collection = secret_collection_for_alias_sync (service,
+                                                 SECRET_COLLECTION_DEFAULT,
+                                                 SECRET_COLLECTION_LOAD_ITEMS,
+                                                 NULL, &error);
+  if (!collection)
+    {
+      g_debug ("Can't load secret collection: %s", error->message);
+      return;
+    }
+
+  collections_to_unlock = g_list_append (collections_to_unlock, collection);
+  secret_service_unlock (NULL,
+                         collections_to_unlock,
+                         self->cancellable,
+                         (GAsyncReadyCallback) unlock_gnome_keyring_cb,
+                         self);
+}
+
+static void
+setup_login_details_group (GObject      *source_object,
+                           GAsyncResult *result,
+                           gpointer      user_data)
+{
+  CcDesktopSharingPage *self = CC_DESKTOP_SHARING_PAGE (user_data);
+  g_autoptr(SecretCollection) collection = NULL;
+  gboolean locked = TRUE;
+  g_autoptr(SecretService) service = NULL;
+  g_autoptr(GError) error = NULL;
+
+  service = secret_service_get_finish (result, &error);
+  if (!service)
+    {
+      g_debug ("Can't connect to secret service: %s", error->message);
+      return;
+    }
+
+  collection = secret_collection_for_alias_sync (service,
+                                                 SECRET_COLLECTION_DEFAULT,
+                                                 SECRET_COLLECTION_LOAD_ITEMS,
+                                                 NULL, &error);
+  if (!collection)
+    {
+      g_debug ("Can't load secret collection: %s", error->message);
+      return;
+    }
+
+  locked = secret_collection_get_locked (collection);
+  g_debug ("Secret collection locked: %s", locked ? "yes" : "no");
+
+  adw_banner_set_revealed (self->keyring_infobar, locked);
+  gtk_widget_set_sensitive (self->login_details_group, !locked);
+  gtk_widget_set_sensitive (GTK_WIDGET (self->generate_password_button_row), !locked);
+
+  if (!locked)
+      load_credentials (self);
+}
+
+static void
+setup_desktop_sharing_page (CcDesktopSharingPage *self)
+{
+  g_autofree char *hostname = NULL;
+
+  self->rdp_settings = g_settings_new (GNOME_REMOTE_DESKTOP_RDP_SCHEMA_ID);
+
+  hostname = get_hostname ();
+  adw_action_row_set_subtitle (self->hostname_row, hostname);
+
+  /* Initialize credentials only when keyring is accessible.
+   * See https://gitlab.gnome.org/GNOME/gnome-control-center/-/issues/3547 */
+  secret_service_get (SECRET_SERVICE_LOAD_COLLECTIONS,
+                      self->cancellable,
+                      (GAsyncReadyCallback)setup_login_details_group,
+                      self);
+
+  g_signal_connect_swapped (self->username_entry,
+                            "notify::text",
+                            G_CALLBACK (on_credentials_changed),
+                            self);
+  g_signal_connect_swapped (self->password_entry,
+                            "notify::text",
+                            G_CALLBACK (on_credentials_changed),
+                            self);
   g_signal_connect_object (self->desktop_sharing_row, "notify::active",
                            G_CALLBACK (on_desktop_sharing_active_changed), self,
                            G_CONNECT_SWAPPED);
@@ -517,10 +624,12 @@ cc_desktop_sharing_page_class_init (CcDesktopSharingPageClass * klass)
 
   gtk_widget_class_bind_template_child (widget_class, CcDesktopSharingPage, toast_overlay);
 
+  gtk_widget_class_bind_template_child (widget_class, CcDesktopSharingPage, keyring_infobar);
   gtk_widget_class_bind_template_child (widget_class, CcDesktopSharingPage, desktop_sharing_row);
   gtk_widget_class_bind_template_child (widget_class, CcDesktopSharingPage, remote_control_row);
   gtk_widget_class_bind_template_child (widget_class, CcDesktopSharingPage, hostname_row);
   gtk_widget_class_bind_template_child (widget_class, CcDesktopSharingPage, port_row);
+  gtk_widget_class_bind_template_child (widget_class, CcDesktopSharingPage, login_details_group);
   gtk_widget_class_bind_template_child (widget_class, CcDesktopSharingPage, username_entry);
   gtk_widget_class_bind_template_child (widget_class, CcDesktopSharingPage, password_entry);
   gtk_widget_class_bind_template_child (widget_class, CcDesktopSharingPage, generate_password_button_row);
@@ -532,6 +641,7 @@ cc_desktop_sharing_page_class_init (CcDesktopSharingPageClass * klass)
   gtk_widget_class_bind_template_callback (widget_class, on_password_copy_clicked);
   gtk_widget_class_bind_template_callback (widget_class, on_generate_password_button_row_activated);
   gtk_widget_class_bind_template_callback (widget_class, on_verify_encryption_button_row_activated);
+  gtk_widget_class_bind_template_callback (widget_class, unlock_gnome_keyring);
 }
 
 static gboolean

panels/system/remote-desktop/cc-desktop-sharing-page.blp
@@ -3,133 +3,144 @@ using Adw 1;
 
 template $CcDesktopSharingPage: Adw.Bin {
   Adw.ToastOverlay toast_overlay {
-    child: Adw.PreferencesPage {
-      Adw.PreferencesGroup {
-        Label {
-          label: _("Share your existing desktop with other devices. The remote connection uses the existing screen resolution.");
-          hexpand: true;
-          wrap: true;
-          justify: center;
+    child: Box {
+      orientation: vertical;
 
-          styles [
-            "dim-label",
-          ]
-        }
+      Adw.Banner keyring_infobar {
+        title: _("Some settings are locked");
+        button-label: _("Unlock…");
+        button-style: suggested;
+        button-clicked => $unlock_gnome_keyring(template);
       }
 
-      Adw.PreferencesGroup {
-        Adw.SwitchRow desktop_sharing_row {
-          title: _("_Desktop Sharing");
-          use-underline: true;
-        }
-
-        Adw.SwitchRow remote_control_row {
-          title: _("Remote _Control");
-          use-underline: true;
-          subtitle: _("Allows desktop shares to control the screen");
-        }
-      }
-
-      Adw.PreferencesGroup {
-        title: _("How to Connect");
-        description: _("Use a remote desktop app to connect using the RDP protocol. Additional information about the device’s network location may also be required.");
-
-        Adw.ActionRow hostname_row {
-          title: _("_Hostname");
-          use-underline: true;
-
-          styles [
-            "property",
-          ]
-
-          [suffix]
-          Button {
-            tooltip-text: _("Copy");
-            valign: center;
-            icon-name: "edit-copy-symbolic";
-            clicked => $on_address_copy_clicked(template);
+      Adw.PreferencesPage {
+        Adw.PreferencesGroup {
+          Label {
+            label: _("Share your existing desktop with other devices. The remote connection uses the existing screen resolution.");
+            hexpand: true;
+            wrap: true;
+            justify: center;
 
             styles [
-              "flat",
+              "dim-label",
             ]
           }
         }
 
-        Adw.ActionRow port_row {
-          title: _("_Port");
-          use-underline: true;
-
-          styles [
-            "property",
-          ]
-
-          [suffix]
-          Button {
-            tooltip-text: _("Copy");
-            valign: center;
-            icon-name: "edit-copy-symbolic";
-            clicked => $on_port_copy_clicked(template);
-
-            styles [
-              "flat",
-            ]
+        Adw.PreferencesGroup {
+          Adw.SwitchRow desktop_sharing_row {
+            title: _("_Desktop Sharing");
+            use-underline: true;
           }
-        }
-      }
 
-      Adw.PreferencesGroup {
-        title: _("Login Details");
-
-        Adw.EntryRow username_entry {
-          title: _("_Username");
-          use-underline: true;
-
-          [suffix]
-          Button {
-            tooltip-text: _("Copy");
-            valign: center;
-            icon-name: "edit-copy-symbolic";
-            clicked => $on_username_copy_clicked(template);
-
-            styles [
-              "flat",
-            ]
+          Adw.SwitchRow remote_control_row {
+            title: _("Remote _Control");
+            use-underline: true;
+            subtitle: _("Allows desktop shares to control the screen");
           }
         }
 
-        Adw.PasswordEntryRow password_entry {
-          title: _("P_assword");
-          use-underline: true;
+        Adw.PreferencesGroup {
+          title: _("How to Connect");
+          description: _("Use a remote desktop app to connect using the RDP protocol. Additional information about the device’s network location may also be required.");
 
-          [suffix]
-          Button {
-            tooltip-text: _("Copy");
-            valign: center;
-            icon-name: "edit-copy-symbolic";
-            clicked => $on_password_copy_clicked(template);
+          Adw.ActionRow hostname_row {
+            title: _("_Hostname");
+            use-underline: true;
 
             styles [
-              "flat",
+              "property",
             ]
+
+            [suffix]
+            Button {
+              tooltip-text: _("Copy");
+              valign: center;
+              icon-name: "edit-copy-symbolic";
+              clicked => $on_address_copy_clicked(template);
+
+              styles [
+                "flat",
+              ]
+            }
+          }
+
+          Adw.ActionRow port_row {
+            title: _("_Port");
+            use-underline: true;
+
+            styles [
+              "property",
+            ]
+
+            [suffix]
+            Button {
+              tooltip-text: _("Copy");
+              valign: center;
+              icon-name: "edit-copy-symbolic";
+              clicked => $on_port_copy_clicked(template);
+
+              styles [
+                "flat",
+              ]
+            }
           }
         }
-      }
 
-      Adw.PreferencesGroup {
-        separate-rows: true;
+        Adw.PreferencesGroup login_details_group {
+          title: _("Login Details");
 
-        Adw.ButtonRow generate_password_button_row {
-          sensitive: false;
-          title: _("_Generate New Password");
-          use-underline: true;
-          activated => $on_generate_password_button_row_activated(template);
+          Adw.EntryRow username_entry {
+            title: _("_Username");
+            use-underline: true;
+
+            [suffix]
+            Button {
+              tooltip-text: _("Copy");
+              valign: center;
+              icon-name: "edit-copy-symbolic";
+              clicked => $on_username_copy_clicked(template);
+
+              styles [
+                "flat",
+              ]
+            }
+          }
+
+          Adw.PasswordEntryRow password_entry {
+            title: _("P_assword");
+            use-underline: true;
+
+            [suffix]
+            Button {
+              tooltip-text: _("Copy");
+              valign: center;
+              icon-name: "edit-copy-symbolic";
+              clicked => $on_password_copy_clicked(template);
+
+              styles [
+                "flat",
+              ]
+            }
+          }
         }
 
-        Adw.ButtonRow verify_encryption_button_row {
-          sensitive: false;
-          title: _("_Verify Encryption");
-          use-underline: true;
-          activated => $on_verify_encryption_button_row_activated(template);
+        Adw.PreferencesGroup {
+          separate-rows: true;
+
+          Adw.ButtonRow generate_password_button_row {
+            sensitive: false;
+            title: _("_Generate New Password");
+            use-underline: true;
+            activated => $on_generate_password_button_row_activated(template);
+          }
+
+          Adw.ButtonRow verify_encryption_button_row {
+            sensitive: false;
+            title: _("_Verify Encryption");
+            use-underline: true;
+            activated => $on_verify_encryption_button_row_activated(template);
+          }
         }
       }
     };

commit 334c2a076e22295dbf46b7d1849e1ebb1f8d897d

remote-desktop: Correctly handle missing secret collection
The default collection is missing on a fresh user login with auto-login.
In that case, NULL is a valid return value for secret_collection_for_alias
and does not necessarily indicate an error, so error will not have been set.

The function signature for secret_collection_for_alias_sync correctly annotates
the return value as (nullable).

Fixes: 5940af5b20 ("remote-desktop, desktop-sharing: Handle when keyring is locked")

panels/system/remote-desktop/cc-desktop-sharing-page.c
@@ -497,7 +497,9 @@ setup_login_details_group (GObject      *source_object,
                                                  NULL, &error);
   if (!collection)
     {
-      g_debug ("Can't load secret collection: %s", error->message);
+      const char *msg = error ? error->message : "No such collection";
+
+      g_debug ("Can't load secret collection: %s", msg);
       return;
     }
 