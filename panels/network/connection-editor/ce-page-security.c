cefd15a4311f290e0cae27eb845e9fcd73ee95d9
Felipe Borges <felipeborges@gnome.org>
2026-09-22 14:43:34 +0200

    network, style: Merge individual css files into main css file
    Ever since 4f099e33c5fd3afa9ad37c1e5b62abb9ba645ec5 we started using
    a single CSS stylesheet.

--- panels/network/connection-editor/ce-page-security.c
+++ panels/network/connection-editor/ce-page-security.c
@@ -356,11 +356,6 @@ ce_page_security_init (CEPageSecurity *self)
     g_autoptr (GtkCssProvider) provider = NULL;
 
     gtk_widget_init_template (GTK_WIDGET (self));
-
-    provider = gtk_css_provider_new ();
-    gtk_css_provider_load_from_resource (provider, "/org/gnome/control-center/network/ce-page-security.css");
-    gtk_style_context_add_provider_for_display (gdk_display_get_default (), GTK_STYLE_PROVIDER (provider),
-                                                GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
 }
 
 static void
